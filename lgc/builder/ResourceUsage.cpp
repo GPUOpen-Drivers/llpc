d1c2046e342cea753c71e0d06281c566f08df4c2
Sebastian Neubauer <sebastian.neubauer@amd.com>
2020-04-08 16:41:27 +0200

    Init ResourceUsage to prevent invalid values
    In some cases, fields stay uninitialized but are read.
    This bug was found by ubsan.

---
+++ lgc/builder/ResourceUsage.cpp
@@ -0,0 +1,73 @@
+/*
+ ***********************************************************************************************************************
+ *
+ *  Copyright (c) 2020 Advanced Micro Devices, Inc. All Rights Reserved.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ **********************************************************************************************************************/
+/**
+ ***********************************************************************************************************************
+ * @file  llpcResourceUsage.cpp
+ * @brief LLPC source file: contains implementation of ResourceUsage and InterfaceData.
+ ***********************************************************************************************************************
+ */
+#include "lgc/state/ResourceUsage.h"
+
+using namespace lgc;
+
+// =====================================================================================================================
+ResourceUsage::ResourceUsage(ShaderStage shaderStage) {
+  if (shaderStage == ShaderStageVertex) {
+    // NOTE: For vertex shader, PAL expects base vertex and base instance in user data,
+    // even if they are not used in shader.
+    builtInUsage.vs.baseVertex = true;
+    builtInUsage.vs.baseInstance = true;
+  } else if (shaderStage == ShaderStageTessControl) {
+    auto &calcFactor = inOutUsage.tcs.calcFactor;
+
+    calcFactor.inVertexStride = InvalidValue;
+    calcFactor.outVertexStride = InvalidValue;
+    calcFactor.patchCountPerThreadGroup = InvalidValue;
+    calcFactor.offChip.outPatchStart = InvalidValue;
+    calcFactor.offChip.patchConstStart = InvalidValue;
+    calcFactor.onChip.outPatchStart = InvalidValue;
+    calcFactor.onChip.patchConstStart = InvalidValue;
+    calcFactor.outPatchSize = InvalidValue;
+    calcFactor.patchConstSize = InvalidValue;
+  } else if (shaderStage == ShaderStageGeometry) {
+    inOutUsage.gs.rasterStream = 0;
+    inOutUsage.gs.calcFactor = {};
+  } else if (shaderStage == ShaderStageFragment) {
+    for (uint32_t i = 0; i < MaxColorTargets; ++i) {
+      inOutUsage.fs.expFmts[i] = EXP_FORMAT_ZERO;
+      inOutUsage.fs.outputTypes[i] = BasicType::Unknown;
+    }
+
+    inOutUsage.fs.cbShaderMask = 0;
+    inOutUsage.fs.dummyExport = true;
+    inOutUsage.fs.isNullFs = false;
+  }
+}
+
+// =====================================================================================================================
+InterfaceData::InterfaceData() {
+  memset(userDataMap, InterfaceData::UserDataUnmapped, sizeof(userDataMap));
+  entryArgIdxs.spillTable = InvalidValue;
+}
