c2aed2d7528b9eab8f82d683fa5f2e3f51ec21e1
Carl Ritson <carl.ritson@amd.com>
2020-04-27 11:34:54 +0900

    Remove reference to PassSupport.h
    Including PassSupport.h now throws an error with LLVM.
    Pass.h should be included instead.

--- llpc/lower/llpcSpirvLowerGlobal.cpp
+++ llpc/lower/llpcSpirvLowerGlobal.cpp
@@ -37,7 +37,7 @@
 #include "llvm/ADT/SmallSet.h"
 #include "llvm/IR/Constant.h"
 #include "llvm/IR/Instructions.h"
-#include "llvm/PassSupport.h"
+#include "llvm/Pass.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
