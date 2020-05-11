7f93a6e4ad48ff39ce6723bcf6c9c84cee17df5c
Jiajun Xie <jiaxie@amd.com>
2020-05-11 13:47:33 +0800

    Add PipeBankXor Support for YCbCr Multi Planar Base Addresses
    - Currently, only ADDR_SW_64KB_R_X swizzle mode is supported
    
    Signed-off-by: Jiajun Xie <jiaxie@amd.com>

--- lgc/builder/YCbCrAddressHandler.cpp
+++ lgc/builder/YCbCrAddressHandler.cpp
@@ -29,6 +29,7 @@
 ***********************************************************************************************************************
 */
 #include "YCbCrAddressHandler.h"
+#include "chip/gfx9/gfx9_plus_merged_enum.h"
 #include "lgc/util/GfxRegHandler.h"
 #include "lgc/util/Internal.h"
 #include "llvm/IR/Intrinsics.h"
@@ -47,19 +48,67 @@ void YCbCrAddressHandler::genBaseAddress(unsigned planeCount) {
   // For YCbCr, the possible plane counts are among 1 and 3.
   assert(planeCount > 0 && planeCount < 4);
 
-  // PlaneBaseAddresses[0] is the same as original base addressed as passed in SRD
-  m_planeBaseAddresses.push_back(m_regHandler->getReg(SqRsrcRegs::BaseAddress));
+  // PlaneBaseAddresses[0] is the same as original base addressed as passed in SRD, and is already pipeBankXored
+  Value *virtualAddrPlane0 = m_regHandler->getReg(SqRsrcRegs::BaseAddress);
+  m_planeBaseAddresses.push_back(virtualAddrPlane0);
 
+  Value *pipeBankXor1 = nullptr;
+  Value *pipeBankXor2 = nullptr;
+  Value *pipeBankXorNone = m_builder->getInt32(0);
+
+  switch (m_gfxIp->major) {
+  case 9: {
+    pipeBankXor1 = pipeBankXorNone;
+    pipeBankXor2 = pipeBankXorNone;
+    break;
+  }
+  case 10: {
+    // Judge if current swizzle mode is SW_64KB_R_X
+    Value *isSw64KbRxMode =
+        m_builder->CreateICmpEQ(m_swizzleMode, m_builder->getInt32(Pal::Gfx9::Chip::SWIZZLE_MODE_ENUM::SW_64KB_R_X));
+
+    const unsigned pipesLog2 = 3;
+    const unsigned columnBits = 2;
+    const unsigned xorPatternLen = 8;
+    static const unsigned XorBankRot3b[] = {0, 4, 2, 6, 1, 5, 3, 7};
+    static_assert((sizeof(XorBankRot3b) / sizeof(unsigned)) == xorPatternLen,
+                  "The length of XorBankRot3b is different from xorPatternLen");
+
+    // GFX10 hardware calculate pipe banck xor
+    // SEE: Gfx10Lib::HwlComputePipeBankXor in pal\src\core\imported\addrlib\src\gfx10\gfx10addrlib.cpp
+    auto gfx10HwlComputePipeBankXor = [this](unsigned surfIdx) -> Value * {
+      return m_builder->getInt32(XorBankRot3b[surfIdx % xorPatternLen] << (pipesLog2 + columnBits));
+    };
+
+    // Calculate the pipeBankXor for second plane
+    pipeBankXor1 = m_builder->CreateSelect(isSw64KbRxMode, gfx10HwlComputePipeBankXor(1), pipeBankXorNone);
+
+    // Calculate the pipeBankXor for third plane
+    pipeBankXor2 = m_builder->CreateSelect(isSw64KbRxMode, gfx10HwlComputePipeBankXor(2), pipeBankXorNone);
+    break;
+  }
+  default: {
+    llvm_unreachable("GFX IP not supported!");
+    break;
+  }
+  }
+
+  // Begin the calculating of planar 2 base addresses
   if (planeCount > 1) {
-    // PlaneBaseAddresses[1] = PlaneBaseAddresses[0] + addr256B(PitchY * HeightY)
-    m_planeBaseAddresses.push_back(
-        m_builder->CreateAdd(m_planeBaseAddresses[0],
-                             m_builder->CreateLShr(m_builder->CreateMul(m_pitchY, m_heightY), m_builder->getInt32(8))));
+    // virtualAddrPlane1 = PlaneBaseAddresses[0] + addr256B(PitchY * HeightY)
+    Value *virtualAddrPlane1 = m_builder->CreateAdd(
+        virtualAddrPlane0, m_builder->CreateLShr(m_builder->CreateMul(m_pitchY, m_heightY), m_builder->getInt32(8)));
+    // PlaneBaseAddresses[1] = (virtualAddrPlane1 | pipeBankXor1)
+    m_planeBaseAddresses.push_back(m_builder->CreateOr(virtualAddrPlane1, pipeBankXor1));
+
+    // Begin the calculating of planar 3 base addresses
     if (planeCount > 2) {
-      // PlaneBaseAddresses[2] = PlaneBaseAddresses[1] + addr256B(PitchCb * HeightCb)
-      m_planeBaseAddresses.push_back(m_builder->CreateAdd(
-          m_planeBaseAddresses[1],
-          m_builder->CreateLShr(m_builder->CreateMul(m_pitchCb, m_heightCb), m_builder->getInt32(8))));
+      // virtualAddrPlane2 = PlaneBaseAddresses[1] + addr256B(PitchCb * HeightCb)
+      Value *virtualAddrPlane2 =
+          m_builder->CreateAdd(virtualAddrPlane1, m_builder->CreateLShr(m_builder->CreateMul(m_pitchCb, m_heightCb),
+                                                                        m_builder->getInt32(8)));
+      // PlaneBaseAddresses[2] = (virtualAddrPlane2 | pipeBankXor2)
+      m_planeBaseAddresses.push_back(m_builder->CreateOr(virtualAddrPlane2, pipeBankXor2));
     }
   }
 }
