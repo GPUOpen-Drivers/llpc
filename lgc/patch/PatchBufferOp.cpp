GPUOpen-Drivers/llpc

commit d717e0bd6241989c1881517e11cc74a0c6264eee

Hanlde the behavior of getting array length for a null descriptor
VK_EXT_robustness2 supports for "null descriptors" and adds stricter requirements for how out of bounds reads and writes are handled. This change guarantee the size of variable-length array is well-defined if robustAccess is on.
- Add robustAccessEx in PipelineOptions to guard the implementation for VK_EXT_robustness2.
- Add robustAccess option in Lgc::Options
- Add a new second argument for the byte offset of variable array in Builder::CreateGetBufferDescLength. The result is clamped at 0 if the offset is greater than the buffer length and robustAccess is on.

lgc/patch/PatchBufferOp.cpp
@@ -30,6 +30,7 @@
  */
 #include "PatchBufferOp.h"
 #include "lgc/Builder.h"
+#include "lgc/LgcContext.h"
 #include "lgc/state/IntrinsDefs.h"
 #include "lgc/state/PipelineShaders.h"
 #include "lgc/state/PipelineState.h"
@@ -421,11 +422,30 @@ void PatchBufferOp::visitCallInst(CallInst &callInst) {
     Value *const pointer = getPointerOperandAsInst(callInst.getArgOperand(0));
 
     // Extract element 2 which is the NUM_RECORDS field from the buffer descriptor.
-    Value *const bufferLength = m_builder->CreateExtractElement(m_replacementMap[pointer].first, 2);
+    Value *const bufferDesc = m_replacementMap[pointer].first;
+    Value *numRecords = m_builder->CreateExtractElement(bufferDesc, 2);
+
+    // The result is 0 for a null buffer descriptor
+    if (m_pipelineState->getOptions().allowNullDescriptor) {
+      // Check if the buffer descriptor all bits 0
+      Value *const descIcmpVec = m_builder->CreateICmpEQ(bufferDesc, Constant::getNullValue(bufferDesc->getType()));
+      Value *isNullDesc = m_builder->CreateExtractElement(descIcmpVec, uint64_t(0));
+      const unsigned elemCount =
+          m_pipelineState->getLgcContext()->getTargetInfo().getGpuProperty().descriptorSizeBuffer / sizeof(unsigned);
+      for (unsigned elemIdx = 1; elemIdx != elemCount; ++elemIdx) {
+        Value *elem = m_builder->CreateExtractElement(descIcmpVec, elemIdx);
+        isNullDesc = m_builder->CreateAnd(isNullDesc, elem);
+      }
+      Value *offset = callInst.getArgOperand(1);
+
+      numRecords =
+          m_builder->CreateSelect(isNullDesc, m_builder->getInt32(0), m_builder->CreateSub(numRecords, offset));
+    }
+
     // Record the call instruction so we remember to delete it later.
     m_replacementMap[&callInst] = std::make_pair(nullptr, nullptr);
 
-    callInst.replaceAllUsesWith(bufferLength);
+    callInst.replaceAllUsesWith(numRecords);
   } else
     llvm_unreachable("Should never be called!");
 }

llpc/translator/lib/SPIRV/SPIRVReader.cpp
@@ -2631,17 +2631,16 @@ template <> Value *SPIRVToLLVM::transValueWithOpcode<OpArrayLength>(SPIRVValue *
   const unsigned remappedMemberIndex =
       lookupRemappedTypeElements(spvStruct->getType()->getPointerElementType(), memberIndex);
 
-  Value *const bufferLength = getBuilder()->CreateGetBufferDescLength(pStruct);
-
   StructType *const structType = cast<StructType>(pStruct->getType()->getPointerElementType());
   const StructLayout *const structLayout = m_m->getDataLayout().getStructLayout(structType);
   const unsigned offset = static_cast<unsigned>(structLayout->getElementOffset(remappedMemberIndex));
   Value *const offsetVal = getBuilder()->getInt32(offset);
+  Value *const bufferLength = getBuilder()->CreateGetBufferDescLength(pStruct, offsetVal);
 
   Type *const memberType = structType->getStructElementType(remappedMemberIndex)->getArrayElementType();
   const unsigned stride = static_cast<unsigned>(m_m->getDataLayout().getTypeSizeInBits(memberType) / 8);
 
-  return getBuilder()->CreateUDiv(getBuilder()->CreateSub(bufferLength, offsetVal), getBuilder()->getInt32(stride));
+  return getBuilder()->CreateUDiv(bufferLength, getBuilder()->getInt32(stride));
 }
 
 // =====================================================================================================================

include/vkgcDefs.h
@@ -73,6 +73,7 @@
 //* %Version History
 //* | %Version | Change Description                                                                                    |
 //* | -------- | ----------------------------------------------------------------------------------------------------- |
+//* |     40.2 | Added flag nullDescriptor in PipelineOptions to support VK_EXT_robustness2                            |
 //* |     40.1 | Added disableLoopUnroll to PipelineShaderOptions                                                      |
 //* |     40.0 | Added DescriptorReserved12, which moves DescriptorYCbCrSampler down to 13                             |
 //* |     39.0 | Non-LLPC-specific XGL code should #include vkcgDefs.h instead of llpc.h                               |
@@ -266,6 +267,8 @@ struct PipelineOptions {
 
   ShadowDescriptorTableUsage shadowDescriptorTableUsage; ///< Controls shadow descriptor table.
   unsigned shadowDescriptorTablePtrHigh;                 ///< Sets high part of VA ptr for shadow descriptor table.
+  bool nullDescriptor;                                   ///< If set, support VK_EXT_robustness2 to give defined
+                                                         ///  behavior for null descriptor
 };
 
 /// Prototype of allocator for output data buffer, used in shader-specific operations.

lgc/builder/BuilderImpl.h
@@ -286,7 +286,7 @@ public:
   llvm::Value *CreateLoadPushConstantsPtr(llvm::Type *pushConstantsTy, const llvm::Twine &instName) override final;
 
   // Create a buffer length query based on the specified descriptor.
-  llvm::Value *CreateGetBufferDescLength(llvm::Value *const bufferDesc,
+  llvm::Value *CreateGetBufferDescLength(llvm::Value *const bufferDesc, llvm::Value *offset,
                                          const llvm::Twine &instName = "") override final;
 
 private:

lgc/builder/BuilderRecorder.cpp
@@ -1096,8 +1096,8 @@ Value *BuilderRecorder::CreateLoadPushConstantsPtr(Type *pushConstantsTy, const 
 //
 // @param bufferDesc : The buffer descriptor to query.
 // @param instName : Name to give instruction(s).
-Value *BuilderRecorder::CreateGetBufferDescLength(Value *const bufferDesc, const Twine &instName) {
-  return record(Opcode::GetBufferDescLength, getInt32Ty(), {bufferDesc}, instName);
+Value *BuilderRecorder::CreateGetBufferDescLength(Value *const bufferDesc, Value *offset, const Twine &instName) {
+  return record(Opcode::GetBufferDescLength, getInt32Ty(), {bufferDesc, offset}, instName);
 }
 
 // =====================================================================================================================

lgc/builder/BuilderRecorder.h
@@ -364,7 +364,7 @@ public:
 
   llvm::Value *CreateLoadPushConstantsPtr(llvm::Type *pushConstantsTy, const llvm::Twine &instName) override final;
 
-  llvm::Value *CreateGetBufferDescLength(llvm::Value *const bufferDesc,
+  llvm::Value *CreateGetBufferDescLength(llvm::Value *const bufferDesc, llvm::Value *offset,
                                          const llvm::Twine &instName = "") override final;
 
   // -----------------------------------------------------------------------------------------------------------------

lgc/builder/BuilderReplayer.cpp
@@ -454,7 +454,8 @@ Value *BuilderReplayer::processCall(unsigned opcode, CallInst *call) {
   }
 
   case BuilderRecorder::Opcode::GetBufferDescLength: {
-    return m_builder->CreateGetBufferDescLength(args[0]);
+    return m_builder->CreateGetBufferDescLength(args[0],  // buffer descriptor
+                                                args[1]); // offset
   }
 
   // Replayer implementations of ImageBuilder methods

lgc/builder/DescBuilder.cpp
@@ -494,12 +494,11 @@ Value *DescBuilder::scalarizeIfUniform(Value *value, bool isNonUniform) {
 //
 // @param bufferDesc : The buffer descriptor to query.
 // @param instName : Name to give instruction(s).
-Value *DescBuilder::CreateGetBufferDescLength(Value *const bufferDesc, const Twine &instName) {
+Value *DescBuilder::CreateGetBufferDescLength(Value *const bufferDesc, Value *offset, const Twine &instName) {
   // In future this should become a full LLVM intrinsic, but for now we patch in a late intrinsic that is cleaned up
   // in patch buffer op.
   std::string callName = lgcName::LateBufferLength;
-  addTypeMangling(nullptr, bufferDesc, callName);
-  return CreateNamedCall(callName, getInt32Ty(), bufferDesc, Attribute::ReadNone);
+  return CreateNamedCall(lgcName::LateBufferLength, getInt32Ty(), {bufferDesc, offset}, Attribute::ReadNone);
 }
 
 // =====================================================================================================================

commit 277ee2183a088a18da39635f29d33b30c6e51ee9

PatchBufferOp: fix and improve GetBufferDescLength lowering
Since d717e0bd62 ("Hanlde the behavior of getting array length for
a null descriptor"), the implementation of OpArrayLength no longer
subtracts the runtime array's offset, relying on the
GetBufferDescLength implementation to do so. However, that
implementation only did the subtraction in the allowNullDescriptor
case.

Furthermore, the implementation of the allowNullDescriptor is
rather expensive: it shouldn't be necessary to check all dwords
of the descriptor. Instead, just clamping the array length to 0
should do the trick (which has the side effect of likely being
more robust in practice when applications use buffers that are
non-null but too small -- this case seems underspecified in
SPIR-V and Vulkan, so clamping should be fine).

Fixes: d717e0bd62 ("Hanlde the behavior of getting array length for a null descriptor")

lgc/patch/PatchBufferOp.cpp
@@ -424,23 +424,18 @@ void PatchBufferOp::visitCallInst(CallInst &callInst) {
     // Extract element 2 which is the NUM_RECORDS field from the buffer descriptor.
     Value *const bufferDesc = m_replacementMap[pointer].first;
     Value *numRecords = m_builder->CreateExtractElement(bufferDesc, 2);
+    Value *offset = callInst.getArgOperand(1);
 
-    // The result is 0 for a null buffer descriptor
+    // If null descriptors are allowed, we must guarantee a 0 result for a null buffer descriptor.
+    //
+    // What we implement here is in fact more robust: ensure that the subtraction of the offset is clamped to 0.
+    // The backend should be able to achieve this with a single additional ALU instruction (e.g. s_max_u32).
     if (m_pipelineState->getOptions().allowNullDescriptor) {
-      // Check if the buffer descriptor all bits 0
-      Value *const descIcmpVec = m_builder->CreateICmpEQ(bufferDesc, Constant::getNullValue(bufferDesc->getType()));
-      Value *isNullDesc = m_builder->CreateExtractElement(descIcmpVec, uint64_t(0));
-      const unsigned elemCount =
-          m_pipelineState->getLgcContext()->getTargetInfo().getGpuProperty().descriptorSizeBuffer / sizeof(unsigned);
-      for (unsigned elemIdx = 1; elemIdx != elemCount; ++elemIdx) {
-        Value *elem = m_builder->CreateExtractElement(descIcmpVec, elemIdx);
-        isNullDesc = m_builder->CreateAnd(isNullDesc, elem);
-      }
-      Value *offset = callInst.getArgOperand(1);
-
-      numRecords =
-          m_builder->CreateSelect(isNullDesc, m_builder->getInt32(0), m_builder->CreateSub(numRecords, offset));
+      Value *const underflow = m_builder->CreateICmpUGT(offset, numRecords);
+      numRecords = m_builder->CreateSelect(underflow, offset, numRecords);
     }
+
+    numRecords = m_builder->CreateSub(numRecords, offset);
 
     // Record the call instruction so we remember to delete it later.
     m_replacementMap[&callInst] = std::make_pair(nullptr, nullptr);

llpc/translator/lib/SPIRV/SPIRVReader.cpp
@@ -2635,12 +2635,12 @@ template <> Value *SPIRVToLLVM::transValueWithOpcode<OpArrayLength>(SPIRVValue *
   const StructLayout *const structLayout = m_m->getDataLayout().getStructLayout(structType);
   const unsigned offset = static_cast<unsigned>(structLayout->getElementOffset(remappedMemberIndex));
   Value *const offsetVal = getBuilder()->getInt32(offset);
-  Value *const bufferLength = getBuilder()->CreateGetBufferDescLength(pStruct, offsetVal);
+  Value *const arrayBytes = getBuilder()->CreateGetBufferDescLength(pStruct, offsetVal);
 
   Type *const memberType = structType->getStructElementType(remappedMemberIndex)->getArrayElementType();
   const unsigned stride = static_cast<unsigned>(m_m->getDataLayout().getTypeSizeInBits(memberType) / 8);
 
-  return getBuilder()->CreateUDiv(bufferLength, getBuilder()->getInt32(stride));
+  return getBuilder()->CreateUDiv(arrayBytes, getBuilder()->getInt32(stride));
 }
 
 // =====================================================================================================================