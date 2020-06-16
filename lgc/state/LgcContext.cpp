2a9bc024895fb3f64697dee1d97bd8734ddae3ed
Piotr Sobczak <Piotr.Sobczak@amd.com>
2020-06-16 10:53:35 +0200

    Re-enable backend transform for kills
    The transform was disabled in #682, but has been fixed since.

--- lgc/state/LgcContext.cpp
+++ lgc/state/LgcContext.cpp
@@ -145,6 +145,7 @@ void LgcContext::initialize() {
   setOptionDefault("amdgpu-atomic-optimizations", "1");
   setOptionDefault("use-gpu-divergence-analysis", "1");
   setOptionDefault("structurizecfg-skip-uniform-regions", "1");
+  setOptionDefault("amdgpu-conditional-discard-transformations", "1");
 }
 
 // =====================================================================================================================
