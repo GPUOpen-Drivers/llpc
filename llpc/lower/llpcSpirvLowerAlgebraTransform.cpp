6e3f7e69fded9d9f6456f4da13212a5f85a57364
dstuttar <david.stuttard@amd.com>
2020-06-19 15:56:30 +0100

    Update VectorType::get to remove deprecated variant
    LLVM updates have removed the VectorType::get variant with a defaulted third
    argument. Instead, FixedVectorType::get is preferred.
    
    Test before and after the change in LLVM, both work.

--- llpc/lower/llpcSpirvLowerAlgebraTransform.cpp
+++ llpc/lower/llpcSpirvLowerAlgebraTransform.cpp
@@ -339,7 +339,7 @@ void SpirvLowerAlgebraTransform::visitFPTruncInst(FPTruncInst &fptruncInst) {
       // NOTE: doubel -> float16 conversion is done in backend compiler with RTE rounding. Thus, we have to split
       // it with two phases to disable such lowering if we need RTZ rounding.
       auto floatTy = srcTy->isVectorTy()
-                         ? VectorType::get(Type::getFloatTy(*m_context), cast<VectorType>(srcTy)->getNumElements())
+                         ? FixedVectorType::get(Type::getFloatTy(*m_context), cast<VectorType>(srcTy)->getNumElements())
                          : Type::getFloatTy(*m_context);
       auto floatValue = new FPTruncInst(src, floatTy, "", &fptruncInst);
       auto dest = new FPTruncInst(floatValue, destTy, "", &fptruncInst);
