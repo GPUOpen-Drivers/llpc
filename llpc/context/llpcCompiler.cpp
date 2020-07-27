GPUOpen-Drivers/llpc

commit 00e90a26fa81e11fcfcd20cbfddfaf3836e807a7

Refactor loop metadata handling
Moved the loop metadata handling from llpc/lower to lgc/patch as a
first step in extending the functionality to allow more flexible
handling of loop metadata, e.g. DontUnroll.
This change preserves the previous behavior.

llpc/context/llpcCompiler.cpp
@@ -159,9 +159,6 @@ opt<int> ForceLoopUnrollCount("force-loop-unroll-count", cl::desc("Force loop un
 opt<bool> EnableShaderModuleOpt("enable-shader-module-opt",
                                 cl::desc("Enable translate & lower phase in shader module build."), init(false));
 
-// -disable-licm: annotate loops with metadata to disable the LLVM LICM pass
-opt<bool> DisableLicm("disable-licm", desc("Disable LLVM LICM pass"), init(false));
-
 // -trim-debug-info: Trim debug information in SPIR-V binary
 opt<bool> TrimDebugInfo("trim-debug-info", cl::desc("Trim debug information in SPIR-V binary"), init(true));
 
@@ -627,8 +624,7 @@ Result Compiler::BuildShaderModule(const ShaderModuleBuildInfo *shaderInfo, Shad
 
           // Per-shader SPIR-V lowering passes.
           SpirvLower::addPasses(context, static_cast<ShaderStage>(entryNames[i].stage), *lowerPassMgr,
-                                timerProfiler.getTimer(TimerLower), cl::ForceLoopUnrollCount
-          );
+                                timerProfiler.getTimer(TimerLower));
 
           lowerPassMgr->add(createBitcodeWriterPass(moduleBinaryStream));
 
@@ -1139,8 +1135,7 @@ Result Compiler::buildPipelineInternal(Context *context, ArrayRef<const Pipeline
       std::unique_ptr<lgc::PassManager> lowerPassMgr(lgc::PassManager::Create());
       lowerPassMgr->setPassIndex(&passIndex);
 
-      SpirvLower::addPasses(context, entryStage, *lowerPassMgr, timerProfiler.getTimer(TimerLower), forceLoopUnrollCount
-      );
+      SpirvLower::addPasses(context, entryStage, *lowerPassMgr, timerProfiler.getTimer(TimerLower));
       // Run the passes.
       bool success = runPasses(&*lowerPassMgr, modules[shaderIndex]);
       if (!success) {

llpc/context/llpcPipelineContext.cpp
@@ -84,6 +84,9 @@ static cl::opt<unsigned> ScalarThreshold("scalar-threshold", cl::desc("The thres
 static cl::opt<bool> EnableSiScheduler("enable-si-scheduler", cl::desc("Enable target option si-scheduler"),
                                        cl::init(false));
 
+// -disable-licm: annotate loops with metadata to disable the LLVM LICM pass
+static cl::opt<bool> DisableLicm("disable-licm", cl::desc("Disable LLVM LICM pass"), cl::init(false));
+
 // -subgroup-size: sub-group size exposed via Vulkan API.
 static cl::opt<int> SubgroupSize("subgroup-size", cl::desc("Sub-group size exposed via Vulkan API"), cl::init(64));
 
@@ -374,6 +377,7 @@ void PipelineContext::setOptionsInPipeline(Pipeline *pipeline) const {
 #endif
 
       shaderOptions.useSiScheduler = EnableSiScheduler || shaderInfo->options.useSiScheduler;
+      shaderOptions.disableLicm = DisableLicm || shaderInfo->options.disableLicm;
       shaderOptions.updateDescInElf = shaderInfo->options.updateDescInElf;
       shaderOptions.unrollThreshold = shaderInfo->options.unrollThreshold;
 

lgc/CMakeLists.txt
@@ -116,6 +116,7 @@ target_sources(LLVMlgc PRIVATE
     patch/PatchIntrinsicSimplify.cpp
     patch/PatchLlvmIrInclusion.cpp
     patch/PatchLoadScalarizer.cpp
+    patch/PatchLoopMetadata.cpp
     patch/PatchNullFragShader.cpp
     patch/PatchPeepholeOpt.cpp
     patch/PatchPreparePipelineAbi.cpp

lgc/include/lgc/patch/Patch.h
@@ -52,6 +52,7 @@ void initializePatchInOutImportExportPass(PassRegistry &);
 void initializePatchIntrinsicSimplifyPass(PassRegistry &);
 void initializePatchLlvmIrInclusionPass(PassRegistry &);
 void initializePatchLoadScalarizerPass(PassRegistry &);
+void initializePatchLoopMetadataPass(PassRegistry &);
 void initializePatchNullFragShaderPass(PassRegistry &);
 void initializePatchPeepholeOptPass(PassRegistry &);
 void initializePatchPreparePipelineAbiPass(PassRegistry &);
@@ -77,6 +78,7 @@ inline static void initializePatchPasses(llvm::PassRegistry &passRegistry) {
   initializePatchIntrinsicSimplifyPass(passRegistry);
   initializePatchLlvmIrInclusionPass(passRegistry);
   initializePatchLoadScalarizerPass(passRegistry);
+  initializePatchLoopMetadataPass(passRegistry);
   initializePatchNullFragShaderPass(passRegistry);
   initializePatchPeepholeOptPass(passRegistry);
   initializePatchPreparePipelineAbiPass(passRegistry);
@@ -93,6 +95,7 @@ llvm::ModulePass *createPatchInOutImportExport();
 llvm::FunctionPass *createPatchIntrinsicSimplify();
 llvm::ModulePass *createPatchLlvmIrInclusion();
 llvm::FunctionPass *createPatchLoadScalarizer();
+llvm::ModulePass *createPatchLoopMetadata();
 llvm::ModulePass *createPatchNullFragShader();
 llvm::FunctionPass *createPatchPeepholeOpt();
 llvm::ModulePass *createPatchPreparePipelineAbi(bool onlySetCallingConvs);

lgc/interface/lgc/Pipeline.h
@@ -160,7 +160,7 @@ struct ShaderOptions {
   WaveBreak waveBreakSize; // Size of region to force the end of a wavefront (GFX10+).
                            // Only valid for fragment shaders.
 
-  // Vector szie threshold for load scalarizer. 0 means do not scalarize loads at all.
+  // Vector size threshold for load scalarizer. 0 means do not scalarize loads at all.
   unsigned loadScalarizerThreshold;
 
   // Use the LLVM backend's SI scheduler instead of the default scheduler.
@@ -169,11 +169,20 @@ struct ShaderOptions {
   // Whether update descriptor root offset in ELF
   bool updateDescInElf;
 
-  /// Default unroll threshold for LLVM.
+  // Default unroll threshold for LLVM.
   unsigned unrollThreshold;
 
   /// Override FP32 denormal handling.
   DenormalMode fp32DenormalMode;
+
+  // Unroll loops by specified amount. 0 is default, 1 is no unroll.
+  unsigned forceLoopUnrollCount;
+
+  // Disable loop unrolling.
+  bool disableLoopUnroll;
+
+  // Disable LICM pass.
+  bool disableLicm;
 };
 
 // =====================================================================================================================

lgc/patch/Patch.cpp
@@ -123,6 +123,9 @@ void Patch::addPasses(PipelineState *pipelineState, legacy::PassManager &passMgr
   passMgr.add(createAlwaysInlinerLegacyPass());
   passMgr.add(createGlobalDCEPass());
 
+  // Patch loop metadata
+  passMgr.add(createPatchLoopMetadata());
+
   // Check shader cache
   auto checkShaderCachePass = createPatchCheckShaderCache();
   passMgr.add(checkShaderCachePass);

lgc/patch/PatchLoopMetadata.cpp
@@ -24,91 +24,85 @@
  **********************************************************************************************************************/
 /**
  ***********************************************************************************************************************
- * @file  llpcSpirvLowerLoopUnrollControl.cpp
- * @brief LLPC source file: contains implementation of class Llpc::SpirvLowerLoopUnrollControl.
+ * @file  PatchLoopMetadata.cpp
+ * @brief LLPC source file: contains implementation of class lgc::PatchLoopMetadata.
  ***********************************************************************************************************************
  */
-#include "llpcSpirvLowerLoopUnrollControl.h"
-#include "SPIRVInternal.h"
-#include "llpcContext.h"
+#include "lgc/patch/Patch.h"
+#include "lgc/state/PipelineState.h"
+#include "lgc/state/TargetInfo.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 #include <vector>
 
-#define DEBUG_TYPE "llpc-spirv-lower-loop-unroll-control"
-
-namespace llvm {
-
-namespace cl {
-
-extern opt<bool> DisableLicm;
-
-} // namespace cl
-
-} // namespace llvm
+#define DEBUG_TYPE "llpc-patch-loop-metadata"
 
 using namespace llvm;
-using namespace SPIRV;
-using namespace Llpc;
+using namespace lgc;
 
-namespace Llpc {
+namespace {
+
+// =====================================================================================================================
+// Represents the LLVM pass for patching loop metadata.
+class PatchLoopMetadata : public Patch {
+public:
+  PatchLoopMetadata();
+
+  bool runOnModule(Module &module) override;
+
+  static char ID; // ID of this pass
+
+  void getAnalysisUsage(AnalysisUsage &analysisUsage) const override {
+    analysisUsage.addRequired<PipelineStateWrapper>();
+  }
+
+private:
+  unsigned m_forceLoopUnrollCount; // Force loop unroll count
+  bool m_disableLicm;              // Disable LLVM LICM pass
+  bool m_disableLoopUnroll;        // Forcibly disable loop unroll
+  GfxIpVersion m_gfxIp;
+};
+
+} // anonymous namespace
 
 // =====================================================================================================================
 // Initializes static members.
-char SpirvLowerLoopUnrollControl::ID = 0;
+char PatchLoopMetadata::ID = 0;
 
 // =====================================================================================================================
-// Pass creator, creates the pass of SPIR-V lowering operations for loop unroll control
-//
-// @param forceLoopUnrollCount : Force loop unroll count
-ModulePass *createSpirvLowerLoopUnrollControl(unsigned forceLoopUnrollCount) {
-  auto pass = new SpirvLowerLoopUnrollControl(forceLoopUnrollCount);
-  return pass;
+// Pass creator, creates the pass for patching loop metadata
+ModulePass *lgc::createPatchLoopMetadata() {
+  return new PatchLoopMetadata();
 }
 
 // =====================================================================================================================
-SpirvLowerLoopUnrollControl::SpirvLowerLoopUnrollControl()
-    : SpirvLower(ID), m_forceLoopUnrollCount(0), m_disableLicm(false), m_disableLoopUnroll(false) {
+PatchLoopMetadata::PatchLoopMetadata()
+    : Patch(ID), m_forceLoopUnrollCount(0), m_disableLicm(false), m_disableLoopUnroll(false) {
 }
 
 // =====================================================================================================================
-//
-// @param forceLoopUnrollCount : Force loop unroll count
-SpirvLowerLoopUnrollControl::SpirvLowerLoopUnrollControl(unsigned forceLoopUnrollCount)
-    : SpirvLower(ID), m_forceLoopUnrollCount(forceLoopUnrollCount), m_disableLicm(false), m_disableLoopUnroll(false) {
-}
-
-// =====================================================================================================================
-// Executes this SPIR-V lowering pass on the specified LLVM module.
+// Executes this LLVM patching pass on the specified LLVM module.
 //
 // @param [in,out] module : LLVM module to be run on
-bool SpirvLowerLoopUnrollControl::runOnModule(Module &module) {
-  LLVM_DEBUG(dbgs() << "Run the pass Spirv-Lower-Unroll-Control\n");
+bool PatchLoopMetadata::runOnModule(Module &module) {
+  LLVM_DEBUG(dbgs() << "Run the pass patch-loop-metadata\n");
 
-  SpirvLower::init(&module);
-
-  if (m_context->getPipelineContext()) {
-    auto shaderOptions = &(m_context->getPipelineShaderInfo(m_shaderStage)->options);
-    if (shaderOptions->forceLoopUnrollCount > 0)
-      m_forceLoopUnrollCount = shaderOptions->forceLoopUnrollCount;
-#if LLPC_CLIENT_INTERFACE_MAJOR_VERSION >= 35
-    m_disableLicm = shaderOptions->disableLicm | cl::DisableLicm;
-#endif
-    m_disableLoopUnroll = shaderOptions->disableLoopUnroll;
-  }
-
-  if (m_forceLoopUnrollCount == 0 && !m_disableLicm && !m_disableLoopUnroll)
-    return false;
-
-  if (m_shaderStage == ShaderStageTessControl || m_shaderStage == ShaderStageTessEval ||
-      m_shaderStage == ShaderStageGeometry) {
-    // Disabled on above shader types.
-    return false;
-  }
-
+  Patch::init(&module);
+  PipelineState *m_pipelineState = getAnalysis<PipelineStateWrapper>().getPipelineState(&module);
+  m_gfxIp = m_pipelineState->getTargetInfo().getGfxIpVersion();
   bool changed = false;
-  for (auto &func : module) {
+
+  for (Function &func : module) {
+    ShaderStage stage = getShaderStage(&func);
+    if (stage == -1)
+      continue;
+    if (auto shaderOptions = &m_pipelineState->getShaderOptions(stage)) {
+      m_disableLoopUnroll = shaderOptions->disableLoopUnroll;
+      m_forceLoopUnrollCount = shaderOptions->forceLoopUnrollCount;
+      m_disableLicm = shaderOptions->disableLicm;
+    }
+
     for (auto &block : func) {
       auto terminator = block.getTerminator();
       MDNode *loopMetaNode = terminator->getMetadata("llvm.loop");
@@ -124,17 +118,16 @@ bool SpirvLowerLoopUnrollControl::runOnModule(Module &module) {
             MDNode::get(*m_context, MDString::get(*m_context, "llvm.loop.unroll.disable"));
         loopMetaNode = MDNode::concatenate(loopMetaNode, MDNode::get(*m_context, disableLoopUnrollMetaNode));
       } else if (m_forceLoopUnrollCount && loopMetaNode->getNumOperands() <= 1) {
-        // We have a loop backedge with !llvm.loop metadata containing just
-        // one operand pointing to itself, meaning that the SPIR-V did not
-        // have an unroll or don't-unroll directive, so we can add the force
-        // unroll count metadata.
+        // We have a loop backedge with !llvm.loop metadata containing just one
+        // operand pointing to itself, meaning that the SPIR-V did not have an
+        // unroll directive, so we can add the force unroll count metadata.
         Metadata *unrollCountMeta[] = {
             MDString::get(*m_context, "llvm.loop.unroll.count"),
             ConstantAsMetadata::get(ConstantInt::get(Type::getInt32Ty(*m_context), m_forceLoopUnrollCount))};
         MDNode *loopUnrollCountMetaNode = MDNode::get(*m_context, unrollCountMeta);
         loopMetaNode = MDNode::concatenate(loopMetaNode, MDNode::get(*m_context, loopUnrollCountMetaNode));
         // We also disable all nonforced loop transformations to ensure our transformation is not blocked
-        llvm::Metadata *nonforcedMeta[] = {MDString::get(*m_context, "llvm.loop.disable_nonforced")};
+        Metadata *nonforcedMeta[] = {MDString::get(*m_context, "llvm.loop.disable_nonforced")};
         MDNode *nonforcedMetaNode = MDNode::get(*m_context, nonforcedMeta);
         loopMetaNode = MDNode::concatenate(loopMetaNode, MDNode::get(*m_context, nonforcedMetaNode));
       }
@@ -151,9 +144,6 @@ bool SpirvLowerLoopUnrollControl::runOnModule(Module &module) {
   return changed;
 }
 
-} // namespace Llpc
-
 // =====================================================================================================================
-// Initializes the pass of SPIR-V lowering operations for loop unroll control.
-INITIALIZE_PASS(SpirvLowerLoopUnrollControl, "Spirv-lower-loop-unroll-control",
-                "Set metadata to control LLPC loop unrolling", false, false)
+// Initializes the pass for patching Loop metadata.
+INITIALIZE_PASS(PatchLoopMetadata, DEBUG_TYPE, "Set or amend metadata to control loop unrolling", false, false)

llpc/CMakeLists.txt
@@ -167,7 +167,6 @@ if(ICD_BUILD_LLPC)
         lower/llpcSpirvLowerConstImmediateStore.cpp
         lower/llpcSpirvLowerGlobal.cpp
         lower/llpcSpirvLowerInstMetaRemove.cpp
-        lower/llpcSpirvLowerLoopUnrollControl.cpp
         lower/llpcSpirvLowerMath.cpp
         lower/llpcSpirvLowerMemoryOp.cpp
         lower/llpcSpirvLowerResourceCollect.cpp

commit 4b43d88e20326656cfdd6617ee46748a09d02d69

Fix and tidy up the -force-loop-unroll-count option
An earlier refactoring of the loop metadata handling accidentally
broke the amdllpc -force-loop-unroll-count option.
This change tidies up the handling of this option and restores
its functionality.

Fixes: 00e90a2 ("Refactor loop metadata handling")

llpc/context/llpcCompiler.cpp
@@ -152,9 +152,6 @@ opt<bool> EnableDynamicLoopUnroll("enable-dynamic-loop-unroll", desc("Enable dyn
                                   init(false));
 #endif
 
-// -force-loop-unroll-count: Force to set the loop unroll count.
-opt<int> ForceLoopUnrollCount("force-loop-unroll-count", cl::desc("Force loop unroll count"), init(0));
-
 // -enable-shader-module-opt: Enable translate & lower phase in shader module build.
 opt<bool> EnableShaderModuleOpt("enable-shader-module-opt",
                                 cl::desc("Enable translate & lower phase in shader module build."), init(false));
@@ -788,10 +785,9 @@ Result Compiler::BuildShaderModule(const ShaderModuleBuildInfo *shaderInfo, Shad
 //
 // @param context : Acquired context
 // @param shaderInfo : Shader info of this pipeline
-// @param forceLoopUnrollCount : Force loop unroll count (0 means disable)
 // @param [out] pipelineElf : Output Elf package
 Result Compiler::buildPipelineWithRelocatableElf(Context *context, ArrayRef<const PipelineShaderInfo *> shaderInfo,
-                                                 unsigned forceLoopUnrollCount, ElfPackage *pipelineElf) {
+                                                 ElfPackage *pipelineElf) {
   LLPC_OUTS("Building pipeline with relocatable shader elf.\n")
   Result result = Result::Success;
 
@@ -860,8 +856,7 @@ Result Compiler::buildPipelineWithRelocatableElf(Context *context, ArrayRef<cons
                                                                                     nullptr, nullptr, nullptr};
     singleStageShaderInfo[stage] = shaderInfo[stage];
 
-    result =
-        buildPipelineInternal(context, singleStageShaderInfo, forceLoopUnrollCount, /*unlinked=*/true, &elf[stage]);
+    result = buildPipelineInternal(context, singleStageShaderInfo, /*unlinked=*/true, &elf[stage]);
 
     // Add the result to the cache.
     if (result == Result::Success) {
@@ -987,11 +982,10 @@ bool Compiler::canUseRelocatableComputeShaderElf(const PipelineShaderInfo *shade
 //
 // @param context : Acquired context
 // @param shaderInfo : Shader info of this pipeline
-// @param forceLoopUnrollCount : Force loop unroll count (0 means disable)
 // @param unlinked : Do not provide some state to LGC, so offsets are generated as relocs
 // @param [out] pipelineElf : Output Elf package
-Result Compiler::buildPipelineInternal(Context *context, ArrayRef<const PipelineShaderInfo *> shaderInfo,
-                                       unsigned forceLoopUnrollCount, bool unlinked, ElfPackage *pipelineElf) {
+Result Compiler::buildPipelineInternal(Context *context, ArrayRef<const PipelineShaderInfo *> shaderInfo, bool unlinked,
+                                       ElfPackage *pipelineElf) {
   Result result = Result::Success;
   unsigned passIndex = 0;
   const PipelineShaderInfo *fragmentShaderInfo = nullptr;
@@ -1393,21 +1387,19 @@ unsigned Compiler::ConvertColorBufferFormatToExportFormat(const ColorTarget *tar
 //
 // @param graphicsContext : Graphics context this graphics pipeline
 // @param shaderInfo : Shader info of this graphics pipeline
-// @param forceLoopUnrollCount : Force loop unroll count (0 means disable)
 // @param buildingRelocatableElf : Build the pipeline by linking relocatable elf
 // @param [out] pipelineElf : Output Elf package
 Result Compiler::buildGraphicsPipelineInternal(GraphicsContext *graphicsContext,
                                                ArrayRef<const PipelineShaderInfo *> shaderInfo,
-                                               unsigned forceLoopUnrollCount, bool buildingRelocatableElf,
-                                               ElfPackage *pipelineElf) {
+                                               bool buildingRelocatableElf, ElfPackage *pipelineElf) {
   Context *context = acquireContext();
   context->attachPipelineContext(graphicsContext);
 
   Result result = Result::Success;
   if (buildingRelocatableElf)
-    result = buildPipelineWithRelocatableElf(context, shaderInfo, forceLoopUnrollCount, pipelineElf);
+    result = buildPipelineWithRelocatableElf(context, shaderInfo, pipelineElf);
   else
-    result = buildPipelineInternal(context, shaderInfo, forceLoopUnrollCount, /*unlinked=*/false, pipelineElf);
+    result = buildPipelineInternal(context, shaderInfo, /*unlinked=*/false, pipelineElf);
   releaseContext(context);
   return result;
 }
@@ -1489,11 +1481,9 @@ Result Compiler::BuildGraphicsPipeline(const GraphicsPipelineBuildInfo *pipeline
   ElfPackage candidateElf;
 
   if (cacheEntryState == ShaderEntryState::Compiling || (m_cache && cacheResult != Result::Success)) {
-    unsigned forceLoopUnrollCount = cl::ForceLoopUnrollCount;
 
     GraphicsContext graphicsContext(m_gfxIp, pipelineInfo, &pipelineHash, &cacheHash);
-    result = buildGraphicsPipelineInternal(&graphicsContext, shaderInfo, forceLoopUnrollCount, buildingRelocatableElf,
-                                           &candidateElf);
+    result = buildGraphicsPipelineInternal(&graphicsContext, shaderInfo, buildingRelocatableElf, &candidateElf);
 
     if (result == Result::Success) {
       elfBin.codeSize = candidateElf.size();
@@ -1533,12 +1523,10 @@ Result Compiler::BuildGraphicsPipeline(const GraphicsPipelineBuildInfo *pipeline
 //
 // @param computeContext : Compute context this compute pipeline
 // @param pipelineInfo : Pipeline info of this compute pipeline
-// @param forceLoopUnrollCount : Force loop unroll count (0 means disable)
 // @param buildingRelocatableElf : Build the pipeline by linking relocatable elf
 // @param [out] pipelineElf : Output Elf package
 Result Compiler::buildComputePipelineInternal(ComputeContext *computeContext,
-                                              const ComputePipelineBuildInfo *pipelineInfo,
-                                              unsigned forceLoopUnrollCount, bool buildingRelocatableElf,
+                                              const ComputePipelineBuildInfo *pipelineInfo, bool buildingRelocatableElf,
                                               ElfPackage *pipelineElf) {
   Context *context = acquireContext();
   context->attachPipelineContext(computeContext);
@@ -1548,9 +1536,9 @@ Result Compiler::buildComputePipelineInternal(ComputeContext *computeContext,
   };
   Result result;
   if (buildingRelocatableElf)
-    result = buildPipelineWithRelocatableElf(context, shadersInfo, forceLoopUnrollCount, pipelineElf);
+    result = buildPipelineWithRelocatableElf(context, shadersInfo, pipelineElf);
   else
-    result = buildPipelineInternal(context, shadersInfo, forceLoopUnrollCount, /*unlinked=*/false, pipelineElf);
+    result = buildPipelineInternal(context, shadersInfo, /*unlinked=*/false, pipelineElf);
   releaseContext(context);
   return result;
 }
@@ -1621,12 +1609,10 @@ Result Compiler::BuildComputePipeline(const ComputePipelineBuildInfo *pipelineIn
   ElfPackage candidateElf;
 
   if ((cacheEntryState == ShaderEntryState::Compiling) || (m_cache && (cacheResult != Result::Success))) {
-    unsigned forceLoopUnrollCount = cl::ForceLoopUnrollCount;
 
     ComputeContext computeContext(m_gfxIp, pipelineInfo, &pipelineHash, &cacheHash);
 
-    result = buildComputePipelineInternal(&computeContext, pipelineInfo, forceLoopUnrollCount, buildingRelocatableElf,
-                                          &candidateElf);
+    result = buildComputePipelineInternal(&computeContext, pipelineInfo, buildingRelocatableElf, &candidateElf);
 
     if (result == Result::Success) {
       elfBin.codeSize = candidateElf.size();

llpc/context/llpcCompiler.h
@@ -122,18 +122,16 @@ public:
                                       ComputePipelineBuildOut *pipelineOut, void *pipelineDumpFile = nullptr);
   Result buildGraphicsPipelineInternal(GraphicsContext *graphicsContext,
                                        llvm::ArrayRef<const PipelineShaderInfo *> shaderInfo,
-                                       unsigned forceLoopUnrollCount, bool buildingRelocatableElf,
-                                       ElfPackage *pipelineElf);
+                                       bool buildingRelocatableElf, ElfPackage *pipelineElf);
 
   Result buildComputePipelineInternal(ComputeContext *computeContext, const ComputePipelineBuildInfo *pipelineInfo,
-                                      unsigned forceLoopUnrollCount, bool buildingRelocatableElf,
-                                      ElfPackage *pipelineElf);
+                                      bool buildingRelocatableElf, ElfPackage *pipelineElf);
 
   Result buildPipelineWithRelocatableElf(Context *context, llvm::ArrayRef<const PipelineShaderInfo *> shaderInfo,
-                                         unsigned forceLoopUnrollCount, ElfPackage *pipelineElf);
+                                         ElfPackage *pipelineElf);
 
-  Result buildPipelineInternal(Context *context, llvm::ArrayRef<const PipelineShaderInfo *> shaderInfo,
-                               unsigned forceLoopUnrollCount, bool unlinked, ElfPackage *pipelineElf);
+  Result buildPipelineInternal(Context *context, llvm::ArrayRef<const PipelineShaderInfo *> shaderInfo, bool unlinked,
+                               ElfPackage *pipelineElf);
 
   // Gets the count of compiler instance.
   static unsigned getInstanceCount() { return m_instanceCount; }

llpc/context/llpcPipelineContext.cpp
@@ -99,6 +99,9 @@ static cl::opt<unsigned> ShadowDescTablePtrHigh("shadow-desc-table-ptr-high",
                                                 cl::desc("High part of VA for shadow descriptor table pointer"),
                                                 cl::init(2));
 
+// -force-loop-unroll-count: Force to set the loop unroll count.
+static cl::opt<int> ForceLoopUnrollCount("force-loop-unroll-count", cl::desc("Force loop unroll count"), cl::init(0));
+
 namespace Llpc {
 
 // =====================================================================================================================
@@ -380,6 +383,9 @@ void PipelineContext::setOptionsInPipeline(Pipeline *pipeline) const {
       shaderOptions.disableLicm = DisableLicm || shaderInfo->options.disableLicm;
       shaderOptions.updateDescInElf = shaderInfo->options.updateDescInElf;
       shaderOptions.unrollThreshold = shaderInfo->options.unrollThreshold;
+      // A non-zero command line -force-loop-unroll-count value overrides the shaderInfo option value.
+      shaderOptions.forceLoopUnrollCount =
+          ForceLoopUnrollCount ? ForceLoopUnrollCount : shaderInfo->options.forceLoopUnrollCount;
 
       static_assert(static_cast<lgc::DenormalMode>(Vkgc::DenormalMode::Auto) == lgc::DenormalMode::Auto, "Mismatch");
       static_assert(static_cast<lgc::DenormalMode>(Vkgc::DenormalMode::FlushToZero) == lgc::DenormalMode::FlushToZero,