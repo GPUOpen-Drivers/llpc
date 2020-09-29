GPUOpen-Drivers/llpc

commit 8cd0205db83b2e2c5cdbf710c2bd13bc436e91a3

Support packing input and output on tessellation
In the current framework of packing input and output on VS-FS, we add the support to pack input and ouput on pipeline VS-TCS-TES-(FS). Pack VS-TCS and TES-FS.
- The packing for VS-TCS is to reduce lds fragmentations by mapping all scalarized input calls to lds locations as continuious as possible. We execute addSpan and then fillInOutLocMap for TCS inputs as same as FS inputs. In PatchInOutImportExport, we use packed InOutLocationInfo to do patchVsGenericOutputExport and patchTcsGenericInputImport for VS-TCS.
- The packing for TES-FS is similar to VS-FS by re-assembling scalarized output calls in order to reduce the count of time-consuming exp instcutions.

Fixes: #523

lgc/patch/PatchInOutImportExport.cpp
@@ -446,10 +446,12 @@ void PatchInOutImportExport::visitCallInst(CallInst &callInst) {
             loc = resUsage->inOutUsage.perPatchInputLocMap[value];
           }
         } else {
-          if ((m_shaderStage == ShaderStageFragment) && m_pipelineState->isPackInOut()) {
+          if (m_pipelineState->isPackInOut() &&
+              (m_shaderStage == ShaderStageFragment || m_shaderStage == ShaderStageTessControl)) {
+            // The new InOutLocationInfo is used to map scalarized FS and TCS input import as compact as possible
             InOutLocationInfo origLocInfo = {};
-            origLocInfo.location = cast<ConstantInt>(callInst.getOperand(0))->getZExtValue();
-            const uint32_t elemIdxArgIdx = isInterpolantInputImport ? 2 : 1;
+            origLocInfo.location = value;
+            const uint32_t elemIdxArgIdx = isInterpolantInputImport || m_shaderStage != ShaderStageFragment ? 2 : 1;
             origLocInfo.component = cast<ConstantInt>(callInst.getOperand(elemIdxArgIdx))->getZExtValue();
             origLocInfo.half = false;
             assert(resUsage->inOutUsage.inOutLocMap.find(origLocInfo.u16All) != resUsage->inOutUsage.inOutLocMap.end());
@@ -471,8 +473,10 @@ void PatchInOutImportExport::visitCallInst(CallInst &callInst) {
       case ShaderStageTessControl: {
         assert(callInst.getNumArgOperands() == 4);
 
-        auto elemIdx = callInst.getOperand(2);
-        assert(isDontCareValue(elemIdx) == false);
+        if (!elemIdx) {
+          elemIdx = callInst.getOperand(2);
+          assert(isDontCareValue(elemIdx) == false);
+        }
 
         auto vertexIdx = callInst.getOperand(3);
         assert(isDontCareValue(vertexIdx) == false);
@@ -694,6 +698,7 @@ void PatchInOutImportExport::visitCallInst(CallInst &callInst) {
       bool exist = false;
       unsigned loc = InvalidValue;
       Value *locOffset = nullptr;
+      unsigned elemIdx = InvalidValue;
 
       if (m_shaderStage == ShaderStageTessControl) {
         // NOTE: If location offset is a constant, we have to add it to the unmapped location before querying
@@ -729,7 +734,23 @@ void PatchInOutImportExport::visitCallInst(CallInst &callInst) {
           loc = resUsage->inOutUsage.outputLocMap[outLocInfo.u32All];
         }
       } else {
-        if (resUsage->inOutUsage.outputLocMap.find(value) != resUsage->inOutUsage.outputLocMap.end()) {
+        if (m_pipelineState->isPackInOut() && m_shaderStage == ShaderStageVertex &&
+            m_pipelineState->hasShaderStage(ShaderStageTessControl)) {
+          // The new InOutLocationInfo is used to map scalarized VS output export in a VS-TCS-TES-FS to lds as compact
+          // as possible
+          InOutLocationInfo origLocInfo = {};
+          origLocInfo.location = value;
+          origLocInfo.component = cast<ConstantInt>(callInst.getOperand(1))->getZExtValue();
+          origLocInfo.half = false;
+          if (resUsage->inOutUsage.inOutLocMap.find(origLocInfo.u16All) != resUsage->inOutUsage.inOutLocMap.end()) {
+            InOutLocationInfo newLocInfo = {};
+            newLocInfo.u16All = resUsage->inOutUsage.inOutLocMap[origLocInfo.u16All];
+            loc = newLocInfo.location;
+            elemIdx = newLocInfo.component;
+            exist = true;
+          } else
+            exist = false;
+        } else if (resUsage->inOutUsage.outputLocMap.find(value) != resUsage->inOutUsage.outputLocMap.end()) {
           exist = true;
           loc = resUsage->inOutUsage.outputLocMap[value];
         }
@@ -742,8 +763,9 @@ void PatchInOutImportExport::visitCallInst(CallInst &callInst) {
         switch (m_shaderStage) {
         case ShaderStageVertex: {
           assert(callInst.getNumArgOperands() == 3);
-          const unsigned compIdx = cast<ConstantInt>(callInst.getOperand(1))->getZExtValue();
-          patchVsGenericOutputExport(output, loc, compIdx, &callInst);
+          if (elemIdx == InvalidValue)
+            elemIdx = cast<ConstantInt>(callInst.getOperand(1))->getZExtValue();
+          patchVsGenericOutputExport(output, loc, elemIdx, &callInst);
           break;
         }
         case ShaderStageTessControl: {
@@ -759,8 +781,9 @@ void PatchInOutImportExport::visitCallInst(CallInst &callInst) {
         }
         case ShaderStageTessEval: {
           assert(callInst.getNumArgOperands() == 3);
-          const unsigned compIdx = cast<ConstantInt>(callInst.getOperand(1))->getZExtValue();
-          patchTesGenericOutputExport(output, loc, compIdx, &callInst);
+          if (elemIdx == InvalidValue)
+            elemIdx = cast<ConstantInt>(callInst.getOperand(1))->getZExtValue();
+          patchTesGenericOutputExport(output, loc, elemIdx, &callInst);
           break;
         }
         case ShaderStageGeometry: {

lgc/state/PipelineState.cpp
@@ -988,8 +988,15 @@ bool PipelineState::isTessOffChip() {
 bool PipelineState::isPackInOut() {
   // Pack input/output requirements:
   // 1) -pack-in-out option is on
-  // 2) It is a VS-FS pipeline
-  return PackInOut && (m_stageMask == (shaderStageToMask(ShaderStageVertex) | shaderStageToMask(ShaderStageFragment)));
+  // 2) It supports VS-FS, VS-TCS-TES-(FS)
+  if (!PackInOut)
+    return false;
+
+  if (hasShaderStage(ShaderStageVertex) && !hasShaderStage(ShaderStageGeometry)) {
+    const unsigned nextStage = getNextShaderStage(ShaderStageVertex);
+    return nextStage == ShaderStageFragment || nextStage == ShaderStageTessControl;
+  }
+  return false;
 }
 
 // =====================================================================================================================

lgc/patch/PatchResourceCollect.h
@@ -142,8 +142,8 @@ class InOutLocationMapManager {
 public:
   InOutLocationMapManager() {}
 
-  void addSpan(llvm::CallInst *call);
-  void buildLocationMap();
+  void addSpan(llvm::CallInst *call, ShaderStage shaderStage);
+  void buildLocationMap(bool checkCompatibility);
 
   bool findMap(const InOutLocation &originalLocation, const InOutLocation *&newLocation);
 

llpc/test/shaderdb/PipelineGsTess_TestInOutPacking.pipe.avoid
@@ -1,0 +1,182 @@
+; BEGIN_SHADERTEST
+; RUN: amdllpc -spvgen-dir=%spvgendir% -v %gfxip %s | FileCheck -check-prefix=SHADERTEST %s
+; SHADERTEST-LABEL: {{^// LLPC}} pipeline patching results
+; SHADERTEST: shl i32 %3, 3
+; SHADERTEST: or i32 %{{[0-9]*}}, 4
+; SHADERTEST: getelementptr [16384 x i32], [16384 x i32] addrspace(3)* @lds, i32 0, i32 %{{[0-9]*}}
+; SHADERTEST: shl nuw nsw i32 %{{[0-9]*}}, 3
+; SHADERTEST: or i32 %{{[0-9]*}}, 2
+; SHADERTEST: getelementptr [16384 x i32], [16384 x i32] addrspace(3)* @lds, i32 0, i32 %{{[0-9]*}}
+; SHADERTEST: add nuw nsw i32 %{{[0-9]*}}, 384
+; SHADERTEST: AMDLLPC SUCCESS
+; END_SHADERTEST
+
+[Version]
+version = 40
+
+[VsGlsl]
+#version 450 core
+
+layout(location = 0) in vec4 inV0;
+layout(location = 1) in vec2 inV1;
+layout(location = 2) in vec2 inV2;
+
+layout(location = 0) out vec2 out1;
+layout(location = 1) out vec2 out2;
+
+void main(void)
+{
+    gl_Position = inV0;
+    out1 = inV1;
+    out2 = inV2;
+}
+
+[VsInfo]
+entryPoint = main
+
+
+[TcsGlsl]
+#version 450 core
+layout(vertices=3) out;
+layout(location = 0) in vec2 inArray1[];
+layout(location = 1) in vec2 inArray2[];
+
+layout(location = 0) out vec4 outArray[];
+
+void main(void)
+{
+    gl_TessLevelOuter[0] = 2.0;
+    gl_TessLevelOuter[1] = 2.0;
+    gl_TessLevelOuter[2] = 2.0;
+    gl_TessLevelInner[0] = 4.0;
+
+    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
+    outArray[gl_InvocationID] = vec4(inArray2[gl_InvocationID].xy, inArray1[gl_InvocationID].xy);
+}
+[TcsInfo]
+entryPoint = main
+
+[TesGlsl]
+#version 450 core
+
+layout(triangles,fractional_even_spacing,ccw) in;
+
+layout(location = 0) in vec4 inArray[];
+
+layout(location = 0) out vec4 color;
+
+void main(void)
+{
+    float u = gl_TessCoord[0];
+    float v = gl_TessCoord[1];
+    float w = gl_TessCoord[2];
+
+    gl_Position = inArray[0] * u + inArray[1] * v + inArray[2] * w;
+    color = (inArray[0] + inArray[1] + inArray[2]) / 3.0;
+}
+
+[TesInfo]
+entryPoint = main
+
+[GsGlsl]
+#version 450 core
+layout(triangles) in;
+layout(triangle_strip, max_vertices = 16) out;
+layout(location = 0) in vec4 in_vtxColor[];
+layout(location = 0) out vec4 vtxColor;
+out gl_PerVertex { vec4 gl_Position; float gl_PointSize; };
+in gl_PerVertex { vec4 gl_Position; float gl_PointSize; } gl_in[];
+void main()
+{
+    for (uint i = 0; i < gl_in.length(); ++i)
+    {
+        gl_Position = gl_in[i].gl_Position;
+        vtxColor = in_vtxColor[i];
+        EmitVertex();
+    }
+    EndPrimitive();
+}
+
+
+[GsInfo]
+entryPoint = main
+
+
+[FsGlsl]
+#version 450 core
+
+layout(location = 0) in vec4 inColor;
+
+layout(location = 0) out vec4 fragColor;
+
+void main(void)
+{
+    fragColor =  inColor;
+}
+
+[FsInfo]
+entryPoint = main
+
+[GraphicsPipelineState]
+topology = VK_PRIMITIVE_TOPOLOGY_PATCH_LIST
+patchControlPoints = 3
+deviceIndex = 0
+disableVertexReuse = 0
+switchWinding = 0
+enableMultiView = 0
+depthClipEnable = 1
+rasterizerDiscardEnable = 0
+perSampleShading = 0
+numSamples = 1
+samplePatternIdx = 0
+usrClipPlaneMask = 0
+polygonMode = VK_POLYGON_MODE_FILL
+cullMode = VK_CULL_MODE_NONE
+frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE
+depthBiasEnable = 0
+alphaToCoverageEnable = 0
+dualSourceBlendEnable = 0
+colorBuffer[0].format = VK_FORMAT_R8G8B8A8_UNORM
+colorBuffer[0].channelWriteMask = 15
+colorBuffer[0].blendEnable = 0
+colorBuffer[0].blendSrcAlphaToColor = 1
+nggState.enableNgg = 0
+nggState.enableGsUse = 0
+nggState.forceNonPassthrough = 0
+nggState.alwaysUsePrimShaderTable = 0
+nggState.compactMode = NggCompactDisable
+nggState.enableFastLaunch = 0
+nggState.enableVertexReuse = 0
+nggState.enableBackfaceCulling = 0
+nggState.enableFrustumCulling = 0
+nggState.enableBoxFilterCulling = 0
+nggState.enableSphereCulling = 0
+nggState.enableSmallPrimFilter = 0
+nggState.enableCullDistanceCulling = 0
+nggState.backfaceExponent = 0
+nggState.subgroupSizing = Auto
+nggState.primsPerSubgroup = 0
+nggState.vertsPerSubgroup = 0
+options.includeDisassembly = 0
+options.scalarBlockLayout = 1
+options.includeIr = 0
+options.robustBufferAccess = 0
+options.reconfigWorkgroupLayout = 0
+
+
+[VertexInputState]
+binding[0].binding = 0
+binding[0].stride = 16
+binding[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+attribute[0].location = 0
+attribute[0].binding = 0
+attribute[0].format = VK_FORMAT_R32G32B32A32_SFLOAT
+attribute[0].offset = 0
+attribute[1].location = 1
+attribute[1].binding = 0
+attribute[1].format = VK_FORMAT_R32G32_SFLOAT
+attribute[1].offset = 0
+attribute[2].location = 2
+attribute[2].binding = 0
+attribute[2].format = VK_FORMAT_R32G32_SFLOAT
+attribute[2].offset = 0

llpc/test/shaderdb/PipelineTess_TestInOutPacking.pipe
@@ -1,0 +1,177 @@
+; BEGIN_SHADERTEST
+; RUN: amdllpc -spvgen-dir=%spvgendir% -v %gfxip %s | FileCheck -check-prefix=SHADERTEST %s
+; SHADERTEST-LABEL: {{^// LLPC}} pipeline patching results
+; SHADERTEST: shl i32 %3, 3
+; SHADERTEST: or i32 %{{[0-9]*}}, 4
+; SHADERTEST: getelementptr [16384 x i32], [16384 x i32] addrspace(3)* @lds, i32 0, i32 %{{[0-9]*}}
+; SHADERTEST: shl nuw nsw i32 %{{[0-9]*}}, 3
+; SHADERTEST: or i32 %{{[0-9]*}}, 2
+; SHADERTEST: getelementptr [16384 x i32], [16384 x i32] addrspace(3)* @lds, i32 0, i32 %{{[0-9]*}}
+; SHADERTEST: add nuw nsw i32 %{{[0-9]*}}, 384
+; SHADERTEST: call void @llvm.amdgcn.exp.f32(i32 immarg 32, i32 immarg 15, float %{{[0-9]*}}, float %{{[0-9]*}}, float %{{[0-9]*}}, float %{{[0-9]*}}, i1 immarg false, i1 immarg false)
+; SHADERTEST: call void @llvm.amdgcn.exp.f32(i32 immarg 33, i32 immarg 3, float %{{[0-9]*}}, float %{{[0-9]*}}, float undef, float undef, i1 immarg false, i1 immarg false)
+; SHADERTEST: call float @llvm.amdgcn.interp.p1(float %{{[0-9]*}}, i32 immarg 1, i32 immarg 1, i32 %2)
+; SHADERTEST: call float @llvm.amdgcn.interp.p1(float %{{[0-9]*}}, i32 immarg 2, i32 immarg 0, i32 %2)
+; SHADERTEST: call float @llvm.amdgcn.interp.p1(float %{{[0-9]*}}, i32 immarg 3, i32 immarg 0, i32 %2)
+; SHADERTEST: call float @llvm.amdgcn.interp.p1(float %{{[0-9]*}}, i32 immarg 0, i32 immarg 1, i32 %2)
+; SHADERTEST: call float @llvm.amdgcn.interp.p1(float %{{[0-9]*}}, i32 immarg 0, i32 immarg 0, i32 %2)
+; SHADERTEST: call float @llvm.amdgcn.interp.p1(float %{{[0-9]*}}, i32 immarg 1, i32 immarg 0, i32 %2)
+; SHADERTEST: AMDLLPC SUCCESS
+; END_SHADERTEST
+
+[Version]
+version = 40
+
+[VsGlsl]
+#version 450 core
+
+layout(location = 0) in vec4 inV0;
+layout(location = 1) in vec2 inV1;
+layout(location = 2) in vec2 inV2;
+
+layout(location = 0) out vec2 out1;
+layout(location = 1) out vec2 out2;
+
+void main(void)
+{
+    gl_Position = inV0;
+    out1 = inV1;
+    out2 = inV2;
+}
+
+[VsInfo]
+entryPoint = main
+
+
+[TcsGlsl]
+#version 450 core
+layout(vertices=3) out;
+layout(location = 0) in vec2 inArray1[];
+layout(location = 1) in vec2 inArray2[];
+
+layout(location = 0) out vec4 outArray[];
+
+void main(void)
+{
+    gl_TessLevelOuter[0] = 2.0;
+    gl_TessLevelOuter[1] = 2.0;
+    gl_TessLevelOuter[2] = 2.0;
+    gl_TessLevelInner[0] = 4.0;
+
+    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
+    outArray[gl_InvocationID] = vec4(inArray2[gl_InvocationID].xy, inArray1[gl_InvocationID].xy);
+}
+[TcsInfo]
+entryPoint = main
+
+[TesGlsl]
+#version 450 core
+
+layout(triangles,fractional_even_spacing,ccw) in;
+
+layout(location = 0) in vec4 inArray[];
+
+layout(location = 0) out vec2 oVf2;
+layout(location = 1) out vec3 oVf3;
+layout(location = 2) out float of;
+void main(void)
+{
+    float u = gl_TessCoord[0];
+    float v = gl_TessCoord[1];
+    float w = gl_TessCoord[2];
+
+    gl_Position = inArray[0] * u + inArray[1] * v + inArray[2] * w;
+    oVf2 = inArray[0].xy;
+    oVf3 = inArray[1].xyz;
+    of = inArray[2].w;
+}
+
+[TesInfo]
+entryPoint = main
+
+
+[FsGlsl]
+#version 450 core
+
+layout(location = 0) in vec2 inVf2;
+layout(location = 1) in vec3 inVf3;
+layout(location = 2) in float inf;
+
+layout(location = 0) out vec4 fragColor;
+
+void main(void)
+{
+    fragColor =  vec4(inVf2.xy + inVf3.xy, inVf3.z, inf);
+}
+
+[FsInfo]
+entryPoint = main
+
+[GraphicsPipelineState]
+topology = VK_PRIMITIVE_TOPOLOGY_PATCH_LIST
+patchControlPoints = 3
+deviceIndex = 0
+disableVertexReuse = 0
+switchWinding = 0
+enableMultiView = 0
+depthClipEnable = 1
+rasterizerDiscardEnable = 0
+perSampleShading = 0
+numSamples = 1
+samplePatternIdx = 0
+usrClipPlaneMask = 0
+polygonMode = VK_POLYGON_MODE_FILL
+cullMode = VK_CULL_MODE_NONE
+frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE
+depthBiasEnable = 0
+alphaToCoverageEnable = 0
+dualSourceBlendEnable = 0
+colorBuffer[0].format = VK_FORMAT_R8G8B8A8_UNORM
+colorBuffer[0].channelWriteMask = 15
+colorBuffer[0].blendEnable = 0
+colorBuffer[0].blendSrcAlphaToColor = 1
+nggState.enableNgg = 0
+nggState.enableGsUse = 0
+nggState.forceNonPassthrough = 0
+nggState.alwaysUsePrimShaderTable = 0
+nggState.compactMode = NggCompactDisable
+nggState.enableFastLaunch = 0
+nggState.enableVertexReuse = 0
+nggState.enableBackfaceCulling = 0
+nggState.enableFrustumCulling = 0
+nggState.enableBoxFilterCulling = 0
+nggState.enableSphereCulling = 0
+nggState.enableSmallPrimFilter = 0
+nggState.enableCullDistanceCulling = 0
+nggState.backfaceExponent = 0
+nggState.subgroupSizing = Auto
+nggState.primsPerSubgroup = 0
+nggState.vertsPerSubgroup = 0
+options.includeDisassembly = 0
+options.scalarBlockLayout = 1
+options.includeIr = 0
+options.robustBufferAccess = 0
+options.reconfigWorkgroupLayout = 0
+options.shadowDescriptorTableUsage = Enable
+options.shadowDescriptorTablePtrHigh = 2
+options.extendedRobustness.robustBufferAccess = 0
+options.extendedRobustness.robustImageAccess = 0
+options.extendedRobustness.nullDescriptor = 0
+
+
+[VertexInputState]
+binding[0].binding = 0
+binding[0].stride = 16
+binding[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+attribute[0].location = 0
+attribute[0].binding = 0
+attribute[0].format = VK_FORMAT_R32G32B32A32_SFLOAT
+attribute[0].offset = 0
+attribute[1].location = 1
+attribute[1].binding = 0
+attribute[1].format = VK_FORMAT_R32G32_SFLOAT
+attribute[1].offset = 0
+attribute[2].location = 2
+attribute[2].binding = 0
+attribute[2].format = VK_FORMAT_R32G32_SFLOAT
+attribute[2].offset = 0

commit 922cd721d8bfed671cd7c3d2c8139f8ce7611ad5

Disable packing input/output for dynamic component index
Commit 8cd0205 caused a regression in tessellation in the Sascha WIllems
tesselation example. The root cause is not taking dynamic component
index into consideration. This change is a wordround for this case by
totaly disabling input/output packing. A proper fix should be a partial
packing scheme that is non-packable locations (with dynamic location
offset or component index) should be skipped and placed at the back of
packable locations with consecitve location index.
Considering a future required change of refactoring the location maps, I
think it' better to integrate the partial packing in that change.

lgc/builder/BuilderReplayer.cpp
@@ -101,6 +101,7 @@ bool BuilderReplayer::runOnModule(Module &module) {
   // Set up the pipeline state from the specified linked IR module.
   PipelineState *pipelineState = getAnalysis<PipelineStateWrapper>().getPipelineState(&module);
   pipelineState->readState(&module);
+  pipelineState->initializePackInOut();
 
   // Create the BuilderImpl to replay into, passing it the PipelineState
   LgcContext *builderContext = pipelineState->getLgcContext();

lgc/include/lgc/state/PipelineState.h
@@ -257,8 +257,14 @@ public:
   // ring is on-chip.
   bool isGsOnChip() const { return m_gsOnChip; }
 
-  // Determine whether to use input/output packing
-  bool isPackInOut();
+  // Initialize the state of input/output packing
+  void initializePackInOut();
+
+  // Set the state of input/output packing
+  void setPackInOut(bool isPack) { m_packInOut = isPack; }
+
+  // Check whether input/output packing can be used
+  bool canPackInOut() const { return m_packInOut; }
 
   // Gets wave size for the specified shader stage
   unsigned getShaderWaveSize(ShaderStage stage);
@@ -419,6 +425,7 @@ private:
   // Cached MDString for each resource node type
 
   bool m_gsOnChip = false;                                                     // Whether to use GS on-chip mode
+  bool m_packInOut = false;                                                    // Whether to use packing on input/output
   NggControl m_nggControl = {};                                                // NGG control settings
   ShaderModes m_shaderModes;                                                   // Shader modes for this pipeline
   unsigned m_deviceIndex = 0;                                                  // Device index

lgc/patch/PatchInOutImportExport.cpp
@@ -446,7 +446,7 @@ void PatchInOutImportExport::visitCallInst(CallInst &callInst) {
             loc = resUsage->inOutUsage.perPatchInputLocMap[value];
           }
         } else {
-          if (m_pipelineState->isPackInOut() &&
+          if (m_pipelineState->canPackInOut() &&
               (m_shaderStage == ShaderStageFragment || m_shaderStage == ShaderStageTessControl)) {
             // The new InOutLocationInfo is used to map scalarized FS and TCS input import as compact as possible
             InOutLocationInfo origLocInfo = {};
@@ -734,7 +734,7 @@ void PatchInOutImportExport::visitCallInst(CallInst &callInst) {
           loc = resUsage->inOutUsage.outputLocMap[outLocInfo.u32All];
         }
       } else {
-        if (m_pipelineState->isPackInOut() && m_shaderStage == ShaderStageVertex &&
+        if (m_pipelineState->canPackInOut() && m_shaderStage == ShaderStageVertex &&
             m_pipelineState->hasShaderStage(ShaderStageTessControl)) {
           // The new InOutLocationInfo is used to map scalarized VS output export in a VS-TCS-TES-FS to lds as compact
           // as possible

lgc/patch/PatchResourceCollect.cpp
@@ -81,7 +81,7 @@ bool PatchResourceCollect::runOnModule(Module &module) {
   m_pipelineState = getAnalysis<PipelineStateWrapper>().getPipelineState(&module);
 
   // If packing {VS, TES} outputs and {TCS, FS} inputs, scalarize those outputs and inputs now.
-  if (m_pipelineState->isPackInOut())
+  if (m_pipelineState->canPackInOut())
     scalarizeForInOutPacking(&module);
 
   // Process each shader stage, in reverse order.
@@ -1289,7 +1289,7 @@ void PatchResourceCollect::visitCallInst(CallInst &callInst) {
     }
   }
 
-  if (m_pipelineState->isPackInOut()) {
+  if (m_pipelineState->canPackInOut()) {
     // Process input import calls with constant location offset in FS (VS-FS, TES-FS) or TCS (VS-TCS)
     // Collect output export calls to re-assemble in VS (VS-FS) or TES (TES-FS)
     const bool isPackIn = m_shaderStage == ShaderStageFragment || m_shaderStage == ShaderStageTessControl;
@@ -1683,7 +1683,7 @@ void PatchResourceCollect::matchGenericInOut() {
     }
   }
 
-  if (m_pipelineState->isPackInOut()) {
+  if (m_pipelineState->canPackInOut()) {
     // Do packing input/output
     packInOutLocation();
   }
@@ -2763,13 +2763,22 @@ void PatchResourceCollect::scalarizeForInOutPacking(Module *module) {
   SmallVector<CallInst *, 4> outputCalls;
   SmallVector<CallInst *, 4> inputCalls;
   for (Function &func : *module) {
-    if (func.getName().startswith(lgcName::InputImportGeneric) ||
-        func.getName().startswith(lgcName::InputImportInterpolant)) {
+    if (!m_pipelineState->canPackInOut())
+      break;
+    const bool isInterpolant = func.getName().startswith(lgcName::InputImportInterpolant);
+    if (func.getName().startswith(lgcName::InputImportGeneric) || isInterpolant) {
       // This is a generic (possibly interpolated) input. Find its uses in FS (VS-FS, TES-FS) or TCS.
       for (User *user : func.users()) {
         auto call = cast<CallInst>(user);
         ShaderStage shaderStage = m_pipelineShaders->getShaderStage(call->getFunction());
         if (shaderStage == ShaderStageFragment || shaderStage == ShaderStageTessControl) {
+          // This is a workaround to disable pack for the pipeline if there exists dynamic component index
+          // TODO: Do partial packing except calls with dynamic component index in future change
+          const unsigned elemArgIdx = (shaderStage == ShaderStageFragment && !isInterpolant) ? 1 : 2;
+          if (!isa<ConstantInt>(call->getOperand(elemArgIdx))) {
+            m_pipelineState->setPackInOut(false);
+            break;
+          }
           // We have a use in FS (VS-FS, TES-FS) or TCS. See if it needs scalarizing.
           if (isa<VectorType>(call->getType()) || call->getType()->getPrimitiveSizeInBits() == 64)
             inputCalls.push_back(call);
@@ -2783,6 +2792,7 @@ void PatchResourceCollect::scalarizeForInOutPacking(Module *module) {
         if (shaderStage == ShaderStageTessEval || shaderStage == ShaderStageVertex) {
           // We have a use in the last vertex processing stage. See if it needs scalarizing. The output value is
           // always the final argument.
+          assert(isa<ConstantInt>(call->getOperand(1)));
           Type *valueTy = call->getArgOperand(call->getNumArgOperands() - 1)->getType();
           if (isa<VectorType>(valueTy) || valueTy->getPrimitiveSizeInBits() == 64)
             outputCalls.push_back(call);
@@ -2790,12 +2800,13 @@ void PatchResourceCollect::scalarizeForInOutPacking(Module *module) {
       }
     }
   }
-
-  // Scalarize the gathered inputs and outputs.
-  for (CallInst *call : inputCalls)
-    scalarizeGenericInput(call);
-  for (CallInst *call : outputCalls)
-    scalarizeGenericOutput(call);
+  if (m_pipelineState->canPackInOut()) {
+    // Scalarize the gathered inputs and outputs.
+    for (CallInst *call : inputCalls)
+      scalarizeGenericInput(call);
+    for (CallInst *call : outputCalls)
+      scalarizeGenericOutput(call);
+  }
 }
 
 // =====================================================================================================================

lgc/state/PipelineState.cpp
@@ -994,19 +994,15 @@ bool PipelineState::isTessOffChip() {
 }
 
 // =====================================================================================================================
-// Determine whether to use input/output packing
-bool PipelineState::isPackInOut() {
+// Initialize the state of input/output packing
+void PipelineState::initializePackInOut() {
   // Pack input/output requirements:
   // 1) -pack-in-out option is on
   // 2) It supports VS-FS, VS-TCS-TES-(FS)
-  if (!PackInOut)
-    return false;
-
-  if (hasShaderStage(ShaderStageVertex) && !hasShaderStage(ShaderStageGeometry)) {
+  if (PackInOut && hasShaderStage(ShaderStageVertex) && !hasShaderStage(ShaderStageGeometry)) {
     const unsigned nextStage = getNextShaderStage(ShaderStageVertex);
-    return nextStage == ShaderStageFragment || nextStage == ShaderStageTessControl;
+    m_packInOut = nextStage == ShaderStageFragment || nextStage == ShaderStageTessControl;
   }
-  return false;
 }
 
 // =====================================================================================================================