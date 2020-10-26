f9adca3ff731507df3ac48125bada32706a02306
David Stuttard <david.stuttard@amd.com>
2020-10-26 14:27:17 +0000

    Move image descriptor workaround to a later workaround patch pass
    Adding the image descriptor workaround (gfx10.1) in the builder code meant that
    it introduced extra code that prevented other optimizations.
    
    Move this to a later patching pass (that could contain future workaround
    patching too).
    
    Note: this is tested by lgc/test/PatchInvalidImageDescriptor.lgc lit test

---
+++ lgc/patch/PatchWorkarounds.cpp
@@ -0,0 +1,186 @@
+/*
+ ***********************************************************************************************************************
+ *
+ *  Copyright (c) 2019-2020 Advanced Micro Devices, Inc. All Rights Reserved.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ **********************************************************************************************************************/
+/**
+ ***********************************************************************************************************************
+ * @file  PatchWorkarounds.cpp
+ * @brief LLPC source file: contains implementation of class lgc::PatchWorkarounds.
+ ***********************************************************************************************************************
+ */
+
+#include "PatchWorkarounds.h"
+#include "lgc/BuilderBase.h"
+#include "lgc/state/PipelineShaders.h"
+#include "lgc/state/PipelineState.h"
+#include "lgc/state/TargetInfo.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicsAMDGPU.h"
+#include "llvm/Support/Debug.h"
+
+#define DEBUG_TYPE "lgc-patch-workarounds"
+
+using namespace lgc;
+using namespace llvm;
+
+namespace lgc {
+
+// =====================================================================================================================
+// Define static members (no initializer needed as LLVM only cares about the address of ID, never its value).
+char PatchWorkarounds::ID;
+
+// =====================================================================================================================
+// Pass creator, creates the pass of LLVM patching operations for peephole optimizations.
+ModulePass *createPatchWorkarounds() {
+  return new PatchWorkarounds();
+}
+
+PatchWorkarounds::PatchWorkarounds() : Patch(ID) {
+}
+
+// =====================================================================================================================
+// Executes this LLVM pass on the specified LLVM function.
+//
+// @param [in/out] module : Module that we will add workarounds in.
+bool PatchWorkarounds::runOnModule(Module &module) {
+  LLVM_DEBUG(dbgs() << "Run the pass Patch-Workarounds\n");
+
+  Patch::init(&module);
+
+  m_pipelineState = getAnalysis<PipelineStateWrapper>().getPipelineState(&module);
+  m_builder = std::make_unique<IRBuilder<>>(*m_context);
+  m_processed.clear();
+
+  m_changed = false;
+
+  // Patch image resource descriptor when app provides wrong type
+  applyImageDescWorkaround();
+
+  return m_changed;
+}
+
+// =====================================================================================================================
+// Apply resource descriptor workaround where application is erroneously providing a buffer descriptor when it should
+// be an image descriptor. We only have to apply the workaround for gfx10.1 (note: this is an application error that
+// are handling gracefully)
+//
+void PatchWorkarounds::applyImageDescWorkaround(void) {
+  if (!m_pipelineState->getOptions().disableImageResourceCheck &&
+      m_pipelineState->getTargetInfo().getGpuWorkarounds().gfx10.waFixBadImageDescriptor) {
+
+    for (const Function &func : m_module->getFunctionList()) {
+      if (func.getName().startswith("llvm.amdgcn.image")) {
+        // Build up a list of uses (since we modify we can't process them immediately)
+        SmallVector<CallInst *, 8> useWorkList;
+        for (auto &use : func.uses()) {
+          if (auto *callInst = dyn_cast<CallInst>(use.getUser())) {
+            if (callInst->isCallee(&use))
+              useWorkList.push_back(callInst);
+          }
+        }
+        // Process the uses
+        for (auto imgInst : useWorkList) {
+          processImageDescWorkaround(*imgInst);
+        }
+      }
+    }
+  }
+}
+
+// =====================================================================================================================
+// Process calls to image instrinsics and apply buffer descriptor should be image descriptor workaround
+//
+// @param callInst : The image intrinsic call instruction
+void PatchWorkarounds::processImageDescWorkaround(CallInst &callInst) {
+  Function *const calledFunc = callInst.getCalledFunction();
+  if (!calledFunc)
+    return;
+
+  // A buffer descriptor may be incorrectly given when it should be an image descriptor, we need to fix it to valid
+  // buffer type (0) to make hardware happily ignore it. This is to check and fix against buggy applications which
+  // declares a image descriptor in shader but provide a buffer descriptor in driver. Note this only applies to gfx10.1.
+  // Look for 8 dword resource descriptor argument
+  for (Value *arg : callInst.args()) {
+    if (auto vecTy = dyn_cast<FixedVectorType>(arg->getType())) {
+      if (vecTy->getNumElements() == 8 && vecTy->getElementType()->isIntegerTy(32)) {
+        if (isa<UndefValue>(arg))
+          // We don't need to worry if the value is actually undef. This situation only really occurs in unit test
+          // but either way, it is pointless to apply the workaround to an undef.
+          break;
+
+        if (m_processed.count(arg) > 0)
+          // Already processed this one
+          break;
+
+        Instruction *rsrcInstr = cast<Instruction>(arg);
+
+        m_builder->SetInsertPoint(rsrcInstr->getNextNode());
+        m_builder->SetCurrentDebugLocation(rsrcInstr->getNextNode()->getDebugLoc());
+
+        // Create a new rsrc load instruction - we apply the workaround the the new instruction and then replace
+        // all uses of the old one with the derived value. This prevents us replacing the original value with the
+        // derived one in the code to insert the new element.
+        Instruction *newInstr = rsrcInstr->clone();
+        newInstr->insertAfter(rsrcInstr);
+
+        Value *elem3 = m_builder->CreateExtractElement(newInstr, 3);
+        Value *isInvalid = m_builder->CreateICmpSGE(elem3, m_builder->getInt32(0));
+        Value *masked = m_builder->CreateAnd(elem3, m_builder->getInt32(0x0FFFFFFF));
+        elem3 = m_builder->CreateSelect(isInvalid, masked, elem3);
+
+        // Re-assemble descriptor
+        Value *newArg = m_builder->CreateInsertElement(newInstr, elem3, 3);
+
+        auto rsrcInstrName = rsrcInstr->getName();
+        newInstr->setName(rsrcInstrName); // Preserve the old name for the load
+        newArg->setName(rsrcInstrName);   // Derive a new name based on the old name for the load
+        rsrcInstr->replaceAllUsesWith(newArg);
+        rsrcInstr->eraseFromParent();
+
+        // Record the new argument as already processed. If we encounter it in
+        // another image intrinsic call we can skip
+        m_processed.insert(newArg);
+
+        m_changed = true;
+        break;
+      }
+    }
+  }
+}
+
+// =====================================================================================================================
+// Get the analysis usage of this pass.
+//
+// @param [out] analysisUsage : The analysis usage.
+void PatchWorkarounds::getAnalysisUsage(AnalysisUsage &analysisUsage) const {
+  analysisUsage.addRequired<PipelineStateWrapper>();
+  analysisUsage.addRequired<PipelineShaders>();
+  analysisUsage.addPreserved<PipelineShaders>();
+}
+
+} // namespace lgc
+
+// =====================================================================================================================
+// Initializes the pass of LLVM patching opertions for workarounds
+INITIALIZE_PASS(PatchWorkarounds, DEBUG_TYPE, "Patch LLVM for workarounds", false, false)
---
+++ lgc/patch/PatchWorkarounds.h
@@ -0,0 +1,71 @@
+/*
+ ***********************************************************************************************************************
+ *
+ *  Copyright (c) 2019-2020 Advanced Micro Devices, Inc. All Rights Reserved.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ **********************************************************************************************************************/
+/**
+ ***********************************************************************************************************************
+ * @file  PatchWorkarounds.h
+ * @brief LLPC header file: contains declaration of class lgc::PatchWorkarounds.
+ ***********************************************************************************************************************
+ */
+#pragma once
+
+#include "lgc/patch/Patch.h"
+#include "lgc/util/Internal.h"
+#include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Value.h"
+#include "llvm/Pass.h"
+
+namespace lgc {
+
+// =====================================================================================================================
+// Represents the pass of LLVM patching operations for applying workarounds:
+//
+// - fix up issues when buffer descriptor is incorrectly given when it should be an image descriptor. Some architectures
+//   require a fix so the hardware will ignore this difference (actually an app error, but common enough to require
+//   handling)
+//
+class PatchWorkarounds final : public Patch {
+public:
+  PatchWorkarounds();
+
+  bool runOnModule(llvm::Module &module) override;
+
+  void getAnalysisUsage(llvm::AnalysisUsage &analysisUsage) const override;
+
+  static char ID; // ID of this pass
+
+private:
+  std::unique_ptr<llvm::IRBuilder<>> m_builder; // The IRBuilder.
+  PipelineState *m_pipelineState;               // The pipeline state
+
+  llvm::SmallPtrSet<llvm::Value *, 8> m_processed; // Track rsrc-desc args already processed
+
+  bool m_changed;
+
+  void applyImageDescWorkaround(void);
+  void processImageDescWorkaround(llvm::CallInst &callInst);
+};
+
+} // namespace lgc
