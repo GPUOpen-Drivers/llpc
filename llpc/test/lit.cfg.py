b07eeaaaad390eb5469490db6f67d22c929ecc7a
Sebastian Neubauer <Sebastian.Neubauer@amd.com>
2021-07-23 13:30:59 +0200

    Fix amdllpc lit tests on Windows
    - Python3 needs to be defined
    - Using bash does not work well on Windows

--- llpc/test/lit.cfg.py
+++ llpc/test/lit.cfg.py
@@ -36,7 +36,7 @@ from lit.llvm.subst import FindTool
 config.name = 'LLPC_SHADERTEST'
 
 # testFormat: The test format to use to interpret tests.
-config.test_format = lit.formats.ShTest(True)
+config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 
 # suffixes: A list of file extensions to treat as test files.
 config.suffixes = ['.vert', '.tesc', '.tese', '.geom', '.frag', '.comp', '.spvasm', '.pipe', '.ll']
