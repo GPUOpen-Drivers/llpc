GPUOpen-Drivers/llpc

commit 0c8ceddf9d78f112cb462a4338a474ef58f27e09

Support VK_EXT_shader_atomic_float2
 - Add three SPIRV instructions: OpAtomicFMinEXT,OpAtomicFMaxEXT,OpAtomicFAddEXT.

 - Work around the atomic swap by bitcasting.

lgc/patch/PatchBufferOp.cpp
@@ -245,120 +245,189 @@ void PatchBufferOp::visitAtomicCmpXchgInst(AtomicCmpXchgInst &atomicCmpXchgInst)
 //
 // @param atomicRmwInst : The instruction
 void PatchBufferOp::visitAtomicRMWInst(AtomicRMWInst &atomicRmwInst) {
-  // If the type we are doing an atomic operation on is not a fat pointer, bail.
-  if (atomicRmwInst.getPointerAddressSpace() != ADDR_SPACE_BUFFER_FAT_POINTER)
-    return;
+  if (atomicRmwInst.getPointerAddressSpace() == ADDR_SPACE_BUFFER_FAT_POINTER) {
+    m_builder->SetInsertPoint(&atomicRmwInst);
 
-  m_builder->SetInsertPoint(&atomicRmwInst);
+    Value *const pointer = getPointerOperandAsInst(atomicRmwInst.getPointerOperand());
 
-  Value *const pointer = getPointerOperandAsInst(atomicRmwInst.getPointerOperand());
+    Type *const storeType = atomicRmwInst.getValOperand()->getType();
 
-  Type *const storeType = atomicRmwInst.getValOperand()->getType();
+    const bool isSlc = atomicRmwInst.getMetadata(LLVMContext::MD_nontemporal);
 
-  const bool isSlc = atomicRmwInst.getMetadata(LLVMContext::MD_nontemporal);
+    Value *const bufferDesc = m_replacementMap[pointer].first;
+    Value *const baseIndex = m_builder->CreatePtrToInt(m_replacementMap[pointer].second, m_builder->getInt32Ty());
+    copyMetadata(baseIndex, &atomicRmwInst);
 
-  Value *const bufferDesc = m_replacementMap[pointer].first;
-  Value *const baseIndex = m_builder->CreatePtrToInt(m_replacementMap[pointer].second, m_builder->getInt32Ty());
-  copyMetadata(baseIndex, &atomicRmwInst);
+    // If our buffer descriptor is divergent, need to handle it differently.
+    if (m_divergenceSet.count(bufferDesc) > 0) {
+      Value *const baseAddr = getBaseAddressFromBufferDesc(bufferDesc);
 
-  // If our buffer descriptor is divergent, need to handle it differently.
-  if (m_divergenceSet.count(bufferDesc) > 0) {
-    Value *const baseAddr = getBaseAddressFromBufferDesc(bufferDesc);
+      // The 2nd element in the buffer descriptor is the byte bound, we do this to support robust buffer access.
+      Value *const bound = m_builder->CreateExtractElement(bufferDesc, 2);
+      Value *const inBound = m_builder->CreateICmpULT(baseIndex, bound);
+      Value *const newBaseIndex = m_builder->CreateSelect(inBound, baseIndex, m_builder->getInt32(0));
 
-    // The 2nd element in the buffer descriptor is the byte bound, we do this to support robust buffer access.
-    Value *const bound = m_builder->CreateExtractElement(bufferDesc, 2);
-    Value *const inBound = m_builder->CreateICmpULT(baseIndex, bound);
-    Value *const newBaseIndex = m_builder->CreateSelect(inBound, baseIndex, m_builder->getInt32(0));
+      // Add on the index to the address.
+      Value *atomicPointer = m_builder->CreateGEP(m_builder->getInt8Ty(), baseAddr, newBaseIndex);
 
-    // Add on the index to the address.
-    Value *atomicPointer = m_builder->CreateGEP(m_builder->getInt8Ty(), baseAddr, newBaseIndex);
+      atomicPointer = m_builder->CreateBitCast(atomicPointer, storeType->getPointerTo(ADDR_SPACE_GLOBAL));
 
-    atomicPointer = m_builder->CreateBitCast(atomicPointer, storeType->getPointerTo(ADDR_SPACE_GLOBAL));
+      AtomicRMWInst *const newAtomicRmw =
+          m_builder->CreateAtomicRMW(atomicRmwInst.getOperation(), atomicPointer, atomicRmwInst.getValOperand(),
+                                     atomicRmwInst.getAlign(), atomicRmwInst.getOrdering());
+      newAtomicRmw->setVolatile(atomicRmwInst.isVolatile());
+      newAtomicRmw->setSyncScopeID(atomicRmwInst.getSyncScopeID());
+      copyMetadata(newAtomicRmw, &atomicRmwInst);
 
-    AtomicRMWInst *const newAtomicRmw =
-        m_builder->CreateAtomicRMW(atomicRmwInst.getOperation(), atomicPointer, atomicRmwInst.getValOperand(),
-                                   atomicRmwInst.getAlign(), atomicRmwInst.getOrdering());
-    newAtomicRmw->setVolatile(atomicRmwInst.isVolatile());
-    newAtomicRmw->setSyncScopeID(atomicRmwInst.getSyncScopeID());
-    copyMetadata(newAtomicRmw, &atomicRmwInst);
+      // Record the atomic instruction so we remember to delete it later.
+      m_replacementMap[&atomicRmwInst] = std::make_pair(nullptr, nullptr);
 
-    // Record the atomic instruction so we remember to delete it later.
-    m_replacementMap[&atomicRmwInst] = std::make_pair(nullptr, nullptr);
+      atomicRmwInst.replaceAllUsesWith(newAtomicRmw);
+    } else {
+      switch (atomicRmwInst.getOrdering()) {
+      case AtomicOrdering::Release:
+      case AtomicOrdering::AcquireRelease:
+      case AtomicOrdering::SequentiallyConsistent: {
+        FenceInst *const fence = m_builder->CreateFence(AtomicOrdering::Release, atomicRmwInst.getSyncScopeID());
+        copyMetadata(fence, &atomicRmwInst);
+        break;
+      }
+      default: {
+        break;
+      }
+      }
+      Intrinsic::ID intrinsic = Intrinsic::not_intrinsic;
+      switch (atomicRmwInst.getOperation()) {
+      case AtomicRMWInst::Xchg:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_swap;
+        break;
+      case AtomicRMWInst::Add:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_add;
+        break;
+      case AtomicRMWInst::Sub:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_sub;
+        break;
+      case AtomicRMWInst::And:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_and;
+        break;
+      case AtomicRMWInst::Or:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_or;
+        break;
+      case AtomicRMWInst::Xor:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_xor;
+        break;
+      case AtomicRMWInst::Max:
+        if (storeType->isFloatTy() || storeType->isDoubleTy())
+          intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_fmax;
+        else
+          intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_smax;
+        break;
+      case AtomicRMWInst::Min:
+        if (storeType->isFloatTy() || storeType->isDoubleTy())
+          intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_fmin;
+        else
+          intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_smin;
+        break;
+      case AtomicRMWInst::UMax:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_umax;
+        break;
+      case AtomicRMWInst::UMin:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_umin;
+        break;
+      case AtomicRMWInst::FAdd:
+        intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_fadd;
+        break;
+      default:
+        llvm_unreachable("Should never be called!");
+        break;
+      }
 
-    atomicRmwInst.replaceAllUsesWith(newAtomicRmw);
-  } else {
-    switch (atomicRmwInst.getOrdering()) {
-    case AtomicOrdering::Release:
-    case AtomicOrdering::AcquireRelease:
-    case AtomicOrdering::SequentiallyConsistent: {
-      FenceInst *const fence = m_builder->CreateFence(AtomicOrdering::Release, atomicRmwInst.getSyncScopeID());
-      copyMetadata(fence, &atomicRmwInst);
-      break;
+      Value *const atomicCall = m_builder->CreateIntrinsic(intrinsic, cast<IntegerType>(storeType),
+                                                           {atomicRmwInst.getValOperand(), bufferDesc, baseIndex,
+                                                            m_builder->getInt32(0), m_builder->getInt32(isSlc * 2)});
+      copyMetadata(atomicCall, &atomicRmwInst);
+
+      switch (atomicRmwInst.getOrdering()) {
+      case AtomicOrdering::Acquire:
+      case AtomicOrdering::AcquireRelease:
+      case AtomicOrdering::SequentiallyConsistent: {
+        FenceInst *const fence = m_builder->CreateFence(AtomicOrdering::Acquire, atomicRmwInst.getSyncScopeID());
+        copyMetadata(fence, &atomicRmwInst);
+        break;
+      }
+      default: {
+        break;
+      }
+      }
+
+      // Record the atomic instruction so we remember to delete it later.
+      m_replacementMap[&atomicRmwInst] = std::make_pair(nullptr, nullptr);
+
+      atomicRmwInst.replaceAllUsesWith(atomicCall);
     }
-    default: {
-      break;
-    }
-    }
+  } else if (atomicRmwInst.getPointerAddressSpace() == ADDR_SPACE_GLOBAL) {
+    AtomicRMWInst::BinOp op = atomicRmwInst.getOperation();
+    Type *const storeType = atomicRmwInst.getValOperand()->getType();
+    if ((op == AtomicRMWInst::Min || op == AtomicRMWInst::Max || op == AtomicRMWInst::FAdd) &&
+        storeType->isFloatingPointTy()) {
+      Value *const pointer = getPointerOperandAsInst(atomicRmwInst.getPointerOperand());
+      m_builder->SetInsertPoint(&atomicRmwInst);
+      Intrinsic::ID intrinsic = Intrinsic::not_intrinsic;
+      switch (atomicRmwInst.getOperation()) {
+      case AtomicRMWInst::Min:
+        intrinsic = Intrinsic::amdgcn_global_atomic_fmin;
+        break;
+      case AtomicRMWInst::Max:
+        intrinsic = Intrinsic::amdgcn_global_atomic_fmax;
+        break;
+      case AtomicRMWInst::FAdd:
+        intrinsic = Intrinsic::amdgcn_global_atomic_fadd;
+        break;
+      default:
+        llvm_unreachable("Should never be called!");
+        break;
+      }
+      Value *const atomicCall = m_builder->CreateIntrinsic(intrinsic, {storeType, pointer->getType(), storeType},
+                                                           {pointer, atomicRmwInst.getValOperand()});
+      copyMetadata(atomicCall, &atomicRmwInst);
+      // Record the atomic instruction so we remember to delete it later.
+      m_replacementMap[&atomicRmwInst] = std::make_pair(nullptr, nullptr);
 
-    Intrinsic::ID intrinsic = Intrinsic::not_intrinsic;
-    switch (atomicRmwInst.getOperation()) {
-    case AtomicRMWInst::Xchg:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_swap;
-      break;
-    case AtomicRMWInst::Add:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_add;
-      break;
-    case AtomicRMWInst::Sub:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_sub;
-      break;
-    case AtomicRMWInst::And:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_and;
-      break;
-    case AtomicRMWInst::Or:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_or;
-      break;
-    case AtomicRMWInst::Xor:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_xor;
-      break;
-    case AtomicRMWInst::Max:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_smax;
-      break;
-    case AtomicRMWInst::Min:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_smin;
-      break;
-    case AtomicRMWInst::UMax:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_umax;
-      break;
-    case AtomicRMWInst::UMin:
-      intrinsic = Intrinsic::amdgcn_raw_buffer_atomic_umin;
-      break;
-    default:
-      llvm_unreachable("Should never be called!");
-      break;
+      atomicRmwInst.replaceAllUsesWith(atomicCall);
     }
+  } else if (atomicRmwInst.getPointerAddressSpace() == ADDR_SPACE_LOCAL) {
+    AtomicRMWInst::BinOp op = atomicRmwInst.getOperation();
+    Type *const storeType = atomicRmwInst.getValOperand()->getType();
+    if ((op == AtomicRMWInst::Min || op == AtomicRMWInst::Max || op == AtomicRMWInst::FAdd) &&
+        storeType->isFloatingPointTy()) {
+      Value *const pointer = getPointerOperandAsInst(atomicRmwInst.getPointerOperand());
+      m_builder->SetInsertPoint(&atomicRmwInst);
+      Intrinsic::ID intrinsic = Intrinsic::not_intrinsic;
+      switch (atomicRmwInst.getOperation()) {
+      case AtomicRMWInst::Min:
+        intrinsic = Intrinsic::amdgcn_ds_fmin;
+        break;
+      case AtomicRMWInst::Max:
+        intrinsic = Intrinsic::amdgcn_ds_fmax;
+        break;
+      case AtomicRMWInst::FAdd:
+        intrinsic = Intrinsic::amdgcn_ds_fadd;
+        break;
+      default:
+        llvm_unreachable("Should never be called!");
+        break;
+      }
 
-    Value *const atomicCall = m_builder->CreateIntrinsic(
-        intrinsic, cast<IntegerType>(storeType),
-        {atomicRmwInst.getValOperand(), bufferDesc, baseIndex, m_builder->getInt32(0), m_builder->getInt32(isSlc * 2)});
-    copyMetadata(atomicCall, &atomicRmwInst);
-
-    switch (atomicRmwInst.getOrdering()) {
-    case AtomicOrdering::Acquire:
-    case AtomicOrdering::AcquireRelease:
-    case AtomicOrdering::SequentiallyConsistent: {
-      FenceInst *const fence = m_builder->CreateFence(AtomicOrdering::Acquire, atomicRmwInst.getSyncScopeID());
-      copyMetadata(fence, &atomicRmwInst);
-      break;
+      Value *const atomicCall = m_builder->CreateIntrinsic(
+          intrinsic, {storeType},
+          {pointer, atomicRmwInst.getValOperand(),
+           m_builder->getInt32(static_cast<uint32_t>(atomicRmwInst.getOrdering())),
+           m_builder->getInt32(atomicRmwInst.getSyncScopeID()), m_builder->getInt1(atomicRmwInst.isVolatile())});
+      copyMetadata(atomicCall, &atomicRmwInst);
+      // Record the atomic instruction so we remember to delete it later.
+      m_replacementMap[&atomicRmwInst] = std::make_pair(nullptr, nullptr);
+      atomicRmwInst.replaceAllUsesWith(atomicCall);
     }
-    default: {
-      break;
-    }
-    }
-
-    // Record the atomic instruction so we remember to delete it later.
-    m_replacementMap[&atomicRmwInst] = std::make_pair(nullptr, nullptr);
-
-    atomicRmwInst.replaceAllUsesWith(atomicCall);
   }
 }
 

llpc/translator/lib/SPIRV/SPIRVReader.cpp
@@ -1848,7 +1848,19 @@ Value *SPIRVToLLVM::transAtomicRMW(SPIRVValue *const spvValue, const AtomicRMWIn
   Value *const atomicValue = transValue(spvAtomicInst->getOpValue(3), getBuilder()->GetInsertBlock()->getParent(),
                                         getBuilder()->GetInsertBlock());
 
-  return getBuilder()->CreateAtomicRMW(binOp, atomicPointer, atomicValue, MaybeAlign(), ordering, scope);
+  // This is a workaround, buffer.atomic.swap.f64 is not supported, convert double to int64.
+  if ((binOp == AtomicRMWInst::Xchg) && atomicValue->getType()->isDoubleTy()) {
+    Value *const int64Value = getBuilder()->CreateBitCast(atomicValue, getBuilder()->getInt64Ty());
+
+    Value *const int64AtomicPointer = getBuilder()->CreateBitCast(
+        atomicPointer, PointerType::get(getBuilder()->getInt64Ty(),
+                                        dyn_cast<PointerType>(atomicPointer->getType())->getAddressSpace()));
+    Value *const atomicRes =
+        getBuilder()->CreateAtomicRMW(binOp, int64AtomicPointer, int64Value, MaybeAlign(), ordering, scope);
+
+    return getBuilder()->CreateBitCast(atomicRes, getBuilder()->getDoubleTy());
+  } else
+    return getBuilder()->CreateAtomicRMW(binOp, atomicPointer, atomicValue, MaybeAlign(), ordering, scope);
 }
 
 // =====================================================================================================================
@@ -2037,6 +2049,45 @@ template <> Value *SPIRVToLLVM::transValueWithOpcode<OpAtomicXor>(SPIRVValue *co
   }
 
   return transAtomicRMW(spvValue, AtomicRMWInst::Xor);
+}
+
+// =====================================================================================================================
+// Handle OpAtomicFMinEXT.
+//
+// @param spvValue : A SPIR-V value.
+template <> Value *SPIRVToLLVM::transValueWithOpcode<OpAtomicFMinEXT>(SPIRVValue *const spvValue) {
+  // Image texel atomic operations use the older path for now.
+  if (static_cast<SPIRVInstruction *>(spvValue)->getOperands()[0]->getOpCode() == OpImageTexelPointer) {
+    return transSPIRVImageAtomicOpFromInst(static_cast<SPIRVInstruction *>(spvValue), getBuilder()->GetInsertBlock());
+  }
+
+  return transAtomicRMW(spvValue, AtomicRMWInst::Min);
+}
+
+// =====================================================================================================================
+// Handle OpAtomicFMaxEXT.
+//
+// @param spvValue : A SPIR-V value.
+template <> Value *SPIRVToLLVM::transValueWithOpcode<OpAtomicFMaxEXT>(SPIRVValue *const spvValue) {
+  // Image texel atomic operations use the older path for now.
+  if (static_cast<SPIRVInstruction *>(spvValue)->getOperands()[0]->getOpCode() == OpImageTexelPointer) {
+    return transSPIRVImageAtomicOpFromInst(static_cast<SPIRVInstruction *>(spvValue), getBuilder()->GetInsertBlock());
+  }
+
+  return transAtomicRMW(spvValue, AtomicRMWInst::Max);
+}
+
+// =====================================================================================================================
+// Handle OpAtomicFAddEXT.
+//
+// @param spvValue : A SPIR-V value.
+template <> Value *SPIRVToLLVM::transValueWithOpcode<OpAtomicFAddEXT>(SPIRVValue *const spvValue) {
+  // Image texel atomic operations use the older path for now.
+  if (static_cast<SPIRVInstruction *>(spvValue)->getOperands()[0]->getOpCode() == OpImageTexelPointer) {
+    return transSPIRVImageAtomicOpFromInst(static_cast<SPIRVInstruction *>(spvValue), getBuilder()->GetInsertBlock());
+  }
+
+  return transAtomicRMW(spvValue, AtomicRMWInst::FAdd);
 }
 
 // =====================================================================================================================
@@ -4874,6 +4925,12 @@ Value *SPIRVToLLVM::transValueWithoutDecoration(SPIRVValue *bv, Function *f, Bas
     return mapValue(bv, transValueWithOpcode<OpAtomicOr>(bv));
   case OpAtomicXor:
     return mapValue(bv, transValueWithOpcode<OpAtomicXor>(bv));
+  case OpAtomicFMinEXT:
+    return mapValue(bv, transValueWithOpcode<OpAtomicFMinEXT>(bv));
+  case OpAtomicFMaxEXT:
+    return mapValue(bv, transValueWithOpcode<OpAtomicFMaxEXT>(bv));
+  case OpAtomicFAddEXT:
+    return mapValue(bv, transValueWithOpcode<OpAtomicFAddEXT>(bv));
   case OpCopyMemory:
     return mapValue(bv, transValueWithOpcode<OpCopyMemory>(bv));
   case OpLoad:
@@ -5851,7 +5908,15 @@ Value *SPIRVToLLVM::transSPIRVImageAtomicOpFromInst(SPIRVInstruction *bi, BasicB
   case OpAtomicXor:
     atomicOp = lgc::Builder::ImageAtomicXor;
     break;
-
+  case OpAtomicFMinEXT:
+    atomicOp = lgc::Builder::ImageAtomicFMin;
+    break;
+  case OpAtomicFMaxEXT:
+    atomicOp = lgc::Builder::ImageAtomicFMax;
+    break;
+  case OpAtomicFAddEXT:
+    atomicOp = lgc::Builder::ImageAtomicFAdd;
+    break;
   default:
     llvm_unreachable("Unknown image atomic op");
     break;

lgc/builder/ImageBuilder.cpp
@@ -337,7 +337,8 @@ static const Intrinsic::ID StructBufferAtomicIntrinsicTable[] = {
     Intrinsic::amdgcn_struct_buffer_atomic_smin, Intrinsic::amdgcn_struct_buffer_atomic_umin,
     Intrinsic::amdgcn_struct_buffer_atomic_smax, Intrinsic::amdgcn_struct_buffer_atomic_umax,
     Intrinsic::amdgcn_struct_buffer_atomic_and,  Intrinsic::amdgcn_struct_buffer_atomic_or,
-    Intrinsic::amdgcn_struct_buffer_atomic_xor};
+    Intrinsic::amdgcn_struct_buffer_atomic_xor,  Intrinsic::amdgcn_struct_buffer_atomic_fmin,
+    Intrinsic::amdgcn_struct_buffer_atomic_fmax, Intrinsic::amdgcn_struct_buffer_atomic_fadd};
 
 // Intrinsic ID table for image atomic
 static const Intrinsic::ID ImageAtomicIntrinsicTable[][8] = {
@@ -385,7 +386,14 @@ static const Intrinsic::ID ImageAtomicIntrinsicTable[][8] = {
      Intrinsic::amdgcn_image_atomic_xor_3d, Intrinsic::amdgcn_image_atomic_xor_cube,
      Intrinsic::amdgcn_image_atomic_xor_1darray, Intrinsic::amdgcn_image_atomic_xor_2darray,
      Intrinsic::amdgcn_image_atomic_xor_2dmsaa, Intrinsic::amdgcn_image_atomic_xor_2darraymsaa},
-};
+    {Intrinsic::amdgcn_image_atomic_fmin_1d, Intrinsic::amdgcn_image_atomic_fmin_2d,
+     Intrinsic::amdgcn_image_atomic_fmin_3d, Intrinsic::amdgcn_image_atomic_fmin_cube,
+     Intrinsic::amdgcn_image_atomic_fmin_1darray, Intrinsic::amdgcn_image_atomic_fmin_2darray,
+     Intrinsic::amdgcn_image_atomic_fmin_2dmsaa, Intrinsic::amdgcn_image_atomic_fmin_2darraymsaa},
+    {Intrinsic::amdgcn_image_atomic_fmax_1d, Intrinsic::amdgcn_image_atomic_fmax_2d,
+     Intrinsic::amdgcn_image_atomic_fmax_3d, Intrinsic::amdgcn_image_atomic_fmax_cube,
+     Intrinsic::amdgcn_image_atomic_fmax_1darray, Intrinsic::amdgcn_image_atomic_fmax_2darray,
+     Intrinsic::amdgcn_image_atomic_fmax_2dmsaa, Intrinsic::amdgcn_image_atomic_fmax_2darraymsaa}};
 
 // =====================================================================================================================
 // Convert an integer or vector of integer type to the equivalent (vector of) half/float/double

lgc/interface/lgc/Builder.h
@@ -867,16 +867,19 @@ public:
 
   // Atomic operation, for use in CreateImageAtomic.
   enum {
-    ImageAtomicSwap = 0, // Atomic operation: swap
-    ImageAtomicAdd = 2,  // Atomic operation: add
-    ImageAtomicSub = 3,  // Atomic operation: subtract
-    ImageAtomicSMin = 4, // Atomic operation: signed minimum
-    ImageAtomicUMin = 5, // Atomic operation: unsigned minimum
-    ImageAtomicSMax = 6, // Atomic operation: signed maximum
-    ImageAtomicUMax = 7, // Atomic operation: unsigned maximum
-    ImageAtomicAnd = 8,  // Atomic operation: and
-    ImageAtomicOr = 9,   // Atomic operation: or
-    ImageAtomicXor = 10  // Atomic operation: xor
+    ImageAtomicSwap = 0,  // Atomic operation: swap
+    ImageAtomicAdd = 2,   // Atomic operation: add
+    ImageAtomicSub = 3,   // Atomic operation: subtract
+    ImageAtomicSMin = 4,  // Atomic operation: signed minimum
+    ImageAtomicUMin = 5,  // Atomic operation: unsigned minimum
+    ImageAtomicSMax = 6,  // Atomic operation: signed maximum
+    ImageAtomicUMax = 7,  // Atomic operation: unsigned maximum
+    ImageAtomicAnd = 8,   // Atomic operation: and
+    ImageAtomicOr = 9,    // Atomic operation: or
+    ImageAtomicXor = 10,  // Atomic operation: xor
+    ImageAtomicFMin = 11, // Atomic operation: fmin
+    ImageAtomicFMax = 12, // Atomic operation: fmax
+    ImageAtomicFAdd = 13  // Atomic operation: fadd
   };
 
   // Create an image load.

llpc/translator/lib/SPIRV/SPIRVInternal.h
@@ -278,7 +278,10 @@ enum SPIRVImageOpKind {
   ImageOpAtomicUMax,
   ImageOpAtomicAnd,
   ImageOpAtomicOr,
-  ImageOpAtomicXor
+  ImageOpAtomicXor,
+  ImageAtomicFMin,
+  ImageAtomicFMax,
+  ImageAtomicFAdd
 };
 
 template <> inline void SPIRVMap<SPIRVImageOpKind, std::string>::init() {
@@ -304,6 +307,9 @@ template <> inline void SPIRVMap<SPIRVImageOpKind, std::string>::init() {
   add(ImageOpAtomicAnd, "atomicand");
   add(ImageOpAtomicOr, "atomicor");
   add(ImageOpAtomicXor, "atomicxor");
+  add(ImageAtomicFMin, "atomicfmin");
+  add(ImageAtomicFMax, "atomicfmax");
+  add(ImageAtomicFAdd, "atomicfadd");
 }
 typedef SPIRVMap<SPIRVImageOpKind, std::string> SPIRVImageOpKindNameMap;
 
@@ -417,6 +423,9 @@ template <> inline void SPIRVMap<Op, SPIRVImageOpInfo>::init() {
   add(OpAtomicAnd, {ImageOpAtomicAnd, InvalidOperIdx, InvalidOperIdx, false, false, 3, InvalidOperIdx, 1});
   add(OpAtomicOr, {ImageOpAtomicOr, InvalidOperIdx, InvalidOperIdx, false, false, 3, InvalidOperIdx, 1});
   add(OpAtomicXor, {ImageOpAtomicXor, InvalidOperIdx, InvalidOperIdx, false, false, 3, InvalidOperIdx, 1});
+  add(OpAtomicFMinEXT, {ImageAtomicFMin, InvalidOperIdx, InvalidOperIdx, false, false, 3, InvalidOperIdx, 1});
+  add(OpAtomicFMaxEXT, {ImageAtomicFMax, InvalidOperIdx, InvalidOperIdx, false, false, 3, InvalidOperIdx, 1});
+  add(OpAtomicFAddEXT, {ImageAtomicFAdd, InvalidOperIdx, InvalidOperIdx, false, false, 3, InvalidOperIdx, 1});
 }
 typedef SPIRVMap<Op, SPIRVImageOpInfo> SPIRVImageOpInfoMap;
 

llpc/translator/lib/SPIRV/libSPIRV/SPIRVInstruction.h
@@ -2365,6 +2365,11 @@ public:
       // 1. OpAtomicStore - need to check type of the Value operand
       CapVec.push_back(CapabilityInt64Atomics);
     }
+    if ((getOpCode() == OpAtomicFMinEXT) || (getOpCode() == OpAtomicFMaxEXT))
+      if (getType()->isTypeInt(64))
+        CapVec.push_back(CapabilityAtomicFloat64MinMaxEXT);
+      else
+        CapVec.push_back(CapabilityAtomicFloat32MinMaxEXT);
     return CapVec;
   }
 
@@ -2397,6 +2402,9 @@ _SPIRV_OP(AtomicSMax, true, 7)
 _SPIRV_OP(AtomicAnd, true, 7)
 _SPIRV_OP(AtomicOr, true, 7)
 _SPIRV_OP(AtomicXor, true, 7)
+_SPIRV_OP(AtomicFMinEXT, true, 7)
+_SPIRV_OP(AtomicFMaxEXT, true, 7)
+_SPIRV_OP(AtomicFAddEXT, true, 7)
 _SPIRV_OP(MemoryBarrier, false, 3)
 #undef _SPIRV_OP
 

llpc/translator/lib/SPIRV/libSPIRV/SPIRVIsValidEnum.h
@@ -547,6 +547,8 @@ inline bool isValid(spv::Capability V) {
   case CapabilityStorageTexelBufferArrayNonUniformIndexingEXT:
 #endif
   case CapabilityDemoteToHelperInvocationEXT:
+  case CapabilityAtomicFloat32MinMaxEXT:
+  case CapabilityAtomicFloat64MinMaxEXT:
     return true;
   default:
     return false;
@@ -757,6 +759,9 @@ inline bool isValid(spv::Op V) {
   case OpAtomicAnd:
   case OpAtomicOr:
   case OpAtomicXor:
+  case OpAtomicFMinEXT:
+  case OpAtomicFMaxEXT:
+  case OpAtomicFAddEXT:
   case OpPhi:
   case OpLoopMerge:
   case OpSelectionMerge:

llpc/translator/lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
@@ -448,6 +448,8 @@ template <> inline void SPIRVMap<Capability, std::string>::init() {
   add(CapabilitySubgroupBufferBlockIOINTEL, "SubgroupBufferBlockIOINTEL");
   add(CapabilitySubgroupImageBlockIOINTEL, "SubgroupImageBlockIOINTEL");
   add(CapabilityDemoteToHelperInvocationEXT, "DemoteToHelperInvocationEXT");
+  add(CapabilityAtomicFloat32MinMaxEXT, "AtomicFloat32MinMaxEXT");
+  add(CapabilityAtomicFloat64MinMaxEXT, "AtomicFloat64MinMaxEXT");
 }
 SPIRV_DEF_NAMEMAP(Capability, SPIRVCapabilityNameMap)
 

commit 4cfad5ad7794c8ef66e71a3a6ada76f5c455339e

Fixes for asserts caused by atomic-float2
Changes introduced in https://github.com/GPUOpen-Drivers/llpc/pull/1384 caused
regressions when asserts enabled.

Add fixes to ensure that types are consistent.
For the atomic image load variation, the solution was to use a bitcast for the
non-float atomic load (implemented as add), rather than using the fadd variant.

lgc/patch/PatchBufferOp.cpp
@@ -342,7 +342,7 @@ void PatchBufferOp::visitAtomicRMWInst(AtomicRMWInst &atomicRmwInst) {
         break;
       }
 
-      Value *const atomicCall = m_builder->CreateIntrinsic(intrinsic, cast<IntegerType>(storeType),
+      Value *const atomicCall = m_builder->CreateIntrinsic(intrinsic, storeType,
                                                            {atomicRmwInst.getValOperand(), bufferDesc, baseIndex,
                                                             m_builder->getInt32(0), m_builder->getInt32(isSlc * 2)});
       copyMetadata(atomicCall, &atomicRmwInst);

llpc/translator/lib/SPIRV/SPIRVReader.cpp
@@ -5934,6 +5934,9 @@ Value *SPIRVToLLVM::transSPIRVImageAtomicOpFromInst(SPIRVInstruction *bi, BasicB
     result = getBuilder()->CreateImageAtomic(atomicOp, imageInfo.dim, imageInfo.flags, ordering, imageInfo.imageDesc,
                                              coord, inputData);
   }
+  if (bi->getOpCode() == OpAtomicLoad && bi->getType()->isTypeFloat())
+    result = getBuilder()->CreateBitCast(result, transFPType(bi->getType()));
+
   return result;
 }
 