39294b0685b0c1ad4bf62d5063304dcd95f6a7ec
Piotr Sobczak <Piotr.Sobczak@amd.com>
2021-10-01 10:57:58 +0200

    Remove most of lgc-patch-peephole-opt pass
    Some transformations were added ~2 years ago to work around various
    LLVM back-end issues. Now, the code does not seem to have any positive
    effect on generated code as verified with the usual set of benchmarks.

--- lgc/patch/PatchPeepholeOpt.h
+++ lgc/patch/PatchPeepholeOpt.h
@@ -39,17 +39,6 @@ namespace lgc {
 // =====================================================================================================================
 // Represents the pass of LLVM patching operations for peephole optimizations, with the following patterns covered:
 //
-// - Combine multiple identical bit casts (LLVM treats these as no-ops, but since we use the scalarizer they can become
-//   costly when transforming <4 x i8> <-> i32/float).
-// - Change bitcast( shufflevector x, y ) -> shufflevector( bitcast x ), ( bitcast y ).
-// - Change bitcast( phi [x, foo], [y, bar] ) -> phi [( bitcast x ), foo], [( bitcast y ), bar].
-// - Change integer comparisons for unsigned greater than such that x > y, y is a constant -> x < y + 1.
-// - Remove extract elements when they are extracting from an insert (forward the extract to the inserted element).
-// - Combine multiple extract elements that extract the same element.
-// - Aggressively scalarize PHI nodes that use 32-bit or wider types.
-// - Remove PHI nodes whose incoming values can be proven to be identical in the parent blocks (most commonly seen with
-//   multiple extract elements that are identical).
-// - Optimize PHI nodes that are confusingly non-PHIs by deducing these complicated cases and removing the PHIs.
 // - Change inttoptr ( add x, const ) -> gep ( inttoptr x, const ) to improve value tracking and load/store
 //   vectorization.
 //
@@ -61,13 +50,7 @@ public:
 
   void getAnalysisUsage(llvm::AnalysisUsage &analysisUsage) const override;
 
-  void visitBitCast(llvm::BitCastInst &bitCast);
-  void visitExtractElement(llvm::ExtractElementInst &extractElement);
-  void visitPHINode(llvm::PHINode &phiNode);
   void visitIntToPtr(llvm::IntToPtrInst &intToPtr);
-
-  void moveAfter(llvm::Instruction &move, llvm::Instruction &after) const;
-  void insertAfter(llvm::Instruction &insert, llvm::Instruction &after) const;
 
   static char ID; // ID of this pass
 
