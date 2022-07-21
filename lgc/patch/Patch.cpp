353fb7c62211fbb9607c8487fa685324ee41a5e6
Nicolai Hähnle <nicolai.haehnle@amd.com>
2022-07-21 23:49:34 +0200

    lgc: register the lgc-builder-replayer pass
    Since the builder replayer requires an argument for construction, adjust
    the pass registry setup slightly.
    
    If we ever want to implement something like `opt -passes='...'` for lgc,
    we'll need another iteration on the pass registry. Let's cross that
    bridge when we get there.

--- lgc/patch/Patch.cpp
+++ lgc/patch/Patch.cpp
@@ -247,7 +247,7 @@ void Patch::addPasses(PipelineState *pipelineState, lgc::PassManager &passMgr, b
 //
 // @param [in/out] passMgr : Pass manager
 void Patch::registerPasses(lgc::PassManager &passMgr) {
-#define LLPC_PASS(NAME, CLASS) passMgr.registerPass(NAME, decltype(CLASS)::name());
+#define LLPC_PASS(NAME, CLASS) passMgr.registerPass(NAME, CLASS::name());
 #include "PassRegistry.inc"
 }
 
