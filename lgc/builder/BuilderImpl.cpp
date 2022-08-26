GPUOpen-Drivers/llpc

commit 98281b1a8c1098d92131d73bf7090481e9d5ffa7

Add support for scalarize waterfall descriptor loads.
Provide support for scalarizing waterfalled non-uniform descriptor
loads in limited circumstances.
This is an alternative to the current behaviour which scalarizes
access to a vector loaded outside the waterfall loop.

While using scalarized loads introduces memory access into each
loop iteration it significantly reduces VGPR pressure created by
the loop.
It also has the positive side effect of reducing the probability
that a s_waitcnt on the vmcnt is introduced into the waterfall loop
by the backend, as lgkmcnt is used for the loads instead.

In practice performance of the scalarized loads can be equivalent
to vector loads when there is medium to high commonality in the
non-uniform values.

Automatically enable this scalarization when a vgpr limit is set.
As this is where it will be most beneficial.

The limitation of this transform is that loads are only scalarized
when all non-uniform values have a common base/index.
This covers the majority of scenarios.
For a general purpose solution instruction would need to be heavily
reordered to ensure the correct parts occur inside/outside the
waterfall loop.

lgc/builder/BuilderImpl.cpp
@@ -323,6 +323,150 @@ BranchInst *BuilderImplBase::createIf(Value *condition, bool wantElse, const Twi
   return branch;
 }
 
+#if defined(LLVM_HAVE_BRANCH_AMD_GFX)
+// =====================================================================================================================
+// For a non-uniform input, try and trace back through a descriptor load to
+// find the non-uniform index used in it. If that fails, we just use the
+// operand value as the index.
+//
+// Note: this code has to cope with relocs as well, this is why we have to
+// have a worklist of instructions to trace back
+// through. Something like this:
+// %1 = call .... @lgc.descriptor.set(...)          ;; Known uniform base
+// %2 = call .... @llvm.amdgcn.reloc.constant(...)  ;; Known uniform reloc constant
+// %3 = ptrtoint ... %1 to i64
+// %4 = zext ... %2 to i64
+// %5 = add i64 %3, %4
+// %6 = inttoptr i64 %5 to ....
+// %7 = bitcast .... %6 to ....
+// %8 = getelementptr .... %7, i64 %offset
+//
+// As long as the base pointer %7 can be traced back to a descriptor set and
+// reloc we can infer that it is truly uniform and use the gep index as the waterfall index safely.
+//
+// @param nonUniformVal : Value representing non-uniform descriptor
+// @return : Value representing the non-uniform index
+static Value *traceNonUniformIndex(Value *nonUniformVal) {
+  auto load = dyn_cast<LoadInst>(nonUniformVal);
+  if (!load)
+    return nonUniformVal;
+
+  SmallVector<Value *, 2> worklist;
+  Value *base = load->getOperand(0);
+  Value *index = nullptr;
+
+  // Loop until a descriptor table reference or unexpected operation is reached.
+  // In the worst case this may visit all instructions in a function.
+  for (;;) {
+    if (auto bitcast = dyn_cast<BitCastInst>(base)) {
+      base = bitcast->getOperand(0);
+      continue;
+    }
+    if (auto gep = dyn_cast<GetElementPtrInst>(base)) {
+      if (gep->hasAllConstantIndices()) {
+        base = gep->getPointerOperand();
+        continue;
+      }
+      // Variable GEP, to provide the index for the waterfall.
+      if (index || gep->getNumIndices() != 1)
+        break;
+      index = *gep->idx_begin();
+      base = gep->getPointerOperand();
+      continue;
+    }
+    if (auto extract = dyn_cast<ExtractValueInst>(base)) {
+      if (extract->getIndices().size() == 1 && extract->getIndices()[0] == 0) {
+        base = extract->getAggregateOperand();
+        continue;
+      }
+      break;
+    }
+    if (auto insert = dyn_cast<InsertValueInst>(base)) {
+      if (insert->getIndices()[0] != 0) {
+        base = insert->getAggregateOperand();
+        continue;
+      }
+      if (insert->getIndices().size() == 1 && insert->getIndices()[0] == 0) {
+        base = insert->getInsertedValueOperand();
+        continue;
+      }
+      break;
+    }
+    if (auto intToPtr = dyn_cast<IntToPtrInst>(base)) {
+      base = intToPtr->getOperand(0);
+      continue;
+    }
+    if (auto ptrToInt = dyn_cast<PtrToIntInst>(base)) {
+      base = ptrToInt->getOperand(0);
+      continue;
+    }
+    if (auto zExt = dyn_cast<ZExtInst>(base)) {
+      base = zExt->getOperand(0);
+      continue;
+    }
+    if (auto call = dyn_cast<CallInst>(base)) {
+      if (index) {
+        if (auto calledFunc = call->getCalledFunction()) {
+          if (calledFunc->getName().startswith(lgcName::DescriptorTableAddr) ||
+              calledFunc->getName().startswith("llvm.amdgcn.reloc.constant")) {
+            if (!worklist.empty()) {
+              base = worklist.pop_back_val();
+              continue;
+            }
+            nonUniformVal = index;
+            break;
+          }
+        }
+      }
+    }
+    if (auto addInst = dyn_cast<Instruction>(base)) {
+      // In this case we have to trace back both operands
+      // Set one to base for continued processing and put the other onto the worklist
+      // Give up if the worklist already has an entry - too complicated
+      if (addInst->isBinaryOp() && addInst->getOpcode() == Instruction::BinaryOps::Add) {
+        if (!worklist.empty())
+          break;
+        base = addInst->getOperand(0);
+        worklist.push_back(addInst->getOperand(1));
+        continue;
+      }
+    }
+    break;
+  }
+
+  return nonUniformVal;
+}
+
+// =====================================================================================================================
+// Test whether two instructions are identical
+// or are the same operation on identical operands.
+// @param lhs : First instruction
+// @param rhs : Second instruction
+// @return Result of equally test
+static bool instructionsEqual(Instruction *lhs, Instruction *rhs) {
+  if (lhs->isIdenticalTo(rhs))
+    return true;
+
+  if (!lhs->isSameOperationAs(rhs))
+    return false;
+
+  for (unsigned idx = 0, end = lhs->getNumOperands(); idx != end; ++idx) {
+    Value *lhsVal = lhs->getOperand(idx);
+    Value *rhsVal = rhs->getOperand(idx);
+    if (lhsVal == rhsVal)
+      continue;
+    Instruction *lhsInst = dyn_cast<Instruction>(lhsVal);
+    Instruction *rhsInst = dyn_cast<Instruction>(rhsVal);
+    if (!lhsInst || !rhsInst)
+      return false;
+    if (!lhsInst->isIdenticalTo(rhsInst))
+      return false;
+  }
+
+  return true;
+}
+#endif // defined(LLVM_HAVE_BRANCH_AMD_GFX)
+
 // =====================================================================================================================
 // Create a waterfall loop containing the specified instruction.
 // This does not use the current insert point; new code is inserted before and after pNonUniformInst.
@@ -331,7 +475,7 @@ BranchInst *BuilderImplBase::createIf(Value *condition, bool wantElse, const Twi
 // @param operandIdxs : The operand index/indices for non-uniform inputs that need to be uniform
 // @param instName : Name to give instruction(s)
 Instruction *BuilderImplBase::createWaterfallLoop(Instruction *nonUniformInst, ArrayRef<unsigned> operandIdxs,
-                                                  const Twine &instName) {
+                                                  bool scalarizeDescriptorLoads, const Twine &instName) {
 #if !defined(LLVM_HAVE_BRANCH_AMD_GFX)
 #warning[!amd-gfx] Waterfall feature disabled
   errs() << "Generating invalid waterfall loop code\n";
@@ -339,146 +483,93 @@ Instruction *BuilderImplBase::createWaterfallLoop(Instruction *nonUniformInst, A
 #else
   assert(operandIdxs.empty() == false);
 
-  // For each non-uniform input, try and trace back through a descriptor load to
-  // find the non-uniform index used in it. If that fails, we just use the
-  // operand value as the index.
-  //
-  // Note: this code has to cope with relocs as well, this is why we have to
-  // have a worklist of instructions to trace back
-  // through. Something like this:
-  // %1 = call .... @lgc.descriptor.set(...)          ;; Known uniform base
-  // %2 = call .... @llvm.amdgcn.reloc.constant(...)  ;; Known uniform reloc constant
-  // %3 = ptrtoint ... %1 to i64
-  // %4 = zext ... %2 to i64
-  // %5 = add i64 %3, %4
-  // %6 = inttoptr i64 %5 to ....
-  // %7 = bitcast .... %6 to ....
-  // %8 = getelementptr .... %7, i64 %offset
-  //
-  // As long as the base pointer %7 can be traced back to a descriptor set and
-  // reloc we can infer that it is truly uniform and use the gep index as the waterfall index safely
-  //
-
   SmallVector<Value *, 2> nonUniformIndices;
-  SmallVector<Value *, 2> worklist;
-
   for (unsigned operandIdx : operandIdxs) {
-    Value *nonUniformVal = nonUniformInst->getOperand(operandIdx);
-    if (auto load = dyn_cast<LoadInst>(nonUniformVal)) {
-      Value *base = load->getOperand(0);
-      Value *index = nullptr;
-      for (;;) {
-        if (auto bitcast = dyn_cast<BitCastInst>(base)) {
-          base = bitcast->getOperand(0);
-          continue;
-        }
-        if (auto gep = dyn_cast<GetElementPtrInst>(base)) {
-          if (gep->hasAllConstantIndices()) {
-            base = gep->getPointerOperand();
-            continue;
-          }
-          // Variable GEP, to provide the index for the waterfall.
-          if (index || gep->getNumIndices() != 1)
-            break;
-          index = *gep->idx_begin();
-          base = gep->getPointerOperand();
-          continue;
-        }
-        if (auto extract = dyn_cast<ExtractValueInst>(base)) {
-          if (extract->getIndices().size() == 1 && extract->getIndices()[0] == 0) {
-            base = extract->getAggregateOperand();
-            continue;
-          }
-          break;
-        }
-        if (auto insert = dyn_cast<InsertValueInst>(base)) {
-          if (insert->getIndices()[0] != 0) {
-            base = insert->getAggregateOperand();
-            continue;
-          }
-          if (insert->getIndices().size() == 1 && insert->getIndices()[0] == 0) {
-            base = insert->getInsertedValueOperand();
-            continue;
-          }
-          break;
-        }
-        if (auto intToPtr = dyn_cast<IntToPtrInst>(base)) {
-          base = intToPtr->getOperand(0);
-          continue;
-        }
-        if (auto ptrToInt = dyn_cast<PtrToIntInst>(base)) {
-          base = ptrToInt->getOperand(0);
-          continue;
-        }
-        if (auto zExt = dyn_cast<ZExtInst>(base)) {
-          base = zExt->getOperand(0);
-          continue;
-        }
-        if (auto call = dyn_cast<CallInst>(base)) {
-          if (index) {
-            if (auto calledFunc = call->getCalledFunction()) {
-              if (calledFunc->getName().startswith(lgcName::DescriptorTableAddr) ||
-                  calledFunc->getName().startswith("llvm.amdgcn.reloc.constant")) {
-                if (worklist.size()) {
-                  base = worklist.pop_back_val();
-                  continue;
-                }
-                nonUniformVal = index;
-                break;
-              }
-            }
-          }
-        }
-        if (auto addInst = dyn_cast<Instruction>(base)) {
-          // In this case we have to trace back both operands
-          // Set one to base for continued processing and put the other onto the worklist
-          // Give up if the worklist already has an entry - too complicated
-          if (addInst->isBinaryOp() && addInst->getOpcode() == Instruction::BinaryOps::Add) {
-            if (worklist.size())
-              break;
-            base = addInst->getOperand(0);
-            worklist.push_back(addInst->getOperand(1));
-            continue;
-          }
-        }
-        break;
-      }
-    }
-    nonUniformIndices.push_back(nonUniformVal);
+    Value *nonUniformIndex = traceNonUniformIndex(nonUniformInst->getOperand(operandIdx));
+    nonUniformIndices.push_back(nonUniformIndex);
   }
-
-  // Save Builder's insert point, and set it to insert new code just before pNonUniformInst.
-  auto savedInsertPoint = saveIP();
-  SetInsertPoint(nonUniformInst);
 
   // For any index that is 64 bit, change it back to 32 bit for comparison at the top of the
   // waterfall loop.
   for (Value *&nonUniformVal : nonUniformIndices) {
-    if (nonUniformVal->getType()->isIntegerTy(64))
-      nonUniformVal = CreateTrunc(nonUniformVal, getInt32Ty());
+    if (nonUniformVal->getType()->isIntegerTy(64)) {
+      auto sExt = dyn_cast<SExtInst>(nonUniformVal);
+      // 64-bit index may already be formed from extension of 32-bit value.
+      if (sExt && sExt->getOperand(0)->getType()->isIntegerTy(32)) {
+        nonUniformVal = sExt->getOperand(0);
+      } else {
+        nonUniformVal = CreateTrunc(nonUniformVal, getInt32Ty());
+      }
+    }
   }
 
-  // The first begin contains a null token for the previous token argument
-  Value *waterfallBegin = ConstantInt::get(getInt32Ty(), 0);
-  for (auto nonUniformVal : nonUniformIndices) {
-    // Start the waterfall loop using the waterfall index.
+  // Find first index instruction and check if index instructions are identical.
+  Instruction *firstIndexInst = nullptr;
+  bool identicalIndexes = true;
+  for (Value *nonUniformVal : nonUniformIndices) {
+    Instruction *nuInst = dyn_cast<Instruction>(nonUniformVal);
+    if (!nuInst || (firstIndexInst && !instructionsEqual(nuInst, firstIndexInst))) {
+      identicalIndexes = false;
+      break;
+    }
+    if (!firstIndexInst || nuInst->comesBefore(firstIndexInst))
+      firstIndexInst = nuInst;
+  }
+
+  // Save Builder's insert point
+  auto savedInsertPoint = saveIP();
+
+  Value *waterfallBegin;
+  if (scalarizeDescriptorLoads && firstIndexInst && identicalIndexes) {
+    // Attempt to scalarize descriptor loads.
+
+    // Begin waterfall loop just after shared index is computed.
+    // This places all dependent instructions within the waterfall loop, including descriptor loads.
+    auto nonUniformVal = cast<Value>(firstIndexInst);
+    SetInsertPoint(firstIndexInst->getNextNonDebugInstruction(false));
+    waterfallBegin = ConstantInt::get(getInt32Ty(), 0);
     waterfallBegin = CreateIntrinsic(Intrinsic::amdgcn_waterfall_begin, nonUniformVal->getType(),
                                      {waterfallBegin, nonUniformVal}, nullptr, instName);
-  }
 
-  // Scalarize each non-uniform operand of the instruction.
-  for (unsigned operandIdx : operandIdxs) {
-    Value *desc = nonUniformInst->getOperand(operandIdx);
-    auto descTy = desc->getType();
-    desc = CreateIntrinsic(Intrinsic::amdgcn_waterfall_readfirstlane, {descTy, descTy}, {waterfallBegin, desc}, nullptr,
-                           instName);
-    if (nonUniformInst->getType()->isVoidTy()) {
-      // The buffer/image operation we are waterfalling is a store with no return value. Use
-      // llvm.amdgcn.waterfall.last.use on the descriptor.
-      desc = CreateIntrinsic(Intrinsic::amdgcn_waterfall_last_use, descTy, {waterfallBegin, desc}, nullptr, instName);
+    // Scalarize shared index.
+    auto descTy = nonUniformVal->getType();
+    Value *desc = CreateIntrinsic(Intrinsic::amdgcn_waterfall_readfirstlane, {descTy, descTy},
+                                  {waterfallBegin, nonUniformVal}, nullptr, instName);
+
+    // Replace all references to shared index within the waterfall loop with scalarized index.
+    // Loads using scalarized index will become scalar loads.
+    for (Value *otherNonUniformVal : nonUniformIndices) {
+      otherNonUniformVal->replaceUsesWithIf(desc, [desc, waterfallBegin, nonUniformInst](Use &U) {
+        Instruction *userInst = cast<Instruction>(U.getUser());
+        return U.getUser() != waterfallBegin && U.getUser() != desc && userInst->comesBefore(nonUniformInst);
+      });
     }
-    // Replace the descriptor operand in the buffer/image operation.
-    nonUniformInst->setOperand(operandIdx, desc);
+  } else {
+    // Insert new code just before pNonUniformInst.
+    SetInsertPoint(nonUniformInst);
+
+    // The first begin contains a null token for the previous token argument
+    waterfallBegin = ConstantInt::get(getInt32Ty(), 0);
+    for (auto nonUniformVal : nonUniformIndices) {
+      // Start the waterfall loop using the waterfall index.
+      waterfallBegin = CreateIntrinsic(Intrinsic::amdgcn_waterfall_begin, nonUniformVal->getType(),
+                                       {waterfallBegin, nonUniformVal}, nullptr, instName);
+    }
+
+    // Scalarize each non-uniform operand of the instruction.
+    for (unsigned operandIdx : operandIdxs) {
+      Value *desc = nonUniformInst->getOperand(operandIdx);
+      auto descTy = desc->getType();
+      desc = CreateIntrinsic(Intrinsic::amdgcn_waterfall_readfirstlane, {descTy, descTy}, {waterfallBegin, desc},
+                             nullptr, instName);
+      if (nonUniformInst->getType()->isVoidTy()) {
+        // The buffer/image operation we are waterfalling is a store with no return value. Use
+        // llvm.amdgcn.waterfall.last.use on the descriptor.
+        desc = CreateIntrinsic(Intrinsic::amdgcn_waterfall_last_use, descTy, {waterfallBegin, desc}, nullptr, instName);
+      }
+      // Replace the descriptor operand in the buffer/image operation.
+      nonUniformInst->setOperand(operandIdx, desc);
+    }
   }
 
   Instruction *resultValue = nonUniformInst;

llpc/test/shaderdb/core/OpTypeSampledImage_TestWaterfallScalarize.frag
@@ -1,0 +1,30 @@
+// Make sure that there is a single begin index
+// Make sure that there is a single waterfall.readfirstlane for the offset
+
+#version 450
+#extension GL_EXT_nonuniform_qualifier : require
+
+layout(set = 0, binding = 7) uniform sampler2D _11[];
+
+layout(location = 0) out vec4 _3;
+layout(location = 3) flat in int _4;
+layout(location = 1) flat in vec2 _6;
+
+void main()
+{
+    int _12 = _4;
+    _3 = texture(_11[nonuniformEXT(_12)], _6);
+}
+
+// BEGIN_SHADERTEST
+//
+// RUN: amdllpc -scalarize-waterfall-descriptor-loads -v %gfxip %s | FileCheck -check-prefix=SHADERTEST %s
+// SHADERTEST-LABEL: {{^// LLPC}} pipeline patching results
+// SHADERTEST: call i32 @llvm.amdgcn.waterfall.begin.i32
+// SHADERTEST-NOT: call i32 @llvm.amdgcn.waterfall.begin.i32
+// SHADERTEST: call i32 @llvm.amdgcn.waterfall.readfirstlane.i32.i32
+// SHADERTEST-NOT: call i32 @llvm.amdgcn.waterfall.readfirstlane.i32.i32
+// SHADERTEST: call {{.*}} <4 x float> @llvm.amdgcn.waterfall.end.v4f32
+// SHADERTEST: AMDLLPC SUCCESS
+//
+// END_SHADERTEST

include/vkgcDefs.h
@@ -82,6 +82,7 @@
 //  %Version History
 //  | %Version | Change Description                                                                                    |
 //  | -------- | ----------------------------------------------------------------------------------------------------- |
+//  |     53.6 | Add scalarizeWaterfallLoads to PipelineShaderOptions                                                  |
 //  |     53.5 | Add forceCsThreadIdSwizzling for thread id swizzle in 8*4                                             |
 //  |     53.4 | Add ldsSpillLimitDwords shader option                                                                 |
 //  |     53.3 | Add disableFastMathFlags shader option, plus support for this and fastMathFlags in pipeline files     |
@@ -668,6 +669,9 @@ struct PipelineShaderOptions {
 
   /// Maximum amount of LDS space to be used for spilling.
   unsigned ldsSpillLimitDwords;
+
+  /// Attempt to scalarize waterfall descriptor loads.
+  bool scalarizeWaterfallLoads;
 };
 
 /// Represents YCbCr sampler meta data in resource descriptor

lgc/builder/BuilderImpl.h
@@ -77,7 +77,7 @@ protected:
 
   // Create a waterfall loop containing the specified instruction.
   llvm::Instruction *createWaterfallLoop(llvm::Instruction *nonUniformInst, llvm::ArrayRef<unsigned> operandIdxs,
-                                         const llvm::Twine &instName = "");
+                                         bool scalarizeDescriptorLoads = false, const llvm::Twine &instName = "");
 
   // Helper method to scalarize a possibly vector unary operation
   llvm::Value *scalarize(llvm::Value *value, const std::function<llvm::Value *(llvm::Value *)> &callback);

lgc/builder/ImageBuilder.cpp
@@ -509,7 +509,8 @@ Value *ImageBuilder::CreateImageLoad(Type *resultTy, unsigned dim, unsigned flag
   // Add a waterfall loop if needed.
   Value *result = imageInst;
   if (flags & ImageFlagNonUniformImage)
-    result = createWaterfallLoop(imageInst, imageDescArgIndex);
+    result = createWaterfallLoop(imageInst, imageDescArgIndex,
+                                 getPipelineState()->getShaderOptions(m_shaderStage).scalarizeWaterfallLoads);
   else if (flags & ImageFlagEnforceReadFirstLaneImage)
     enforceReadFirstLane(imageInst, imageDescArgIndex);
 
@@ -701,7 +702,8 @@ Value *ImageBuilder::CreateImageStore(Value *texel, unsigned dim, unsigned flags
 
   // Add a waterfall loop if needed.
   if (flags & ImageFlagNonUniformImage)
-    createWaterfallLoop(imageStore, imageDescArgIndex);
+    createWaterfallLoop(imageStore, imageDescArgIndex,
+                        getPipelineState()->getShaderOptions(m_shaderStage).scalarizeWaterfallLoads);
   else if (flags & ImageFlagEnforceReadFirstLaneImage)
     enforceReadFirstLane(imageStore, imageDescArgIndex);
 
@@ -1182,7 +1184,8 @@ Value *ImageBuilder::CreateImageSampleGather(Type *resultTy, unsigned dim, unsig
     enforceReadFirstLane(imageOp, samplerDescArgIndex);
 
   if (!nonUniformArgIndexes.empty())
-    imageOp = createWaterfallLoop(imageOp, nonUniformArgIndexes);
+    imageOp = createWaterfallLoop(imageOp, nonUniformArgIndexes,
+                                  getPipelineState()->getShaderOptions(m_shaderStage).scalarizeWaterfallLoads);
   return imageOp;
 }
 
@@ -1288,7 +1291,8 @@ Value *ImageBuilder::CreateImageAtomicCommon(unsigned atomicOp, unsigned dim, un
         CreateIntrinsic(StructBufferAtomicIntrinsicTable[atomicOp], inputValue->getType(), args, nullptr, instName);
   }
   if (flags & ImageFlagNonUniformImage)
-    atomicInst = createWaterfallLoop(atomicInst, imageDescArgIndex);
+    atomicInst = createWaterfallLoop(atomicInst, imageDescArgIndex,
+                                     getPipelineState()->getShaderOptions(m_shaderStage).scalarizeWaterfallLoads);
   else if (flags & ImageFlagEnforceReadFirstLaneImage)
     enforceReadFirstLane(atomicInst, imageDescArgIndex);
 
@@ -1530,7 +1534,8 @@ Value *ImageBuilder::CreateImageGetLod(unsigned dim, unsigned flags, Value *imag
     enforceReadFirstLane(result, samplerDescArgIndex);
 
   if (!nonUniformArgIndexes.empty())
-    result = createWaterfallLoop(result, nonUniformArgIndexes);
+    result = createWaterfallLoop(result, nonUniformArgIndexes,
+                                 getPipelineState()->getShaderOptions(m_shaderStage).scalarizeWaterfallLoads);
 
   return result;
 }

lgc/interface/lgc/Pipeline.h
@@ -200,6 +200,9 @@ struct ShaderOptions {
   // Maximum amount of LDS space to be used for spilling.
   unsigned ldsSpillLimitDwords;
 
+  // Attempt to scalarize waterfall descriptor loads.
+  bool scalarizeWaterfallLoads;
+
   ShaderOptions() {
     // The memory representation of this struct gets written into LLVM metadata. To prevent uninitialized values from
     // being written, we force everything to 0, including alignment gaps.

llpc/context/llpcPipelineContext.cpp
@@ -135,6 +135,11 @@ static cl::opt<unsigned> LdsSpillLimitDwords("lds-spill-limit-dwords",
                                              cl::desc("Maximum amount of LDS space to be used for spilling"),
                                              cl::init(0));
 
+// -scalarize-waterfall-descriptor-loads: try to scalarize loads for non-uniform image sample descriptors
+static cl::opt<bool> ScalarizeWaterfallDescriptorLoads("scalarize-waterfall-descriptor-loads",
+                                                       cl::desc("Try to scalarize non-uniform descriptor loads"),
+                                                       cl::init(false));
+
 namespace Llpc {
 
 // =====================================================================================================================
@@ -374,6 +379,15 @@ void PipelineContext::setOptionsInPipeline(Pipeline *pipeline, Util::MetroHash64
       shaderOptions.vgprLimit = shaderInfo->options.vgprLimit;
     else
       shaderOptions.vgprLimit = VgprLimit;
+
+    if (ScalarizeWaterfallDescriptorLoads.getNumOccurrences() > 0) {
+      shaderOptions.scalarizeWaterfallLoads = ScalarizeWaterfallDescriptorLoads;
+    } else {
+      shaderOptions.scalarizeWaterfallLoads = shaderInfo->options.scalarizeWaterfallLoads;
+      // Enable waterfall load scalarization when vgpr limit is set.
+      if (shaderOptions.vgprLimit != 0 && shaderOptions.vgprLimit != UINT_MAX)
+        shaderOptions.scalarizeWaterfallLoads = true;
+    }
 
     if (shaderInfo->options.sgprLimit != 0 && shaderInfo->options.sgprLimit != UINT_MAX)
       shaderOptions.sgprLimit = shaderInfo->options.sgprLimit;

commit 3f9020836301795e5f5f99003b5ea3dc1aab757e

Peep through image desc modifications during waterfall generation.
An issue was introduced with PR #1827, where waterfall tracing
interacted incorrectly with workaround code that modifies image
descriptors on some GFX10.3 ASICs.
This would result in compilation errors for some shaders.

This patches fixes the issue in two ways:
 * The scalarizer code now correctly updates the non-uniform
   instruction with waterfall replacement values.
 * The tracing code can now peep through workarounds like those
   used on gfx1032, meaning waterfalls output should be identical
   on these ASICs.

lgc/builder/BuilderImpl.cpp
@@ -348,8 +348,36 @@ BranchInst *BuilderImplBase::createIf(Value *condition, bool wantElse, const Twi
 // @return : Value representing the non-uniform index
 static Value *traceNonUniformIndex(Value *nonUniformVal) {
   auto load = dyn_cast<LoadInst>(nonUniformVal);
-  if (!load)
-    return nonUniformVal;
+  if (!load) {
+    // Workarounds that modify image descriptor can be peeped through, i.e.
+    //   %baseValue = load <8 x i32>, <8 x i32> addrspace(4)* %..., align 16
+    //   %rawElement = extractelement <8 x i32> %baseValue, i64 6
+    //   %updatedElement = and i32 %rawElement, -1048577
+    //   %nonUniform = insertelement <8 x i32> %baseValue, i32 %updatedElement, i64 6
+    auto insert = dyn_cast<InsertElementInst>(nonUniformVal);
+    if (!insert)
+      return nonUniformVal;
+
+    load = dyn_cast<LoadInst>(insert->getOperand(0));
+    if (!load)
+      return nonUniformVal;
+
+    // We found the load, but must verify the chain.
+    // Consider updatedElement as a generic instruction or constant.
+    if (auto updatedElement = dyn_cast<Instruction>(insert->getOperand(1))) {
+      for (Value *operand : updatedElement->operands()) {
+        if (auto extract = dyn_cast<ExtractElementInst>(operand)) {
+          // Only dynamic value must be ExtractElementInst based on load.
+          if (dyn_cast<LoadInst>(extract->getOperand(0)) != load)
+            return nonUniformVal;
+        } else if (!isa<Constant>(operand)) {
+          return nonUniformVal;
+        }
+      }
+    } else if (!isa<Constant>(insert->getOperand(1))) {
+      return nonUniformVal;
+    }
+  }
 
   SmallVector<Value *, 2> worklist;
   Value *base = load->getOperand(0);
@@ -537,11 +565,13 @@ Instruction *BuilderImplBase::createWaterfallLoop(Instruction *nonUniformInst, A
                                   {waterfallBegin, nonUniformVal}, nullptr, instName);
 
     // Replace all references to shared index within the waterfall loop with scalarized index.
+    // (Note: this includes the non-uniform instruction itself.)
     // Loads using scalarized index will become scalar loads.
     for (Value *otherNonUniformVal : nonUniformIndices) {
       otherNonUniformVal->replaceUsesWithIf(desc, [desc, waterfallBegin, nonUniformInst](Use &U) {
         Instruction *userInst = cast<Instruction>(U.getUser());
-        return U.getUser() != waterfallBegin && U.getUser() != desc && userInst->comesBefore(nonUniformInst);
+        return U.getUser() != waterfallBegin && U.getUser() != desc &&
+               (userInst->comesBefore(nonUniformInst) || userInst == nonUniformInst);
       });
     }
   } else {

llpc/test/shaderdb/core/OpTypeSampledImage_TestWaterfallScalarize.frag
@@ -19,6 +19,9 @@ void main()
 // BEGIN_SHADERTEST
 //
 // RUN: amdllpc -scalarize-waterfall-descriptor-loads -v %gfxip %s | FileCheck -check-prefix=SHADERTEST %s
+// Explicitly check GFX10.3 ASIC variants:
+// RUN: amdllpc -scalarize-waterfall-descriptor-loads -v --gfxip=10.3.0 %s | FileCheck -check-prefix=SHADERTEST %s
+// RUN: amdllpc -scalarize-waterfall-descriptor-loads -v --gfxip=10.3.2 %s | FileCheck -check-prefix=SHADERTEST %s
 // SHADERTEST-LABEL: {{^// LLPC}} pipeline patching results
 // SHADERTEST: call i32 @llvm.amdgcn.waterfall.begin.i32
 // SHADERTEST-NOT: call i32 @llvm.amdgcn.waterfall.begin.i32