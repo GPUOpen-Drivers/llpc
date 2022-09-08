GPUOpen-Drivers/llpc

commit 2d28dce34f4808601ae6ccabd0395c0895340904

Refine the barycoord (#1980)
1. Separate barycoord from generic builtin variables, because it is
   requried to support interpAt*, so we need to pass more arguments.

2. Move IJCoord operation before the patch.

lgc/builder/InOutBuilder.cpp
@@ -768,6 +768,76 @@ Instruction *InOutBuilder::CreateWriteXfbOutput(Value *valueToWrite, bool isBuil
 }
 
 // =====================================================================================================================
+// Create a read of barycoord input value.
+// The type of the returned value is the fixed type of the specified built-in (see BuiltInDefs.h),
+//
+// @param builtIn : Built-in kind, BuiltInBaryCoord or BuiltInBaryCoordNoPerspKHR
+// @param inputInfo : Extra input info
+// @param auxInterpValue : Auxiliary value of interpolation
+// @param instName : Name to give instruction(s)
+Value *InOutBuilder::CreateReadBaryCoord(BuiltInKind builtIn, InOutInfo inputInfo, llvm::Value *auxInterpValue,
+                                         const llvm::Twine &instName) {
+  assert(builtIn == lgc::BuiltInBaryCoord || builtIn == lgc::BuiltInBaryCoordNoPerspKHR);
+
+  markBuiltInInputUsage(builtIn, 0);
+  if (getPipelineState()->getOptions().enableInterpModePatch && !auxInterpValue &&
+      inputInfo.getInterpLoc() != InOutInfo::InterpLocCentroid) {
+    auxInterpValue = readBuiltIn(false, BuiltInSampleId, {}, nullptr, nullptr, "");
+    inputInfo.setInterpLoc(InOutInfo::InterpLocSample);
+  }
+
+  bool isPersp = builtIn == lgc::BuiltInBaryCoord;
+  Value *evalArg = nullptr;
+  // Adjust ijCoord
+  switch (inputInfo.getInterpLoc()) {
+  case InOutInfo::InterpLocCentroid: {
+    std::string evalInstName = lgcName::InputImportBuiltIn;
+    if (isPersp) {
+      evalInstName += "InterpPerspCentroid";
+      evalArg = getInt32(BuiltInInterpPerspCentroid);
+    } else {
+      evalInstName += "InterpLinearCentroid";
+      evalArg = getInt32(BuiltInInterpLinearCentroid);
+    }
+    auxInterpValue =
+        CreateNamedCall(evalInstName, FixedVectorType::get(getFloatTy(), 2), {evalArg}, Attribute::ReadOnly);
+    break;
+  }
+  case InOutInfo::InterpLocSample: {
+    assert(auxInterpValue != nullptr);
+    auxInterpValue = readBuiltIn(false, BuiltInSamplePosOffset, {}, auxInterpValue, nullptr, "");
+    if (isPersp)
+      auxInterpValue = evalIjOffsetSmooth(auxInterpValue);
+    else
+      auxInterpValue = evalIjOffsetNoPersp(auxInterpValue);
+    break;
+  }
+  case InOutInfo::InterpLocCenter:
+  case InOutInfo::InterpLocUnknown: {
+    // When calling InterpolateAtOffset, auxInterpValue != nullptr
+    if (auxInterpValue) {
+      if (isPersp) {
+        auxInterpValue = evalIjOffsetSmooth(auxInterpValue);
+      } else {
+        auxInterpValue = evalIjOffsetNoPersp(auxInterpValue);
+      }
+    } else {
+      if (isPersp)
+        auxInterpValue = readBuiltIn(false, BuiltInInterpPerspCenter, {}, nullptr, nullptr, "");
+      else
+        auxInterpValue = readBuiltIn(false, BuiltInInterpLinearCenter, {}, nullptr, nullptr, "");
+    }
+    break;
+  }
+  default: {
+    llvm_unreachable("Should never be called!");
+  }
+  }
+
+  return normalizeBaryCoord(auxInterpValue);
+}
+
+// =====================================================================================================================
 // Create a read of (part of) a built-in input value.
 // The type of the returned value is the fixed type of the specified built-in (see BuiltInDefs.h),
 // or the element type if pIndex is not nullptr. For ClipDistance or CullDistance when pIndex is nullptr,
@@ -877,9 +947,7 @@ Value *InOutBuilder::readBuiltIn(bool isOutput, BuiltInKind builtIn, InOutInfo i
       Value *sampleNum = vertexIndex;
       vertexIndex = nullptr;
       args.push_back(sampleNum);
-    } else if (builtIn == BuiltInBaryCoord || builtIn == BuiltInBaryCoordNoPerspKHR)
-      // BuiltInBaryCoord requires interpolate mode.
-      args.push_back(getInt32(inOutInfo.getInterpLoc()));
+    }
     assert(!index && !vertexIndex);
     break;
   default:
@@ -898,6 +966,93 @@ Value *InOutBuilder::readBuiltIn(bool isOutput, BuiltInKind builtIn, InOutInfo i
     result->setName(instName);
 
   return result;
+}
+
+// =====================================================================================================================
+// Reorder and normalize the barycoord
+//
+// @param iJCoord : IJ coordinates provided for the HW interpolation view
+// @returns : gl_Barycoord
+Value *InOutBuilder::normalizeBaryCoord(Value *iJCoord) {
+  auto baryType = FixedVectorType::get(getFloatTy(), 3);
+  Value *barycoord = UndefValue::get(baryType);
+  auto one = ConstantFP::get(getFloatTy(), 1.0);
+  auto zero = ConstantFP::get(getFloatTy(), 0.0);
+
+  auto iCoord = CreateExtractElement(iJCoord, uint64_t(0));
+  auto jCoord = CreateExtractElement(iJCoord, 1);
+  auto kCoord = CreateFSub(ConstantFP::get(getFloatTy(), 1.0), iCoord);
+  kCoord = CreateFSub(kCoord, jCoord);
+
+  auto primType = m_pipelineState->getPrimitiveType();
+  auto provokingVertexMode = m_pipelineState->getRasterizerState().provokingVertexMode;
+  switch (primType) {
+  case PrimitiveType::Point: {
+    // Points
+    barycoord = ConstantVector::get({one, zero, zero});
+  }
+  case PrimitiveType::LineList:
+  case PrimitiveType::LineStrip: {
+    // Lines
+    // The weight of vertex0 is (1 - i - j), the weight of vertex1 is (i + j).
+    auto yCoord = CreateFAdd(iCoord, jCoord);
+    barycoord = CreateInsertElement(barycoord, kCoord, uint64_t(0));
+    barycoord = CreateInsertElement(barycoord, yCoord, 1);
+    barycoord = CreateInsertElement(barycoord, zero, 2);
+    break;
+  }
+  case PrimitiveType::TriangleList: {
+    // Triangles
+    // V0 ==> Attr_indx2
+    // V1 ==> Attr_indx0
+    // V2 ==> Attr_indx1
+    barycoord = CreateInsertElement(barycoord, iCoord, 2);
+    barycoord = CreateInsertElement(barycoord, jCoord, uint64_t(0));
+    barycoord = CreateInsertElement(barycoord, kCoord, 1);
+    break;
+  }
+  default: {
+    unsigned oddOffset = 0, evenOffset = 0;
+    Value *odd = UndefValue::get(baryType);
+    Value *even = UndefValue::get(baryType);
+    switch (primType) {
+    case PrimitiveType::TriangleFan: {
+      oddOffset = provokingVertexMode == ProvokingVertexLast ? 0 : 2;
+      evenOffset = provokingVertexMode == ProvokingVertexLast ? 2 : 1;
+      break;
+    }
+    case PrimitiveType::TriangleStrip:
+    case PrimitiveType::TriangleStripAdjacency: {
+      oddOffset = provokingVertexMode == ProvokingVertexLast ? 0 : 2;
+      evenOffset = 1;
+      break;
+    }
+    case PrimitiveType::TriangleListAdjacency: {
+      oddOffset = 0;
+      evenOffset = 1;
+      break;
+    }
+    default: {
+      llvm_unreachable("Should never be called!");
+      break;
+    }
+    }
+    odd = CreateInsertElement(odd, iCoord, oddOffset % 3);
+    odd = CreateInsertElement(odd, jCoord, (1 + oddOffset) % 3);
+    odd = CreateInsertElement(odd, kCoord, (2 + oddOffset) % 3);
+
+    even = CreateInsertElement(even, iCoord, evenOffset % 3);
+    even = CreateInsertElement(even, jCoord, (1 + evenOffset) % 3);
+    even = CreateInsertElement(even, kCoord, (2 + evenOffset) % 3);
+
+    // Select between them.
+    Value *primitiveId = readBuiltIn(false, BuiltInPrimitiveId, {}, nullptr, nullptr, "");
+    Value *parity = CreateTrunc(primitiveId, Type::getInt1Ty(getContext()));
+    barycoord = CreateSelect(parity, odd, even);
+    break;
+  }
+  }
+  return barycoord;
 }
 
 // =====================================================================================================================
@@ -1243,7 +1398,10 @@ Instruction *InOutBuilder::CreateWriteTaskPayload(Value *valueToWrite, Value *by
 Type *InOutBuilder::getBuiltInTy(BuiltInKind builtIn, InOutInfo inOutInfo) {
   switch (static_cast<unsigned>(builtIn)) {
   case BuiltInSamplePosOffset:
+  case BuiltInInterpPerspCenter:
   case BuiltInInterpLinearCenter:
+  case BuiltInInterpPerspCentroid:
+  case BuiltInInterpLinearCentroid:
     return FixedVectorType::get(getFloatTy(), 2);
   case BuiltInInterpPullMode:
     return FixedVectorType::get(getFloatTy(), 3);

lgc/builder/BuilderImpl.h
@@ -501,6 +501,10 @@ public:
                                           unsigned xfbBuffer, unsigned xfbStride, llvm::Value *xfbOffset,
                                           InOutInfo outputInfo) override final;
 
+  // Create a read of barycoord input value.
+  llvm::Value *CreateReadBaryCoord(BuiltInKind builtIn, InOutInfo inputInfo, llvm::Value *auxInterpValue,
+                                   const llvm::Twine &instName = "") override final;
+
   // Create a read of (part of) a built-in input value.
   llvm::Value *CreateReadBuiltInInput(BuiltInKind builtIn, InOutInfo inputInfo, llvm::Value *vertexIndex,
                                       llvm::Value *index, const llvm::Twine &instName = "") override final;
@@ -550,6 +554,9 @@ private:
   // Read (part of) a built-in value
   llvm::Value *readBuiltIn(bool isOutput, BuiltInKind builtIn, InOutInfo inOutInfo, llvm::Value *vertexIndex,
                            llvm::Value *index, const llvm::Twine &instName);
+
+  // Reorder the barycoord
+  llvm::Value *normalizeBaryCoord(llvm::Value *ijCoord);
 
   // Read and directly handle certain built-ins that are common between shader stages
   llvm::Value *readCommonBuiltIn(BuiltInKind builtIn, llvm::Type *resultTy, const llvm::Twine &instName = "");

lgc/builder/BuilderRecorder.cpp
@@ -168,6 +168,8 @@ StringRef BuilderRecorder::getCallName(Opcode opcode) {
     return "write.generic.output";
   case Opcode::WriteXfbOutput:
     return "write.xfb.output";
+  case Opcode::ReadBaryCoord:
+    return "read.bary.coord";
   case Opcode::ReadBuiltInInput:
     return "read.builtin.input";
   case Opcode::ReadBuiltInOutput:
@@ -1518,6 +1520,26 @@ Instruction *BuilderRecorder::CreateWriteXfbOutput(Value *valueToWrite, bool isB
 }
 
 // =====================================================================================================================
+// Create a read of barycoord input value.
+// The type of the returned value is the fixed type of the specified built-in (see BuiltInDefs.h),
+//
+// @param builtIn : Built-in kind, BuiltInBaryCoord or BuiltInBaryCoordNoPerspKHR
+// @param inputInfo : Extra input info
+// @param auxInterpValue : Auxiliary value of interpolation
+// @param instName : Name to give instruction(s)
+llvm::Value *BuilderRecorder::CreateReadBaryCoord(BuiltInKind builtIn, InOutInfo inputInfo, llvm::Value *auxInterpValue,
+                                                  const llvm::Twine &instName) {
+  Type *resultTy = getBuiltInTy(builtIn, inputInfo);
+  return record(Opcode::ReadBaryCoord, resultTy,
+                {
+                    getInt32(builtIn),
+                    getInt32(inputInfo.getData()),
+                    auxInterpValue ? auxInterpValue : PoisonValue::get(getInt32Ty()),
+                },
+                instName);
+}
+
+// =====================================================================================================================
 // Create a read of (part of) a built-in input value.
 // The type of the returned value is the fixed type of the specified built-in (see BuiltInDefs.h),
 // or the element type if pIndex is not nullptr.
@@ -2062,6 +2084,7 @@ Instruction *BuilderRecorder::record(BuilderRecorder::Opcode opcode, Type *resul
     case Opcode::ImageSampleConvert:
     case Opcode::LoadBufferDesc:
     case Opcode::LoadPushConstantsPtr:
+    case Opcode::ReadBaryCoord:
     case Opcode::ReadBuiltInInput:
     case Opcode::ReadBuiltInOutput:
     case Opcode::ReadGenericInput:

lgc/builder/BuilderReplayer.cpp
@@ -661,6 +661,14 @@ Value *BuilderReplayer::processCall(unsigned opcode, CallInst *call) {
                                            outputInfo);
   }
 
+  case BuilderRecorder::Opcode::ReadBaryCoord: {
+    auto builtIn = static_cast<BuiltInKind>(cast<ConstantInt>(args[0])->getZExtValue());
+    InOutInfo inputInfo(cast<ConstantInt>(args[1])->getZExtValue());
+    return m_builder->CreateReadBaryCoord(builtIn,                                         // BuiltIn
+                                          inputInfo,                                       // Input info
+                                          isa<UndefValue>(args[2]) ? nullptr : &*args[2]); // auxInterpValue
+  }
+
   case BuilderRecorder::Opcode::ReadBuiltInInput: {
     auto builtIn = static_cast<BuiltInKind>(cast<ConstantInt>(args[0])->getZExtValue());
     InOutInfo inputInfo(cast<ConstantInt>(args[1])->getZExtValue());

lgc/include/lgc/builder/BuilderRecorder.h
@@ -159,6 +159,7 @@ public:
     ReadPerVertexInput,
     WriteGenericOutput,
     WriteXfbOutput,
+    ReadBaryCoord,
     ReadBuiltInInput,
     ReadBuiltInOutput,
     WriteBuiltInOutput,
@@ -454,6 +455,10 @@ public:
   llvm::Instruction *CreateWriteXfbOutput(llvm::Value *valueToWrite, bool isBuiltIn, unsigned location,
                                           unsigned xfbBuffer, unsigned xfbStride, llvm::Value *xfbOffset,
                                           InOutInfo outputInfo) override final;
+
+  // Create a read of barycoord input value.
+  llvm::Value *CreateReadBaryCoord(BuiltInKind builtIn, InOutInfo inputInfo, llvm::Value *auxInterpValue,
+                                   const llvm::Twine &instName = "") override final;
 
   // Create a read of (part of) a built-in input value.
   llvm::Value *CreateReadBuiltInInput(BuiltInKind builtIn, InOutInfo inputInfo, llvm::Value *vertexIndex,

lgc/interface/lgc/Builder.h
@@ -1207,6 +1207,16 @@ public:
   // @param inOutInfo : Extra input/output info (shader-defined array length)
   llvm::Type *getBuiltInTy(BuiltInKind builtIn, InOutInfo inOutInfo);
 
+  // Create a read of barycoord input value.
+  // The type of the returned value is the fixed type of the specified built-in (see BuiltInDefs.h),
+  //
+  // @param builtIn : Built-in kind, BuiltInBaryCoord or BuiltInBaryCoordNoPerspKHR
+  // @param inputInfo : Extra input info
+  // @param auxInterpValue : Auxiliary value of interpolation
+  // @param instName : Name to give instruction(s)
+  virtual llvm::Value *CreateReadBaryCoord(BuiltInKind builtIn, InOutInfo inputInfo, llvm::Value *auxInterpValue,
+                                           const llvm::Twine &instName = "") = 0;
+
   // Create a read of (part of) a built-in input value.
   // The type of the returned value is the fixed type of the specified built-in (see BuiltInDefs.h),
   // or the element type if pIndex is not nullptr. For ClipDistance or CullDistance when pIndex is nullptr,

lgc/patch/PatchInOutImportExport.cpp
@@ -2727,146 +2727,6 @@ Value *PatchInOutImportExport::patchFsBuiltInInputImport(Type *inputTy, unsigned
     input = getSamplePosition(inputTy, insertPos);
     break;
   }
-  case BuiltInBaryCoord:
-  case BuiltInBaryCoordNoPerspKHR: {
-    assert(isa<ConstantInt>(generalVal));
-    unsigned idx = 0;
-    unsigned interpLoc = cast<ConstantInt>(generalVal)->getZExtValue();
-    Value *iJCoord = nullptr;
-    if (interpLoc == InOutInfo::InterpLocCentroid) {
-      if (BuiltInBaryCoord == builtInId)
-        iJCoord = adjustCentroidIj(getFunctionArgument(m_entryPoint, entryArgIdxs.perspInterp.centroid),
-                                   getFunctionArgument(m_entryPoint, entryArgIdxs.perspInterp.center), insertPos);
-      else
-        iJCoord = adjustCentroidIj(getFunctionArgument(m_entryPoint, entryArgIdxs.linearInterp.centroid),
-                                   getFunctionArgument(m_entryPoint, entryArgIdxs.linearInterp.center), insertPos);
-    } else if (interpLoc == InOutInfo::InterpLocSample) {
-      idx = (BuiltInBaryCoord == builtInId) ? entryArgIdxs.perspInterp.sample : entryArgIdxs.linearInterp.sample;
-      iJCoord = getFunctionArgument(m_entryPoint, idx);
-    } else {
-      idx = (BuiltInBaryCoord == builtInId) ? entryArgIdxs.perspInterp.center : entryArgIdxs.linearInterp.center;
-      iJCoord = getFunctionArgument(m_entryPoint, idx);
-    }
-
-    auto iCoord = builder.CreateExtractElement(iJCoord, uint64_t(0));
-    auto jCoord = builder.CreateExtractElement(iJCoord, 1);
-    auto primType = m_pipelineState->getPrimitiveType();
-    auto provokingVertexMode = m_pipelineState->getRasterizerState().provokingVertexMode;
-    switch (primType) {
-    case PrimitiveType::Point: {
-      // Points
-      input = builder.CreateInsertElement(input, ConstantFP::get(builder.getFloatTy(), 1.0), uint64_t(0));
-      input = builder.CreateInsertElement(input, ConstantFP::get(builder.getFloatTy(), 0.0), 1);
-      input = builder.CreateInsertElement(input, ConstantFP::get(builder.getFloatTy(), 0.0), 2);
-      break;
-    }
-    case PrimitiveType::LineList:
-    case PrimitiveType::LineStrip: {
-      // Lines
-      // The weight of vertex0 is (1 - i - j), the weight of vertex1 is (i + j).
-      auto kCoord = builder.CreateFSub(ConstantFP::get(builder.getFloatTy(), 1.0), iCoord);
-      kCoord = builder.CreateFSub(kCoord, jCoord);
-      jCoord = builder.CreateFAdd(iCoord, jCoord);
-      input = builder.CreateInsertElement(input, kCoord, uint64_t(0));
-      input = builder.CreateInsertElement(input, jCoord, 1);
-      input = builder.CreateInsertElement(input, ConstantFP::get(builder.getFloatTy(), 0.0), 2);
-      break;
-    }
-    case PrimitiveType::TriangleList: {
-      // Triangles
-      // V0 ==> Attr_indx2
-      // V1 ==> Attr_indx0
-      // V2 ==> Attr_indx1
-      auto kCoord = builder.CreateFSub(ConstantFP::get(builder.getFloatTy(), 1.0), iCoord);
-      kCoord = builder.CreateFSub(kCoord, jCoord);
-      input = builder.CreateInsertElement(input, iCoord, 2);
-      input = builder.CreateInsertElement(input, jCoord, uint64_t(0));
-      input = builder.CreateInsertElement(input, kCoord, 1);
-      break;
-    }
-    case PrimitiveType::TriangleFan: {
-      Value *odd = UndefValue::get(inputTy);
-      Value *even = UndefValue::get(inputTy);
-      auto kCoord = builder.CreateFSub(ConstantFP::get(builder.getFloatTy(), 1.0), iCoord);
-      kCoord = builder.CreateFSub(kCoord, jCoord);
-      if (provokingVertexMode == ProvokingVertexLast) {
-        odd = builder.CreateInsertElement(odd, iCoord, uint64_t(0));
-        odd = builder.CreateInsertElement(odd, jCoord, 1);
-        odd = builder.CreateInsertElement(odd, kCoord, 2);
-
-        even = builder.CreateInsertElement(even, iCoord, 2);
-        even = builder.CreateInsertElement(even, jCoord, uint64_t(0));
-        even = builder.CreateInsertElement(even, kCoord, 1);
-      } else {
-        odd = builder.CreateInsertElement(odd, iCoord, 2);
-        odd = builder.CreateInsertElement(odd, jCoord, uint64_t(0));
-        odd = builder.CreateInsertElement(odd, kCoord, 1);
-
-        even = builder.CreateInsertElement(even, iCoord, 1);
-        even = builder.CreateInsertElement(even, jCoord, 2);
-        even = builder.CreateInsertElement(even, kCoord, uint64_t(0));
-      }
-      auto primitiveId = patchFsBuiltInInputImport(builder.getInt32Ty(), BuiltInPrimitiveId, nullptr, insertPos);
-      auto evenV = builder.CreateSRem(primitiveId, builder.getInt32(2));
-      Value *con = builder.CreateICmpEQ(evenV, builder.getInt32(0));
-      input = builder.CreateSelect(con, even, odd);
-      break;
-    }
-    case PrimitiveType::TriangleStrip:
-    case PrimitiveType::TriangleStripAdjacency: {
-      Value *odd = UndefValue::get(inputTy);
-      Value *even = UndefValue::get(inputTy);
-      auto kCoord = builder.CreateFSub(ConstantFP::get(builder.getFloatTy(), 1.0), iCoord);
-      kCoord = builder.CreateFSub(kCoord, jCoord);
-      if (provokingVertexMode == ProvokingVertexLast) {
-        odd = builder.CreateInsertElement(odd, iCoord, uint64_t(0));
-        odd = builder.CreateInsertElement(odd, jCoord, 1);
-        odd = builder.CreateInsertElement(odd, kCoord, 2);
-
-        even = builder.CreateInsertElement(even, iCoord, 1);
-        even = builder.CreateInsertElement(even, jCoord, 2);
-        even = builder.CreateInsertElement(even, kCoord, uint64_t(0));
-      } else {
-        odd = builder.CreateInsertElement(odd, iCoord, 2);
-        odd = builder.CreateInsertElement(odd, jCoord, uint64_t(0));
-        odd = builder.CreateInsertElement(odd, kCoord, 1);
-
-        even = builder.CreateInsertElement(even, iCoord, 1);
-        even = builder.CreateInsertElement(even, jCoord, 2);
-        even = builder.CreateInsertElement(even, kCoord, uint64_t(0));
-      }
-      auto primitiveId = patchFsBuiltInInputImport(builder.getInt32Ty(), BuiltInPrimitiveId, nullptr, insertPos);
-      auto evenV = builder.CreateSRem(primitiveId, builder.getInt32(2));
-      Value *con = builder.CreateICmpEQ(evenV, builder.getInt32(0));
-      input = builder.CreateSelect(con, even, odd);
-      break;
-    }
-    case PrimitiveType::TriangleListAdjacency: {
-      auto kCoord = builder.CreateFSub(ConstantFP::get(builder.getFloatTy(), 1.0), iCoord);
-      kCoord = builder.CreateFSub(kCoord, jCoord);
-      Value *odd = UndefValue::get(inputTy);
-      odd = builder.CreateInsertElement(odd, iCoord, uint64_t(0));
-      odd = builder.CreateInsertElement(odd, jCoord, 1);
-      odd = builder.CreateInsertElement(odd, kCoord, 2);
-
-      Value *even = UndefValue::get(inputTy);
-      even = builder.CreateInsertElement(even, iCoord, 1);
-      even = builder.CreateInsertElement(even, jCoord, 2);
-      even = builder.CreateInsertElement(even, kCoord, uint64_t(0));
-
-      auto primitiveId = patchFsBuiltInInputImport(builder.getInt32Ty(), BuiltInPrimitiveId, nullptr, insertPos);
-      auto evenV = builder.CreateSRem(primitiveId, builder.getInt32(2));
-      Value *con = builder.CreateICmpEQ(evenV, builder.getInt32(0));
-      input = builder.CreateSelect(con, even, odd);
-      break;
-    }
-    default: {
-      llvm_unreachable("Should never be called!");
-      break;
-    }
-    }
-    break;
-  }
   default: {
     llvm_unreachable("Should never be called!");
     break;

lgc/state/PipelineState.cpp
@@ -1472,6 +1472,12 @@ StringRef PipelineState::getBuiltInName(BuiltInKind builtIn) {
     return "InterpLinearCenter";
   case BuiltInInterpPullMode:
     return "InterpPullMode";
+  case BuiltInInterpPerspCenter:
+    return "InterpPerspCenter";
+  case BuiltInInterpPerspCentroid:
+    return "InterpPerspCentroid";
+  case BuiltInInterpLinearCentroid:
+    return "InterpLinearCenteroid";
 
   default:
     llvm_unreachable("Should never be called!");

commit f6f6c185e8c5b1087a0a0923e2fdf9f7440e57bf

lgc/InOutBuilder: add missing break to prevent fall-through
Fixes: 2d28dce34f48 ("Refine the barycoord (#1980)")

lgc/builder/InOutBuilder.cpp
@@ -991,6 +991,7 @@ Value *InOutBuilder::normalizeBaryCoord(Value *iJCoord) {
   case PrimitiveType::Point: {
     // Points
     barycoord = ConstantVector::get({one, zero, zero});
+    break;
   }
   case PrimitiveType::LineList:
   case PrimitiveType::LineStrip: {