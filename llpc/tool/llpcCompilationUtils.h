d7e07490357b3f62e817eb11f64c3c2f8c7f7382
Jay Foad <jay.foad@amd.com>
2023-01-16 16:45:32 +0000

    Use std::optional instead of llvm::Optional (#2225)

--- llpc/tool/llpcCompilationUtils.h
+++ llpc/tool/llpcCompilationUtils.h
@@ -103,7 +103,7 @@ struct CompileInfo {
   bool robustBufferAccess;        // Whether to enable robust buffer access
   bool scratchAccessBoundsChecks; // Whether to enable scratch access bounds checks
   VfxPipelineType pipelineType;   // Pipeline type
-  llvm::Optional<llvm::CodeGenOpt::Level> optimizationLevel; // The optimization level to pass the compiler
+  std::optional<llvm::CodeGenOpt::Level> optimizationLevel; // The optimization level to pass the compiler
 #if VKI_RAY_TRACING
   bool internalRtShaders; // Whether to enable intrinsics for internal RT shaders
 #endif
