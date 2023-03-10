GPUOpen-Drivers/llpc

commit c1e683e022b26b8036770881064720485689a08a

lgc: dialectify input.import and output.import ops
Use convenience classes with meaningful method names to access these
operations instead of shader-stage-specific argument indices.

Input imports now use only two different ops:

- input.import.generic for all vertex processing shader stages and for
  FS per-primitive inputs (in mesh shading pipelines)
- input.import.interpolated for "interpolated" FS inputs (those that are
  per-vertex outputs of the last vertex processing stage)

Also update llvm-dialects to a newer version to pick up some fixes
required for the use of OpClass in the dialect definition.

v2:
- address review comments

lgc/patch/VertexFetch.cpp
@@ -30,6 +30,7 @@
  */
 #include "lgc/patch/VertexFetch.h"
 #include "lgc/LgcContext.h"
+#include "lgc/LgcDialect.h"
 #include "lgc/builder/BuilderImpl.h"
 #include "lgc/patch/Patch.h"
 #include "lgc/patch/ShaderInputs.h"
@@ -39,6 +40,7 @@
 #include "lgc/state/TargetInfo.h"
 #include "lgc/util/BuilderBase.h"
 #include "lgc/util/Internal.h"
+#include "llvm-dialects/Dialect/Visitor.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicsAMDGPU.h"
@@ -545,20 +547,22 @@ PreservedAnalyses LowerVertexFetch::run(Module &module, ModuleAnalysisManager &a
 // @param pipelineState : Pipeline state
 // @returns : True if the module was modified by the transformation and false otherwise
 bool LowerVertexFetch::runImpl(Module &module, PipelineState *pipelineState) {
-  std::unique_ptr<VertexFetch> vertexFetch(VertexFetch::create(pipelineState->getLgcContext()));
-
   // Gather vertex fetch calls. We can assume they're all in one function, the vertex shader.
   // We can assume that multiple fetches of the same location, component and type have been CSEd.
-  SmallVector<CallInst *, 8> vertexFetches;
-  BuilderBase builder(module.getContext());
-  for (auto &func : module) {
-    if (!func.isDeclaration() || !func.getName().startswith(lgcName::InputImportVertex))
-      continue;
-    for (auto user : func.users())
-      vertexFetches.push_back(cast<CallInst>(user));
-  }
+  SmallVector<InputImportGenericOp *, 8> vertexFetches;
+  static const auto fetchVisitor = llvm_dialects::VisitorBuilder<SmallVectorImpl<InputImportGenericOp *>>()
+                                       .setStrategy(llvm_dialects::VisitorStrategy::ByFunctionDeclaration)
+                                       .add<InputImportGenericOp>([](auto &fetches, InputImportGenericOp &op) {
+                                         if (lgc::getShaderStage(op.getFunction()) == ShaderStageVertex)
+                                           fetches.push_back(&op);
+                                       })
+                                       .build();
+  fetchVisitor.visit(vertexFetches, module);
   if (vertexFetches.empty())
     return false;
+
+  std::unique_ptr<VertexFetch> vertexFetch(VertexFetch::create(pipelineState->getLgcContext()));
+  BuilderBase builder(module.getContext());
 
   if (pipelineState->getOptions().enableUberFetchShader) {
     // NOTE: The 10_10_10_2 formats are not supported by the uber fetch shader on gfx9 and older.
@@ -586,26 +590,30 @@ bool LowerVertexFetch::runImpl(Module &module, PipelineState *pipelineState) {
   if (!pipelineState->isUnlinked() || !pipelineState->getVertexInputDescriptions().empty()) {
     // Whole-pipeline compilation (or shader compilation where we were given the vertex input descriptions).
     // Lower each vertex fetch.
-    for (CallInst *call : vertexFetches) {
+    for (InputImportGenericOp *fetch : vertexFetches) {
       Value *vertex = nullptr;
 
       // Find the vertex input description.
-      unsigned location = cast<ConstantInt>(call->getArgOperand(0))->getZExtValue();
-      unsigned component = cast<ConstantInt>(call->getArgOperand(1))->getZExtValue();
+      unsigned location = fetch->getLocation();
+      unsigned component = cast<ConstantInt>(fetch->getElemIdx())->getZExtValue();
+
+      assert(!fetch->getPerPrimitive());
+      assert(cast<ConstantInt>(fetch->getLocOffset())->isZero());
+
       const VertexInputDescription *description = pipelineState->findVertexInputDescription(location);
 
       if (!description) {
         // If we could not find vertex input info matching this location, just return undefined value.
-        vertex = UndefValue::get(call->getType());
+        vertex = UndefValue::get(fetch->getType());
       } else {
         // Fetch the vertex.
-        builder.SetInsertPoint(call);
-        vertex = vertexFetch->fetchVertex(call->getType(), description, location, component, builder);
+        builder.SetInsertPoint(fetch);
+        vertex = vertexFetch->fetchVertex(fetch->getType(), description, location, component, builder);
       }
 
       // Replace and erase this call.
-      call->replaceAllUsesWith(vertex);
-      call->eraseFromParent();
+      fetch->replaceAllUsesWith(vertex);
+      fetch->eraseFromParent();
     }
 
     return true;
@@ -619,12 +627,16 @@ bool LowerVertexFetch::runImpl(Module &module, PipelineState *pipelineState) {
   SmallVector<VertexFetchInfo, 8> info;
   SmallVector<Type *, 8> argTys;
   SmallVector<std::string, 8> argNames;
-  for (CallInst *call : vertexFetches) {
-    unsigned location = cast<ConstantInt>(call->getArgOperand(0))->getZExtValue();
-    unsigned component = cast<ConstantInt>(call->getArgOperand(1))->getZExtValue();
-    info.push_back({location, component, call->getType()});
+  for (InputImportGenericOp *fetch : vertexFetches) {
+    unsigned location = fetch->getLocation();
+    unsigned component = cast<ConstantInt>(fetch->getElemIdx())->getZExtValue();
 
-    Type *ty = call->getType();
+    assert(!fetch->getPerPrimitive());
+    assert(cast<ConstantInt>(fetch->getLocOffset())->isZero());
+
+    info.push_back({location, component, fetch->getType()});
+
+    Type *ty = fetch->getType();
     // The return value from the fetch shader needs to use all floats, as the back-end maps an int in the
     // return value as an SGPR rather than a VGPR. For symmetry, we also use all floats here, in the input
     // args to the fetchless vertex shader.
@@ -637,21 +649,21 @@ bool LowerVertexFetch::runImpl(Module &module, PipelineState *pipelineState) {
   // Mutate the vertex shader function to add the new args.
   Function *newFunc = addFunctionArgs(vertexFetches[0]->getFunction(), nullptr, argTys, argNames);
 
-  // Hook up each vertex fetch call to the corresponding arg.
+  // Hook up each vertex fetch to the corresponding arg.
   for (unsigned idx = 0; idx != vertexFetches.size(); ++idx) {
-    CallInst *call = vertexFetches[idx];
+    InputImportGenericOp *fetch = vertexFetches[idx];
     Value *vertex = newFunc->getArg(idx);
-    if (call->getType() != vertex->getType()) {
+    if (fetch->getType() != vertex->getType()) {
       // We changed to an all-float type above.
-      builder.SetInsertPoint(call);
-      Type *elementTy = call->getType()->getScalarType();
+      builder.SetInsertPoint(fetch);
+      Type *elementTy = fetch->getType()->getScalarType();
       unsigned numElements = vertex->getType()->getPrimitiveSizeInBits() / elementTy->getPrimitiveSizeInBits();
       vertex =
           builder.CreateBitCast(vertex, numElements == 1 ? elementTy : FixedVectorType::get(elementTy, numElements));
-      if (call->getType() != vertex->getType()) {
-        // The types are now vectors of the same element type but different element counts, or call->getType()
+      if (fetch->getType() != vertex->getType()) {
+        // The types are now vectors of the same element type but different element counts, or fetch->getType()
         // is scalar.
-        if (auto vecTy = dyn_cast<FixedVectorType>(call->getType())) {
+        if (auto vecTy = dyn_cast<FixedVectorType>(fetch->getType())) {
           int indices[] = {0, 1, 2, 3};
           vertex =
               builder.CreateShuffleVector(vertex, vertex, ArrayRef<int>(indices).slice(0, vecTy->getNumElements()));
@@ -661,8 +673,8 @@ bool LowerVertexFetch::runImpl(Module &module, PipelineState *pipelineState) {
       }
     }
     vertex->setName("vertex" + Twine(info[idx].location) + "." + Twine(info[idx].component));
-    call->replaceAllUsesWith(vertex);
-    call->eraseFromParent();
+    fetch->replaceAllUsesWith(vertex);
+    fetch->eraseFromParent();
   }
 
   return true;

lgc/builder/InOutBuilder.cpp
@@ -29,6 +29,7 @@
  ***********************************************************************************************************************
  */
 #include "lgc/LgcContext.h"
+#include "lgc/LgcDialect.h"
 #include "lgc/builder/BuilderImpl.h"
 #include "lgc/patch/ShaderInputs.h"
 #include "lgc/state/AbiUnlinked.h"
@@ -102,16 +103,8 @@ Value *InOutBuilder::CreateReadPerVertexInput(Type *resultTy, unsigned location,
 
   // Lambda to do the actual input read.
   auto readInput = [&](Value *vertexIndex) {
-    std::string callName = lgcName::InputImportInterpolant;
-    SmallVector<Value *, 5> args({
-        getInt32(location),
-        locationOffset,
-        elemIdx,
-        getInt32(InOutInfo::InterpModeCustom),
-        vertexIndex,
-    });
-    addTypeMangling(resultTy, args, callName);
-    return CreateNamedCall(callName, resultTy, args, {Attribute::ReadOnly, Attribute::WillReturn});
+    return create<InputImportInterpolatedOp>(resultTy, false, location, locationOffset, elemIdx,
+                                             PoisonValue::get(getInt32Ty()), InOutInfo::InterpModeCustom, vertexIndex);
   };
 
   unsigned oddOffset = 0, evenOffset = 0;
@@ -222,75 +215,53 @@ Value *InOutBuilder::readGenericInputOutput(bool isOutput, Type *resultTy, unsig
   markGenericInputOutputUsage(isOutput, location, locationCount, inOutInfo, vertexIndex);
 
   // Generate LLPC call for reading the input/output.
-  StringRef baseCallName = lgcName::InputImportGeneric;
-  SmallVector<Value *, 6> args;
+  Value *result = nullptr;
   switch (m_shaderStage) {
   case ShaderStageVertex: {
-    // VS:  @lgc.input.import.vertex.%Type%(i32 location, i32 elemIdx)
     assert(locationOffset == getInt32(0));
-    baseCallName = lgcName::InputImportVertex;
-    args.push_back(getInt32(location));
-    args.push_back(elemIdx);
+    result =
+        create<InputImportGenericOp>(resultTy, false, location, getInt32(0), elemIdx, PoisonValue::get(getInt32Ty()));
     break;
   }
 
   case ShaderStageTessControl:
   case ShaderStageTessEval: {
-    // TCS: @lgc.{input|output}.import.generic.%Type%(i32 location, i32 locOffset, i32 elemIdx, i32 vertexIdx)
-    // TES: @lgc.input.import.generic.%Type%(i32 location, i32 locOffset, i32 elemIdx, i32 vertexIdx)
     assert(!isOutput || m_shaderStage == ShaderStageTessControl);
-    args.push_back(getInt32(location));
-    args.push_back(locationOffset);
-    args.push_back(elemIdx);
-    args.push_back(vertexIndex ? vertexIndex : getInt32(InvalidValue));
+    bool isPerPrimitive = vertexIndex == nullptr;
+
+    if (!vertexIndex)
+      vertexIndex = PoisonValue::get(getInt32Ty());
+
     if (isOutput)
-      baseCallName = lgcName::OutputImportGeneric;
+      result = create<OutputImportGenericOp>(resultTy, isPerPrimitive, location, locationOffset, elemIdx, vertexIndex);
+    else
+      result = create<InputImportGenericOp>(resultTy, isPerPrimitive, location, locationOffset, elemIdx, vertexIndex);
     break;
   }
 
   case ShaderStageGeometry: {
-    // GS:  @lgc.input.import.generic.%Type%(i32 location, i32 elemIdx, i32 vertexIdx)
-    assert(locationOffset == getInt32(0));
-    args.push_back(getInt32(location));
-    args.push_back(elemIdx);
-    args.push_back(vertexIndex ? vertexIndex : getInt32(InvalidValue));
+    assert(cast<ConstantInt>(locationOffset)->isZero());
+    assert(vertexIndex);
+    result = create<InputImportGenericOp>(resultTy, false, location, locationOffset, elemIdx, vertexIndex);
     break;
   }
 
   case ShaderStageFragment: {
-    // FS:  @lgc.input.import.generic.%Type%(i32 location, i32 elemIdx) -- for per-primitive
-    //      @lgc.input.import.interpolant.%Type%(i32 location, i32 locOffset, i32 elemIdx,
-    //                                           i32 interpMode, <2 x float> | i32 interpValue) -- for per-vertex
-    //        interpMode is one of:
-    //         - InterpModeSmooth: interpValue is I,J
-    //         - InterpModeCustom: interpValue is vertex index
-    //         - InterpModeFlat: interpValue is ignored
     if (inOutInfo.isPerPrimitive()) {
       assert(locationOffset == getInt32(0));
-      args.push_back(getInt32(location));
-      args.push_back(elemIdx);
+      result = create<InputImportGenericOp>(resultTy, true, location, locationOffset, elemIdx,
+                                            PoisonValue::get(getInt32Ty()));
     } else {
-      baseCallName = lgcName::InputImportInterpolant;
-
-      args.push_back(getInt32(location));
-      args.push_back(locationOffset);
-      args.push_back(elemIdx);
-
       auto [interpMode, interpValue] = getInterpModeAndValue(inOutInfo, vertexIndex);
-      args.push_back(getInt32(interpMode));
-      args.push_back(interpValue);
+      result = create<InputImportInterpolatedOp>(resultTy, false, location, locationOffset, elemIdx,
+                                                 PoisonValue::get(getInt32Ty()), interpMode, interpValue);
     }
     break;
   }
 
   default:
     llvm_unreachable("Should never be called!");
-    break;
   }
-
-  std::string callName(baseCallName);
-  addTypeMangling(resultTy, args, callName);
-  Value *result = CreateNamedCall(callName, resultTy, args, {Attribute::ReadOnly, Attribute::WillReturn});
 
   result->setName(instName);
   return result;

lgc/include/lgc/patch/PatchResourceCollect.h
@@ -38,6 +38,9 @@
 
 namespace lgc {
 
+class GenericLocationOp;
+class OutputImportGenericOp;
+
 class InOutLocationInfoMapManager;
 typedef std::map<InOutLocationInfo, InOutLocationInfo> InOutLocationInfoMap;
 
@@ -92,7 +95,7 @@ private:
 
   // Input/output scalarizing
   void scalarizeForInOutPacking(llvm::Module *module);
-  void scalarizeGenericInput(llvm::CallInst *call);
+  void scalarizeGenericInput(GenericLocationOp *input);
   void scalarizeGenericOutput(llvm::CallInst *call);
 
   PipelineShadersResult *m_pipelineShaders; // Pipeline shaders
@@ -105,9 +108,9 @@ private:
 
   std::unordered_set<unsigned> m_importedOutputBuiltIns; // IDs of imported built-in outputs
 
-  std::vector<llvm::CallInst *> m_importedOutputCalls; // The output import calls
-  std::vector<llvm::CallInst *> m_inputCalls;          // The input import calls
-  std::vector<llvm::CallInst *> m_outputCalls;         // The output export calls
+  std::vector<OutputImportGenericOp *> m_importedOutputCalls; // The output import calls
+  std::vector<GenericLocationOp *> m_inputCalls;              // The input import calls
+  std::vector<llvm::CallInst *> m_outputCalls;                // The output export calls
 
   ResourceUsage *m_resUsage; // Pointer to shader resource usage
   std::unique_ptr<InOutLocationInfoMapManager>
@@ -135,7 +138,7 @@ class InOutLocationInfoMapManager {
 public:
   InOutLocationInfoMapManager() {}
 
-  void createMap(const std::vector<llvm::CallInst *> &calls, ShaderStage shaderStage, bool requireDword);
+  void createMap(llvm::ArrayRef<GenericLocationOp *> calls, ShaderStage shaderStage, bool requireDword);
   void createMap(const std::vector<InOutLocationInfo> &locInfos, ShaderStage shaderStage);
   void deserializeMap(llvm::ArrayRef<std::pair<unsigned, unsigned>> serializedMap);
   bool findMap(const InOutLocationInfo &origLocInfo, InOutLocationInfoMap::const_iterator &mapIt);

lgc/include/lgc/state/Defs.h
@@ -57,12 +57,8 @@ static const BuiltInKind BuiltInUnswizzledLocalInvocationIndex = static_cast<Bui
 namespace lgcName {
 const static char InternalCallPrefix[] = "lgc.";
 const static char InputCallPrefix[] = "lgc.input.";
-const static char InputImportVertex[] = "lgc.input.import.vertex.";
-const static char InputImportGeneric[] = "lgc.input.import.generic.";
 const static char InputImportBuiltIn[] = "lgc.input.import.builtin.";
-const static char InputImportInterpolant[] = "lgc.input.import.interpolant.";
 const static char OutputCallPrefix[] = "lgc.output.";
-const static char OutputImportGeneric[] = "lgc.output.import.generic.";
 const static char OutputImportBuiltIn[] = "lgc.output.import.builtin.";
 const static char OutputExportGeneric[] = "lgc.output.export.generic.";
 const static char OutputExportBuiltIn[] = "lgc.output.export.builtin.";

lgc/interface/lgc/LgcDialect.td
@@ -79,3 +79,89 @@ def BufferPtrDiffOp : LgcOp<"buffer.ptr.diff", [Memory<[]>, WillReturn]> {
     If `lhs` and `rhs` are both null, the result is 0.
   }];
 }
+
+def GenericLocationOp : OpClass<LgcDialect> {
+  let arguments = (ins AttrI1:$perPrimitive, AttrI32:$location, I32:$locOffset, I32:$elemIdx, I32:$arrayIndex);
+
+  let summary = "family of operations that reference generic shader I/O locations";
+  let description = [{
+    Generic (graphics) shader I/O interfaces are addressed in terms of locations. Locations are 4-element vectors with
+    32-bit elements. Within an interface, the location vectors are indexed by `location + locOffset`. The elements of
+    the vector are indexed by `elemIdx`.
+
+    For some shader stages, the interface is inherently an array. For example, GS inputs are an array of per-vertex
+    inputs, where the size of the array depends on the input primitive type (1 for points, 2 for lines, etc.).
+
+    Some shader stages have both array and non-array interfaces. Specifically, TCS outputs and TES inputs have per-patch
+    locations (non-array) and per-control-point locations (array with one entry per control point).
+
+    For array interfaces, `arrayIndex` is used to index into the array, e.g. arrayIndex is the control point index (in
+    TCS outputs and TES inputs) or the vertex index (in GS inputs). For non-array interfaces, `arrayIndex` is ignored
+    entirely and is recommended to be set to `poison`.
+
+    Some shader stages have a notion of inputs or outputs that are "per primitive":
+    - TCS outputs: per-patch vs. per-control-point
+    - TES inputs: per-patch vs. per-control-point
+    - MESH outputs: per-primitive vs. per-vertex
+    - PS inputs: per-primitive vs. per-vertex/interpolated (note: old-school flat-interpolated inputs are considered
+      to be interpolated)
+
+    TODO: Tease out the exact rules for when `locOffset` is allowed to be non-0. For inputs, this is originally only
+    TCS, TES, and PS in special cases.
+  }];
+}
+
+def InputImportGenericOp : LgcOp<"input.import.generic", [Memory<[]>, WillReturn]> {
+  let superclass = GenericLocationOp;
+
+  let arguments = (ins GenericLocationOp);
+  let results = (outs value:$result);
+
+  let defaultBuilderHasExplicitResultType = true;
+
+  let summary = "read a generic shader input variable";
+  let description = [{
+    Reads a generic graphics shader input variable, either from a vertex buffer or from the previous stage outputs.
+
+    Used in all graphics shader stages except task and mesh shader.
+  }];
+}
+
+// TODO: Consider restricting the memory effect to inaccessible memory only.
+def OutputImportGenericOp : LgcOp<"output.import.generic", [Memory<[(read)]>, WillReturn]> {
+  let superclass = GenericLocationOp;
+
+  let arguments = (ins GenericLocationOp);
+  let results = (outs value:$result);
+
+  let defaultBuilderHasExplicitResultType = true;
+
+  let summary = "read back a generic shader output variable";
+  let description = [{
+    Reads back a generic shader output variable.
+
+    Only used in TCS, where outputs can be used to communicate between multiple invocations.
+  }];
+}
+
+def InputImportInterpolatedOp : LgcOp<"input.import.interpolated", [Memory<[]>, WillReturn]> {
+  let superclass = GenericLocationOp;
+
+  let arguments = (ins GenericLocationOp, AttrI32:$interpMode, value:$interpValue);
+  let results = (outs value:$result);
+
+  let defaultBuilderHasExplicitResultType = true;
+
+  let summary = "read a generic per-vertex (interpolated) pixel shader input";
+  let description = [{
+    Only used in PS for per-vertex/interpolated inputs. Use `input.import.generic` for per-primitive inputs.
+
+    `interpMode` is one of:
+    
+    - InterpModeSmooth for interpolation using the `<2 x float>` barycentrics in `interpValue`
+    - InterpModeFlat for flat shading; `interpValue` is ignored and is recommended to be `poison`
+    - InterpModeCustom to retrieve the attribute of the vertex with the `i32` index `interpValue` (which must be 0, 1,
+      or 2). The raw HW vertex index is used, which may be different from the API vertex index; it is up to the user of
+      this operation to map between HW and API.
+  }];
+}

commit 3c0aa285f9ddac75b3ac68581439a01d6bffa1a2

VertexFetch: fix an uber shader regression
This was a typical case of rebasing an older change and then missing
some code that had changed elsewhere in the meantime.

Fixes: c1e683e022b ("lgc: dialectify input.import and output.import ops")

lgc/include/lgc/patch/VertexFetch.h
@@ -36,6 +36,7 @@
 namespace lgc {
 
 class BuilderBase;
+class InputImportGenericOp;
 
 // =====================================================================================================================
 // Public interface to vertex fetch manager.
@@ -51,7 +52,7 @@ public:
                                    unsigned compIdx, BuilderBase &builder) = 0;
 
   // Generate code to fetch a vertex value for uber shader
-  virtual llvm::Value *fetchVertex(llvm::CallInst *vertexFetches, llvm::Value *inputDesc, BuilderBase &builder) = 0;
+  virtual llvm::Value *fetchVertex(InputImportGenericOp *vertexFetch, llvm::Value *inputDesc, BuilderBase &builder) = 0;
 };
 
 // =====================================================================================================================

lgc/patch/VertexFetch.cpp
@@ -94,7 +94,7 @@ public:
                      BuilderBase &builder) override;
 
   // Generate code to fetch a vertex value for uber shader
-  Value *fetchVertex(llvm::CallInst *callInst, Value *descPtr, BuilderBase &builder) override;
+  Value *fetchVertex(InputImportGenericOp *inst, Value *descPtr, BuilderBase &builder) override;
 
 private:
   void initialize(PipelineState *pipelineState);
@@ -577,12 +577,12 @@ bool LowerVertexFetch::runImpl(Module &module, PipelineState *pipelineState) {
     auto uberFetchAttrType = FixedVectorType::get(builder.getInt32Ty(), 4);
     auto descPtr = desBuilder->CreateIntToPtr(desc, PointerType::get(uberFetchAttrType, ADDR_SPACE_CONST));
 
-    for (CallInst *call : vertexFetches) {
-      builder.SetInsertPoint(call);
-      Value *vertex = vertexFetch->fetchVertex(call, descPtr, builder);
-      // Replace and erase this call.
-      call->replaceAllUsesWith(vertex);
-      call->eraseFromParent();
+    for (InputImportGenericOp *inst : vertexFetches) {
+      builder.SetInsertPoint(inst);
+      Value *vertex = vertexFetch->fetchVertex(inst, descPtr, builder);
+      // Replace and erase this instruction.
+      inst->replaceAllUsesWith(vertex);
+      inst->eraseFromParent();
     }
     return true;
   }
@@ -928,40 +928,37 @@ Function *VertexFetchImpl::generateFetchFunction(unsigned bitWidth, Module *modu
 }
 
 // =====================================================================================================================
-// This is an lgc.input.import.vertex operation.
-// Executes vertex fetch operations based on the uber shader buffer
+// This is an lgc.input.import.generic operation for vertex buffers.
+// Executes vertex fetch operations based on the uber shader buffer.
 //
-// @param callInst : call instruction
+// @param inst : the input instruction
 // @param descPtr : 64bit address of buffer
 // @param builder : Builder to use to insert vertex fetch instructions
 // @returns : vertex
-Value *VertexFetchImpl::fetchVertex(CallInst *callInst, llvm::Value *descPtr, BuilderBase &builder) {
-  unsigned location = cast<ConstantInt>(callInst->getArgOperand(0))->getZExtValue();
-  unsigned compIdx = cast<ConstantInt>(callInst->getArgOperand(1))->getZExtValue();
+Value *VertexFetchImpl::fetchVertex(InputImportGenericOp *inst, llvm::Value *descPtr, BuilderBase &builder) {
+  unsigned location = inst->getLocation();
+  unsigned compIdx = cast<ConstantInt>(inst->getElemIdx())->getZExtValue();
   auto zero = builder.getInt32(0);
 
   if (!m_vertexIndex) {
-    auto savedInsertPoint = builder.saveIP();
-    builder.SetInsertPoint(&*callInst->getFunction()->front().getFirstInsertionPt());
+    IRBuilderBase::InsertPointGuard ipg(builder);
+    builder.SetInsertPoint(&*inst->getFunction()->front().getFirstInsertionPt());
     m_vertexIndex = ShaderInputs::getVertexIndex(builder, *m_lgcContext);
-    builder.restoreIP(savedInsertPoint);
   }
 
   if (!m_instanceIndex) {
-    auto savedInsertPoint = builder.saveIP();
-    builder.SetInsertPoint(&*callInst->getFunction()->front().getFirstInsertionPt());
+    IRBuilderBase::InsertPointGuard ipg(builder);
+    builder.SetInsertPoint(&*inst->getFunction()->front().getFirstInsertionPt());
     m_instanceIndex = ShaderInputs::getInstanceIndex(builder, *m_lgcContext);
-    builder.restoreIP(savedInsertPoint);
   }
 
   // Get the vertex buffer table pointer as pointer to v4i32 descriptor.
   Type *vbDescTy = FixedVectorType::get(Type::getInt32Ty(*m_context), 4);
   if (!m_vertexBufTablePtr) {
-    auto savedInsertPoint = builder.saveIP();
-    builder.SetInsertPoint(&*callInst->getFunction()->front().getFirstInsertionPt());
+    IRBuilderBase::InsertPointGuard ipg(builder);
+    builder.SetInsertPoint(&*inst->getFunction()->front().getFirstInsertionPt());
     m_vertexBufTablePtr =
         ShaderInputs::getSpecialUserDataAsPointer(UserDataMapping::VertexBufferTable, vbDescTy, builder);
-    builder.restoreIP(savedInsertPoint);
   }
 
   // The size of each input descriptor is sizeof(UberFetchShaderAttribInfo). vector4
@@ -1034,13 +1031,12 @@ Value *VertexFetchImpl::fetchVertex(CallInst *callInst, llvm::Value *descPtr, Bu
   // Select VbIndex
   Value *vbIndex = builder.CreateSelect(isPerInstance, vbIndexInstance, m_vertexIndex);
 
-  auto inputType = callInst->getType();
-  auto inputTy = callInst->getType();
+  const auto inputTy = inst->getType();
   Type *basicTy = inputTy->isVectorTy() ? cast<VectorType>(inputTy)->getElementType() : inputTy;
   const unsigned bitWidth = basicTy->getScalarSizeInBits();
   assert(bitWidth == 8 || bitWidth == 16 || bitWidth == 32 || bitWidth == 64);
 
-  auto func = generateFetchFunction(bitWidth, callInst->getFunction()->getParent());
+  auto func = generateFetchFunction(bitWidth, inst->getFunction()->getParent());
   Value *lastVert =
       builder.CreateCall(func, {vbDesc, vbIndex, byteOffset, componentSize, isPacked, isBgr, yMask, zMask, wMask});
 
@@ -1083,7 +1079,7 @@ Value *VertexFetchImpl::fetchVertex(CallInst *callInst, llvm::Value *descPtr, Bu
       vertex = builder.CreateInsertElement(vertex, vertexValues[i], ConstantInt::get(Type::getInt32Ty(*m_context), i));
   }
 
-  const bool is8bitFetch = (inputType->getScalarSizeInBits() == 8);
+  const bool is8bitFetch = (inputTy->getScalarSizeInBits() == 8);
 
   if (is8bitFetch) {
     // NOTE: The vertex fetch results are represented as <n x i16> now. For 8-bit vertex fetch, we have to