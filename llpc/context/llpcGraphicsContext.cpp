e9c87cc718373cfb6e78203af09813279bf2e9a0
Ruiling Song <ruiling.song@amd.com>
2023-07-07 15:29:37 +0800

    llpc: Initialize m_useDualSourceBlend
    This was removed in #2478 by mistake. Add it back.

--- llpc/context/llpcGraphicsContext.cpp
+++ llpc/context/llpcGraphicsContext.cpp
@@ -60,7 +60,7 @@ static cl::opt<bool> DisableColorExportShader("disable-color-export-shader", cl:
 GraphicsContext::GraphicsContext(GfxIpVersion gfxIp, const GraphicsPipelineBuildInfo *pipelineInfo,
                                  MetroHash::Hash *pipelineHash, MetroHash::Hash *cacheHash)
     : PipelineContext(gfxIp, pipelineHash, cacheHash), m_pipelineInfo(pipelineInfo), m_stageMask(0),
-      m_preRasterHasGs(false), m_activeStageCount(0) {
+      m_preRasterHasGs(false), m_useDualSourceBlend(false), m_activeStageCount(0) {
   const Vkgc::BinaryData *gpurtShaderLibrary = nullptr;
 #if LLPC_CLIENT_INTERFACE_MAJOR_VERSION < 62
   gpurtShaderLibrary = &pipelineInfo->shaderLibrary;
