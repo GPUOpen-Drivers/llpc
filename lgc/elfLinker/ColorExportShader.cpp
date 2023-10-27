GPUOpen-Drivers/llpc

commit ab328131304b9beff82bb433ba061646217e97ca

lgc: simplify ColorExportShader::updatePalMetadata
All these values are set by FragColorExport::generateExportInstructions
since a recent refactor, but it seems like this cleanup got dropped.

(cherry picked from commit 4bbeda591b08bc1056160d818f5d1178ed3204e2)

lgc/elfLinker/ColorExportShader.cpp
@@ -176,49 +176,5 @@ Function *ColorExportShader::createColorExportFunc() {
 //
 // @param [in/out] outStream : The PAL metadata object in which to update the color format.
 void ColorExportShader::updatePalMetadata(PalMetadata &palMetadata) {
-  SmallVector<ExportFormat> finalExportFormats;
-  bool hasDepthExpFmtZero = true;
-  for (auto &info : m_exports) {
-    if (info.hwColorTarget != MaxColorTargets) {
-      ExportFormat expFmt = m_exportFormat[info.hwColorTarget];
-      if (expFmt != EXP_FORMAT_ZERO)
-        finalExportFormats.push_back(expFmt);
-    } else {
-      hasDepthExpFmtZero = false;
-      unsigned depthMask = info.location;
-      if (!m_killEnabled && m_pipelineState->getTargetInfo().getGfxIpVersion().major >= 11 &&
-          m_pipelineState->getColorExportState().alphaToCoverageEnable) {
-        for (auto &curInfo : m_exports) {
-          if (curInfo.hwColorTarget == EXP_TARGET_MRT_0 && (m_exportFormat[EXP_TARGET_MRT_0] > EXP_FORMAT_32_GR)) {
-            // Mrt0 is enabled and its alpha channel is enabled
-            depthMask |= 0x8;
-            break;
-          }
-        }
-      }
-
-      unsigned depthExpFmt = EXP_FORMAT_ZERO;
-      if (depthMask & 0x4)
-        depthExpFmt = EXP_FORMAT_32_ABGR;
-      else if (depthMask & 0x2)
-        depthExpFmt = (depthMask & 0x8) ? EXP_FORMAT_32_ABGR : EXP_FORMAT_32_GR;
-      else if (depthMask & 0x1)
-        depthExpFmt = (depthMask & 0x8) ? EXP_FORMAT_32_AR : EXP_FORMAT_32_R;
-      palMetadata.setSpiShaderZFormat(depthExpFmt);
-    }
-  }
-
-  if (finalExportFormats.size() == 0 && hasDepthExpFmtZero) {
-    if (m_pipelineState->getTargetInfo().getGfxIpVersion().major < 10 || m_killEnabled) {
-      // NOTE: Hardware requires that fragment shader always exports "something" (color or depth) to the SX.
-      // If both SPI_SHADER_Z_FORMAT and SPI_SHADER_COL_FORMAT are zero, we need to override
-      // SPI_SHADER_COL_FORMAT to export one channel to MRT0. This dummy export format will be masked
-      // off by updateCbShaderMask().
-      finalExportFormats.push_back(EXP_FORMAT_32_R);
-    }
-  }
-
-  palMetadata.updateSpiShaderColFormat(finalExportFormats);
-  palMetadata.updateCbShaderMask(m_exports);
   palMetadata.updateDbShaderControl();
 }

commit a34a24206e6881f4ef3ce29ee5d7273c31f2dcc7

ColorExportShader: properly control the PAL metadata written to the ELF
Write the PAL metadata related to fragment exports written to the glue
shader ELF, and *only* that metadata (in addition to generic metadata
added by the LLVM backend).

Prior to this change, no case was entirely sound:

- in the "enableColorExportShader" case (which really means: use a jump
  to get to the CES instead of pasting it), PAL metadata was added from
  the PipelineState object that is used for the invoking pipeline, which
  risks leaking state into the glue shader that we don't want to leak
  (and which might later cause problems if a glue shader with leaked
  state is re-used via caching)

- in the "!enableColorExportShader" case, no PAL metadata was added at
  all

This fixes a regression in the fast library link implementation.

Fixes: ab328131304b9be ("lgc: simplify ColorExportShader::updatePalMetadata")

lgc/elfLinker/ColorExportShader.cpp
@@ -116,14 +116,19 @@ Module *ColorExportShader::generate() {
     values[m_exports[idx].hwColorTarget] = colorExportFunc->getArg(idx);
   }
 
+  PalMetadata palMetadata{m_pipelineState, m_pipelineState->useRegisterFieldFormat()};
+
   bool dummyExport = m_lgcContext->getTargetInfo().getGfxIpVersion().major < 10 || m_killEnabled;
-  fragColorExport.generateExportInstructions(m_exports, values, dummyExport, builder);
+  fragColorExport.generateExportInstructions(m_exports, values, dummyExport, &palMetadata, builder);
 
   if (m_pipelineState->getOptions().enableColorExportShader) {
     builder.CreateIntrinsic(Intrinsic::amdgcn_endpgm, {}, {});
     builder.CreateUnreachable();
     ret->eraseFromParent();
   }
+
+  palMetadata.updateDbShaderControl();
+  palMetadata.record(colorExportFunc->getParent());
 
   return colorExportFunc->getParent();
 }
@@ -174,5 +179,4 @@ Function *ColorExportShader::createColorExportFunc() {
 //
 // @param [in/out] outStream : The PAL metadata object in which to update the color format.
 void ColorExportShader::updatePalMetadata(PalMetadata &palMetadata) {
-  palMetadata.updateDbShaderControl();
 }

lgc/elfLinker/GlueShader.cpp
@@ -47,21 +47,8 @@ void GlueShader::compile(raw_pwrite_stream &outStream) {
   // Generate the glue shader IR module.
   std::unique_ptr<Module> module(generate());
 
-  // For explicit color export shader, some registers are required.
-  if (m_pipelineState->getOptions().enableColorExportShader) {
-    m_pipelineState->getPalMetadata()->updateDbShaderControl();
-  }
-
-  // Record pipeline state
-  m_pipelineState->record(&*module);
-
-  // For explicit color export shader, meta data have added some registers.
-  if (!m_pipelineState->getOptions().enableColorExportShader) {
-    // Add empty PAL metadata, to ensure that the back-end writes its PAL metadata in MsgPack format.
-    PalMetadata *palMetadata = new PalMetadata(nullptr, m_pipelineState->useRegisterFieldFormat());
-    palMetadata->record(&*module);
-    delete palMetadata;
-  }
+  // Record pipeline state so that it is available during the subsequent generic IR passes.
+  m_pipelineState->recordExceptPalMetadata(&*module);
 
   // Get the pass managers and run them on the module, generating ELF.
   std::pair<lgc::PassManager &, LegacyPassManager &> passManagers =

lgc/include/lgc/patch/FragColorExport.h
@@ -62,7 +62,7 @@ public:
   FragColorExport(llvm::LLVMContext *context, PipelineState *pipelineState);
 
   void generateExportInstructions(llvm::ArrayRef<lgc::ColorExportInfo> info, llvm::ArrayRef<llvm::Value *> values,
-                                  bool dummyExport, BuilderBase &builder);
+                                  bool dummyExport, PalMetadata *palMetadata, BuilderBase &builder);
   static void setDoneFlag(llvm::Value *exportInst, BuilderBase &builder);
   static llvm::CallInst *addDummyExport(BuilderBase &builder);
   static llvm::Function *generateNullFragmentShader(llvm::Module &module, PipelineState *pipelineState,

lgc/include/lgc/state/PipelineState.h
@@ -238,6 +238,8 @@ public:
   // Record pipeline state into IR metadata of specified module.
   void record(llvm::Module *module);
 
+  void recordExceptPalMetadata(llvm::Module *module);
+
   // Print pipeline state
   void print(llvm::raw_ostream &out) const;
 #if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)

lgc/patch/FragColorExport.cpp
@@ -489,7 +489,8 @@ bool LowerFragColorExport::runImpl(Module &module, PipelineShadersResult &pipeli
   FragColorExport fragColorExport(m_context, m_pipelineState);
   bool dummyExport =
       (m_pipelineState->getTargetInfo().getGfxIpVersion().major < 10 || m_resUsage->builtInUsage.fs.discard);
-  fragColorExport.generateExportInstructions(m_info, m_exportValues, dummyExport, builder);
+  fragColorExport.generateExportInstructions(m_info, m_exportValues, dummyExport, m_pipelineState->getPalMetadata(),
+                                             builder);
   return !m_info.empty() || dummyExport;
 }
 
@@ -872,9 +873,10 @@ Value *FragColorExport::dualSourceSwizzle(BuilderBase &builder) {
 // @param info : The color export information for each color export in no particular order.
 // @param values : The values that are to be exported.  Indexed by the hw color target.
 // @param exportFormat : The export format for each color target. Indexed by the hw color target.
+// @param [out] palMetadata : The PAL metadata that will be extended with relevant information.
 // @param builder : The builder object that will be used to create new instructions.
 void FragColorExport::generateExportInstructions(ArrayRef<ColorExportInfo> info, ArrayRef<Value *> values,
-                                                 bool dummyExport, BuilderBase &builder) {
+                                                 bool dummyExport, PalMetadata *palMetadata, BuilderBase &builder) {
   SmallVector<ExportFormat> finalExportFormats;
   Value *lastExport = nullptr;
 
@@ -928,7 +930,7 @@ void FragColorExport::generateExportInstructions(ArrayRef<ColorExportInfo> info,
     else if (depthMask & 0x1)
       depthExpFmt = (depthMask & 0x8) ? EXP_FORMAT_32_AR : EXP_FORMAT_32_R;
 
-    m_pipelineState->getPalMetadata()->setSpiShaderZFormat(depthExpFmt);
+    palMetadata->setSpiShaderZFormat(depthExpFmt);
     info = info.drop_front(1);
   }
 
@@ -965,8 +967,8 @@ void FragColorExport::generateExportInstructions(ArrayRef<ColorExportInfo> info,
   if (lastExport)
     FragColorExport::setDoneFlag(lastExport, builder);
 
-  m_pipelineState->getPalMetadata()->updateSpiShaderColFormat(finalExportFormats);
-  m_pipelineState->getPalMetadata()->updateCbShaderMask(info);
+  palMetadata->updateSpiShaderColFormat(finalExportFormats);
+  palMetadata->updateCbShaderMask(info);
 }
 
 // =====================================================================================================================

lgc/state/PipelineState.cpp
@@ -435,14 +435,25 @@ void PipelineState::clear(Module *module) {
 //
 // @param [in/out] module : Module to record the IR metadata in
 void PipelineState::record(Module *module) {
+  recordExceptPalMetadata(module);
+  if (m_palMetadata)
+    m_palMetadata->record(module);
+}
+
+// =====================================================================================================================
+// Record pipeline state except for PAL metadata into IR metadata of specified module.
+//
+// TODO: This method shouldn't exist. It is a temporary workaround to the fact that the entire main pipeline state
+//       leaks into glue shader compilation, but we mustn't have all associated PAL metadata in the glue shader ELF.
+//
+// @param [in/out] module : Module to record the IR metadata in
+void PipelineState::recordExceptPalMetadata(Module *module) {
   recordOptions(module);
   recordUserDataNodes(module);
   recordDeviceIndex(module);
   recordVertexInputDescriptions(module);
   recordColorExportState(module);
   recordGraphicsState(module);
-  if (m_palMetadata)
-    m_palMetadata->record(module);
 }
 
 // =====================================================================================================================